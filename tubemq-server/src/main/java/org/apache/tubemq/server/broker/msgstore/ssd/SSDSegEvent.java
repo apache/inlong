f311e5ddbca7a369d0ad102afccf26287e661f51
gosonzhang <4675739@qq.com>
2020-02-13 10:49:01 +0800

    [TUBEMQ-29] Change the package name to "org.apache.tubemq." (#21)
    * [TUBEMQ-29] Change the package name to "org.apache.tubemq."
    
    * [TUBEMQ-29] Change the package name to "org.apache.tubemq."
    
    * [TUBEMQ-29] Change the package name to "org.apache.tubemq."
    
    * [TUBEMQ-29] Change the package name to "org.apache.tubemq."
    
    Compatible with older versions

---
+++ tubemq-server/src/main/java/org/apache/tubemq/server/broker/msgstore/ssd/SSDSegEvent.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tubemq.server.broker.msgstore.ssd;
+
+/***
+ * Transfer files on disk to ssd event.
+ */
+public class SSDSegEvent {
+    public final String storeKey;
+    public final String partStr;
+    public final long startOffset;
+    public final int segCnt;
+
+
+    public SSDSegEvent(final String partStr, final String storeKey,
+                       final long startOffset, final int segCnt) {
+        this.partStr = partStr;
+        this.storeKey = storeKey;
+        this.startOffset = startOffset;
+        this.segCnt = segCnt;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(512).append("{storeKey=").append(storeKey)
+                .append(",partStr=").append(partStr).append(",startOffset=")
+                .append(startOffset).append(",segCnt=").append(segCnt)
+                .append("}").toString();
+    }
+
+
+}
---
+++ tubemq-server/src/main/java/org/apache/tubemq/server/common/aaaserver/CertificateMasterHandler.java
@@ -0,0 +1,39 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.tubemq.server.common.aaaserver;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import org.apache.tubemq.corebase.protobuf.generated.ClientMaster;
+
+
+public interface CertificateMasterHandler {
+
+    CertifiedResult identityValidBrokerInfo(final ClientMaster.MasterCertificateInfo authenticInfo);
+
+    CertifiedResult identityValidUserInfo(final ClientMaster.MasterCertificateInfo authenticInfo,
+                                          boolean isProduce);
+
+    CertifiedResult validProducerAuthorizeInfo(String userName, Set<String> topics, String clientIp);
+
+    CertifiedResult validConsumerAuthorizeInfo(String userName, String grounName, Set<String> topics,
+                                               Map<String, TreeSet<String>> topicConds, String clientIp);
+
+
+}
