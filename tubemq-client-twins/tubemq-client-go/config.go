0cc2ddf72dc8a99b528841ace5aebaddeabd10a4
lubanproj <1811704358@qq.com>
2020-07-03 11:27:57 +0800

    [TUBEMQ-264] Init pull consumer and config

---
+++ tubemq-client-twins/tubemq-client-go/config.go
@@ -0,0 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tubeclient
+
+// Config defines the configurations of Consumer
+type Config struct {
+	// Group is the name of a consumer group
+	// Only one consumer in the same consumer Group can consume messages at any one time
+	Group string
+	// Address TODO...
+	Address string
+}
