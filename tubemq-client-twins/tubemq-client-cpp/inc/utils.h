1e55e3cd4d4371d08426c2b3610f5c14940d7328
gosonzhang <4675739@qq.com>
2020-07-05 12:05:32 +0000

    [TUBEMQ-263] Create C/C++ ini file read utils [addendum] (#185)
    Co-authored-by: gosonzhang <gosonzhang@tencent.com>

--- tubemq-client-twins/tubemq-client-cpp/inc/utils.h
+++ tubemq-client-twins/tubemq-client-cpp/inc/utils.h
@@ -47,6 +47,8 @@ class Utils {
                    const string& group_name, string& tgt_group_name);
   static bool ValidFilterItem(string& err_info, 
                    const string& src_filteritem, string& tgt_filteritem);
+  static string int2str(int data);
+  static string long2str(long data);
   static long GetCurrentTimeMillis();
 
 };
