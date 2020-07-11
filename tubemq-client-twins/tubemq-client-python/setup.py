754c9659993ef61a414fb57cc974159e05ff6602
dockerzhang <zhchao_beyond@outlook.com>
2020-07-11 17:02:41 +0800

    [TUBEMQ-260] Init Python client module (#201)
    Co-authored-by: dockerzhang(张超) <dockerzhang@tencent.com>

---
+++ tubemq-client-twins/tubemq-client-python/setup.py
@@ -0,0 +1,30 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+from setuptools import setup
+
+setup(
+        name='tubemq-client-python',
+        version='0.1.0',
+        description='TubeMQ Python SDK Client',
+        author='dockerzhang',
+        author_email='dockerzhang@tencent.com',
+        url='https://tubemq.apache.org/',
+        packages=['tubemq-client-python'],
+)
