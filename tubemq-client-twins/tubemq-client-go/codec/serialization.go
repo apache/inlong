6f243c9a1ac5f9352549a07ad5f7c8d6c08e537a
lubanproj <1811704358@qq.com>
2020-08-07 16:49:36 +0800

    [TUBEMQ-277] refactor(codec): add liscense, remove unnecessary characters

--- tubemq-client-twins/tubemq-client-go/codec/serialization.go
+++ tubemq-client-twins/tubemq-client-go/codec/serialization.go
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package codec
 
 import (
@@ -56,7 +73,7 @@ func (d *pbSerialization) Marshal(v interface{}) ([]byte, error) {
 		return nil, errors.New("marshal nil interface{}")
 	}
 	if pm, ok := v.(proto.Marshaler); ok {
-		// 可以 marshal 自身，无需 buffer
+		// unmarshal itself, without buffer
 		return pm.Marshal()
 	}
 	buffer := bufferPool.Get().(*cachedBuffer)
@@ -85,7 +102,7 @@ func (d *pbSerialization) Unmarshal(data []byte, v interface{}) error {
 	protoMsg.Reset()
 
 	if pu, ok := protoMsg.(proto.Unmarshaler); ok {
-		// 可以 unmarshal 自身，无需 buffer
+		// unmarshal itself, without buffer
 		return pu.Unmarshal(data)
 	}
 
