0981ae20cc07efc90b73faf4ee004033d563a49f
dockerzhang <zhchao_beyond@outlook.com>
2020-11-02 16:39:52 +0800

    [TUBEMQ-399] expose C++ SDK method by Pybind11 (#299)
    Co-authored-by: dockerzhang <dockerzhang@tencent.com>

---
+++ tubemq-client-twins/tubemq-client-python/src/cpp/tubemq_message.cc
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <pybind11/pybind11.h>
+#include <stdint.h>
+#include "tubemq/tubemq_message.h"
+
+namespace py = pybind11;
+
+using namespace tubemq;
+using std::string;
+
+PYBIND11_MODULE(tubemq_message, m) {
+    py::class_<Message>(m, "Message")
+        .def(py::init<>())
+        .def(py::init<const Message&>())
+        .def(py::init<const string&, const char*, uint32_t>())
+        .def("getMessageId", &Message::GetMessageId)
+        .def("setMessageId", &Message::SetMessageId)
+        .def("getTopic", &Message::GetTopic)
+        .def("setTopic", &Message::SetTopic)
+        .def("getData", &Message::GetData)
+        .def("getDataLength", &Message::GetDataLength);
+}
