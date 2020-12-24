7db938b7b7f75713d3817950f483c6b9d2dd0f00
yuanbo <yuanbo@apache.org>
2020-12-24 10:49:44 +0800

    [TUBEMQ-477] implement state machine for tasks and jobs

---
+++ tubemq-agent/agent-common/src/main/java/org/apache/tubemq/agent/state/StateCallback.java
@@ -0,0 +1,28 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tubemq.agent.state;
+
+/**
+ * state call back
+ */
+public interface StateCallback {
+
+    /**
+     * state change call back
+     *
+     * @param before - before state
+     * @param after - after state
+     */
+    void call(State before, State after);
+}
