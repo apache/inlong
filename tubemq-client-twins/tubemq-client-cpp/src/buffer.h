8cf9e4d4dcc5109d236fb3efb9ce2b7502a104a3
gosonzhang <gosonzhang@tencent.com>
2021-01-28 16:55:44 +0800

    [TUBEMQ-546]Restore the original license header of the referenced external source files

--- tubemq-client-twins/tubemq-client-cpp/src/buffer.h
+++ tubemq-client-twins/tubemq-client-cpp/src/buffer.h
@@ -1,24 +1,8 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// Modified from muduo project http://github.com/chenshuo/muduo
+// @see https://github.com/chenshuo/muduo/blob/master/muduo/net/Buffer.h and https://github.com/chenshuo/muduo/blob/master/muduo/net/Buffer.cc
 
 // Modified from evpp
-// @see https://github.com/Qihoo360/evpp/blob/master/evpp/buffer.h
+// @see https://github.com/Qihoo360/evpp/blob/master/evpp/buffer.h, commit version b2535d7
 
 #ifndef _TUBEMQ_BUFFER_H_
 #define _TUBEMQ_BUFFER_H_
