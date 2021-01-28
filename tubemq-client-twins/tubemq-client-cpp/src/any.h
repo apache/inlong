9530caec89fc123c5e2e91f60def89b7c64789fe
gosonzhang <gosonzhang@tencent.com>
2021-01-28 16:55:44 +0800

    [TUBEMQ-546]Restore the original license header of the referenced external source files

--- tubemq-client-twins/tubemq-client-cpp/src/any.h
+++ tubemq-client-twins/tubemq-client-cpp/src/any.h
@@ -1,24 +1,5 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 // Copy from evpp project
-// @see https://github.com/Qihoo360/evpp/blob/master/evpp/any.h
+// @see https://github.com/Qihoo360/evpp/blob/master/evpp/any.h commit version c5038a6
 
 #ifndef TUBEMQ_CLIENT_CPP_SRC_ANY_H_
 #define TUBEMQ_CLIENT_CPP_SRC_ANY_H_
