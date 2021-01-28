ffb5205bb02fbdf426073906161f61729bdbeb43
gosonzhang <gosonzhang@tencent.com>
2021-01-28 16:55:44 +0800

    [TUBEMQ-546]Restore the original license header of the referenced external source files

--- tubemq-core/src/main/java/org/apache/tubemq/corebase/utils/TStringUtils.java
+++ tubemq-core/src/main/java/org/apache/tubemq/corebase/utils/TStringUtils.java
@@ -1,18 +1,24 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+/*
+ * Copyright (c) 2002-2012 Alibaba Group Holding Limited.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/*
+ * Modified from citrus <a href="https://github.com/webx/citrus">citrus Project</a>
+ *   file address: https://github.com/webx/citrus/blob/r3.1.4/common/util/src/
+ *                          main/java/com/alibaba/citrus/util/StringUtil.java
  */
 
 package org.apache.tubemq.corebase.utils;
@@ -28,7 +34,6 @@ import org.apache.tubemq.corebase.TokenConstants;
 
 /**
  * Utility to String operations.
- * Modified version of <a href="https://github.com/webx/citrus">citrus Project</a>
  */
 public class TStringUtils {
 
