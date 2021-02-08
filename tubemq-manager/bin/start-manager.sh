2d9cef802d7d69ec1cfd8b43ec892aa1400f15a8
gosonzhang <4675739@qq.com>
2021-02-08 22:32:18 +0800

    [TUBEMQ-558] Adjust the LICENSE of the file header (#424)
    Co-authored-by: gosonzhang <gosonzhang@tencent.com>

--- tubemq-manager/bin/start-manager.sh
+++ tubemq-manager/bin/start-manager.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
@@ -8,8 +13,9 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions an
+# See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 base_dir=$(dirname $0)
 
