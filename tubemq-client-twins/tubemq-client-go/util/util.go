d3d07ce879462df887cdd49b2c772617eb1c2da3
Zijie Lu <wslzj40@gmail.com>
2021-06-03 17:48:44 +0800

    Add license and comments
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- tubemq-client-twins/tubemq-client-go/util/util.go
+++ tubemq-client-twins/tubemq-client-go/util/util.go
@@ -1,11 +1,31 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// package util defines the constants and helper functions.
 package util
 
 import (
 	"net"
 )
 
+// InValidValue of TubeMQ config.
 var InvalidValue = int64(-2)
 
+// GetLocalHost returns the local host name.
 func GetLocalHost() string {
 	ifaces, err := net.Interfaces()
 	if err != nil {
@@ -43,10 +63,12 @@ func GetLocalHost() string {
 	return ""
 }
 
+// GenBrokerAuthenticateToken generates the broker authenticate token.
 func GenBrokerAuthenticateToken(username string, password string) string {
 	return ""
 }
 
+// GenMasterAuthenticateToken generates the master authenticate token.
 func GenMasterAuthenticateToken(username string, password string) string {
 	return ""
 }
