d1c868da8614d2bb643983fe7ac8c8fa7e9139db
dockerzhang <dockerzhang@tencent.com>
2021-07-22 12:46:53 +0800

    [INLONG-779] update tubemq manager docker image

--- inlong-tubemq/tubemq-docker/tubemq-manager/manager-docker.sh
+++ inlong-tubemq/tubemq-docker/tubemq-manager/manager-docker.sh
@@ -1,26 +1,37 @@
+#!/bin/sh
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements. See the NOTICE file distributed with
+# contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# <p>
-# http://www.apache.org/licenses/LICENSE-2.0
-# <p>
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+file_path=$(cd "$(dirname "$0")";pwd)
+# config
+cat <<EOF > ${file_path}/../conf/application.properties
 spring.jpa.hibernate.ddl-auto=update
 # configuration for admin
 topic.config.schedule=0/5 * * * * ?
 broker.reload.schedule=0/5 * * * * ?
 # mysql configuration for manager
-spring.datasource.url=jdbc:mysql://mysql_ip:mysql_port/tubemanager
-spring.datasource.username=mysql_username
-spring.datasource.password=mysql_password
+spring.datasource.url=jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/tubemanager
+spring.datasource.username=$MYSQL_USER
+spring.datasource.password=$MYSQL_PASSWD
 # server port
 server.port=8089
+EOF
+# start
+sh ${file_path}/start-manager.sh
+sleep 3
+# keep alive
+tail -F ${file_path}/../logs/tubemq-manager.out
