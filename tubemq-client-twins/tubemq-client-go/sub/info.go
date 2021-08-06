e8be97b08758f2fd6a06893415c7dc9e45eef9cf
Zijie Lu <wslzj40@gmail.com>
2021-08-06 10:40:51 +0800

    [INLONG-1421]Update Go SDK Apache License
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- tubemq-client-twins/tubemq-client-go/sub/info.go
+++ tubemq-client-twins/tubemq-client-go/sub/info.go
@@ -1,21 +1,21 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
-// package sub defines the subscription information of a client.
+// Package sub defines the subscription information of a client.
 package sub
 
 import (
@@ -26,7 +26,7 @@ import (
 	"github.com/apache/incubator-inlong/tubemq-client-twins/tubemq-client-go/protocol"
 )
 
-// InvalidOffset represents the offset which is invalid.
+// InValidOffset represents the offset which is invalid.
 const InValidOffset = -2
 
 // SubInfo represents the sub information of the client.
