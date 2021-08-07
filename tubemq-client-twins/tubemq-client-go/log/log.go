0480e0b4cbf5219f64f13184746be7d41e85d832
Zijie Lu <wslzj40@gmail.com>
2021-08-07 16:49:30 +0800

    [INLONG-1421]Update Go SDK Apache License (#1422)
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- tubemq-client-twins/tubemq-client-go/log/log.go
+++ tubemq-client-twins/tubemq-client-go/log/log.go
@@ -1,21 +1,21 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
-// package log defines the logger for the sdk.
+// Package log defines the logger for the sdk.
 package log
 
 var traceEnabled = false
@@ -66,7 +66,7 @@ func Debug(args ...interface{}) {
 	defaultLogger.Debug(args...)
 }
 
-// Debug logs to DEBUG level log. Arguments are handled in the manner of fmt.Printf.
+// Debugf logs to DEBUG level log. Arguments are handled in the manner of fmt.Printf.
 func Debugf(format string, args ...interface{}) {
 	defaultLogger.Debugf(format, args...)
 }
--- tubemq-client-twins/tubemq-client-go/util/util.go
+++ tubemq-client-twins/tubemq-client-go/util/util.go
@@ -1,21 +1,21 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
-// package util defines the constants and helper functions.
+// Package util defines the constants and helper functions.
 package util
 
 import (
@@ -25,7 +25,7 @@ import (
 	"strings"
 )
 
-// InValidValue of TubeMQ config.
+// InvalidValue defines the invalid value of TubeMQ config.
 var InvalidValue = int64(-2)
 
 // GetLocalHost returns the local host name.
@@ -76,7 +76,7 @@ func GenMasterAuthenticateToken(username string, password string) string {
 	return ""
 }
 
-// ParseConfirmContext parses the confirmcontext to partition key and bookedTime.
+// ParseConfirmContext parses the confirm context to partition key and bookedTime.
 func ParseConfirmContext(confirmContext string) (string, int64, error) {
 	res := strings.Split(confirmContext, "@")
 	if len(res) == 0 {
