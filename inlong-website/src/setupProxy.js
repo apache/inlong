2ec32d4ba06b4f076fab1d05f8a025dab03f1073
Daniel <leezng@outlook.com>
2021-08-26 11:48:08 +0800

    [INLONG-1457][website] remove the user limit for creating a new data access #1457 (#1479)

--- inlong-website/src/setupProxy.js
+++ inlong-website/src/setupProxy.js
@@ -20,7 +20,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'http://10.40.24.52:3000';
+const target = 'http://127.0.0.1';
 
 module.exports = function (app) {
   app.use(
