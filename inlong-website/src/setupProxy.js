5f6fd6e563081ec575715959b19f1b6b9d6e48bc
ruanwenjun <wenjun@apache.org>
2021-10-13 22:47:43 +0800

    i[INLONG-1626] Add back end port in setupProxy

--- inlong-website/src/setupProxy.js
+++ inlong-website/src/setupProxy.js
@@ -20,7 +20,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'http://127.0.0.1';
+const target = 'http://127.0.0.1:8083';
 
 module.exports = function (app) {
   app.use(
