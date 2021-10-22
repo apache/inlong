5bd38733684c99a6d7147fcc95ba338b36267ad0
nicobao <nicobao@tencent.com>
2021-10-22 11:18:46 +0800

    modify check style

--- inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/utils/MonitorIndexExt.java
+++ inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/utils/MonitorIndexExt.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.inlong.dataproxy.utils;
 
 import java.util.HashMap;
@@ -7,9 +23,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Created by jesseyzhou on 2017/9/29.
- */
+
 public class MonitorIndexExt {
     private static final Logger logger = LoggerFactory.getLogger(MonitorIndexExt.class);
     private static final LogCounter logPrinter = new LogCounter(10, 100000, 60 * 1000);
