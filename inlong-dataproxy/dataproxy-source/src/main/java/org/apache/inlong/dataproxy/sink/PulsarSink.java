3f952b8395f9227b735b4297730bec88bbf6d0bd
nicobao <nicobao@tencent.com>
2021-10-22 11:06:46 +0800

    modify check style

--- inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/sink/PulsarSink.java
+++ inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/sink/PulsarSink.java
@@ -1,16 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.inlong.dataproxy.sink;
@@ -76,7 +78,8 @@ import org.slf4j.LoggerFactory;
  *  enable_batch: default is true
  *  block_if_queue_full: default is true
  */
-public class PulsarSink extends AbstractSink implements Configurable, SendMessageCallBack, CreatePulsarClientCallBack {
+public class PulsarSink extends AbstractSink implements Configurable,
+        SendMessageCallBack, CreatePulsarClientCallBack {
 
     private static final Logger logger = LoggerFactory.getLogger(PulsarSink.class);
 
--- inlong-dataproxy/dataproxy-source/src/test/java/org/apache/inlong/dataproxy/sink/TestPulsarSink.java
+++ inlong-dataproxy/dataproxy-source/src/test/java/org/apache/inlong/dataproxy/sink/TestPulsarSink.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.inlong.dataproxy.sink;
 
 import com.google.common.base.Charsets;
@@ -21,17 +37,14 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 
 /**
- * description： send to pulsar test
- * @Auther: nicobao
- * @Date: 2021/10/21 16:55
- * @Description:
+ *
  */
 public class TestPulsarSink {
     private static final Logger logger = LoggerFactory
             .getLogger(TestPulsarSink.class);
     private static final String hostname = "127.0.0.1";
-    private static final Integer port = 41414;
-    private String zkStr = "10.196.129.19:2181,10.196.129.20:2181,10.196.129.21:2181";
+    private static final Integer port = 1234;
+    private String zkStr = "127.0.0.1:2181";
     private String zkRoot = "/meta";
     private int batchSize = 1;
 
