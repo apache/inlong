fd9f990eb7cae1f9bf189aecabad37350942c081
leo65535 <xielei_yewu@cmss.chinamobile.com>
2021-11-16 08:22:04 +0800

    [INLONG-1720] Introduce the basic framework for a workflow based on flinksql

---
+++ inlong-sort/sort-sql/sort-sql-core/src/main/java/org/apache/inlog/sort/sql/core/JobExecutor.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlog.sort.sql.core;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.StatementSet;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.api.internal.TableEnvironmentImpl;
+import org.apache.flink.table.operations.CatalogSinkModifyOperation;
+import org.apache.flink.table.operations.Operation;
+import org.apache.inlog.sort.sql.core.util.JobStatementSplitter;
+
+import java.util.List;
+
+public class JobExecutor {
+
+  public static void runJob(JobInfo jobInfo) {
+
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().inStreamingMode().build();
+    StreamTableEnvironment tEnv = StreamTableEnvironment.create(env, fsSettings);
+
+    StatementSet statementSet = handleStatements(jobInfo.getWorkFlowContent(), tEnv);
+    statementSet.execute();
+
+  }
+
+  /**
+   * Handle each statement.
+   */
+  private static StatementSet handleStatements(String workFlowContent, StreamTableEnvironment tEnv) {
+
+    StatementSet statementSet = tEnv.createStatementSet();
+    TableEnvironmentImpl stEnv = (TableEnvironmentImpl) tEnv;
+
+    List<String> stmts = JobStatementSplitter.normalizeStatements(workFlowContent);
+    for (String stmt : stmts) {
+      Operation op = stEnv.getParser().parse(stmt).get(0);
+      if (op instanceof CatalogSinkModifyOperation) {
+        statementSet.addInsertSql(stmt);
+      } else {
+        tEnv.executeSql(stmt);
+      }
+    }
+    return statementSet;
+
+  }
+
+}
---
+++ inlong-sort/sort-sql/sort-sql-examples/src/main/java/org/apache/inlog/sort/sql/examples/BasicDemoExample.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlog.sort.sql.examples;
+
+import org.apache.inlog.sort.sql.core.JobExecutor;
+import org.apache.inlog.sort.sql.core.JobInfo;
+
+public class BasicDemoExample {
+
+  private static final String DEMO_JOB = ""
+      + "CREATE TABLE events (\n"
+      + "  f_type INT,\n"
+      + "  f_uid INT,\n"
+      + "  ts AS localtimestamp,\n"
+      + "  WATERMARK FOR ts AS ts\n"
+      + ") WITH (\n"
+      + "  'connector' = 'datagen',\n"
+      + "  'rows-per-second'='5',\n"
+      + "  'fields.f_type.min'='1',\n"
+      + "  'fields.f_type.max'='5',\n"
+      + "  'fields.f_uid.min'='1',\n"
+      + "  'fields.f_uid.max'='1000'\n"
+      + ");"
+      + "\n"
+      + "CREATE TABLE print_table (\n"
+      + "  type INT,\n"
+      + "  uid INT,\n"
+      + "  lstmt TIMESTAMP\n"
+      + ") WITH (\n"
+      + "  'connector' = 'print',\n"
+      + "  'sink.parallelism' = '2'\n"
+      + ");"
+      + "\n"
+      + "CREATE TABLE print_table2 (\n"
+      + "  type INT,\n"
+      + "  uid INT,\n"
+      + "  lstmt TIMESTAMP\n"
+      + ") WITH (\n"
+      + "  'connector' = 'print',\n"
+      + "  'sink.parallelism' = '2'\n"
+      + ");"
+      + "\n"
+      + "INSERT INTO print_table SELECT * FROM events where f_type = 1;"
+      + "\n"
+      + "INSERT INTO print_table2 SELECT * FROM events where f_type = 3;";
+
+  public static void main(String[] args) throws Exception {
+
+    JobInfo jobInfo = new JobInfo("demo_job", DEMO_JOB);
+    JobExecutor.runJob(jobInfo);
+
+  }
+
+}
