bfd1c14bc1268050fb7f1242ceac03a7de87b622
Yuanhao Ji <tsund@qq.com>
2022-01-04 15:35:57 +0800

    [INLONG-2094][Bug] Add label name of source prometheus metrics
    issue: #2094

--- inlong-agent/agent-plugins/src/main/java/org/apache/inlong/agent/plugin/metrics/SourcePrometheusMetrics.java
+++ inlong-agent/agent-plugins/src/main/java/org/apache/inlong/agent/plugin/metrics/SourcePrometheusMetrics.java
@@ -31,11 +31,13 @@ public class SourcePrometheusMetrics implements SourceMetrics {
     private static final Counter SOURCE_SUCCESS_COUNTER = Counter.build()
             .name(AGENT_SOURCE_METRICS_PREFIX + SOURCE_SUCCESS_COUNTER_NAME)
             .help("The success message count in agent source since agent started.")
+            .labelNames("tag")
             .register();
 
     private static final Counter SOURCE_FAIL_COUNTER = Counter.build()
             .name(AGENT_SOURCE_METRICS_PREFIX + SOURCE_FAIL_COUNTER_NAME)
             .help("The failed message count in agent source since agent started.")
+            .labelNames("tag")
             .register();
 
     public SourcePrometheusMetrics(String tagName) {
