e91c026fde47219a5843a227b90a8bfdb526397b
vernedeng <dengfan@pku.edu.cn>
2022-01-19 14:52:40 +0800

    [INLONG-2161][Feature][Manager] Fix check style problem

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/SortClusterConfigServiceImpl.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/SortClusterConfigServiceImpl.java
@@ -18,9 +18,6 @@
 package org.apache.inlong.manager.service.core.impl;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.Feature;
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.inlong.manager.common.pojo.sort.SortClusterConfigResponse;
