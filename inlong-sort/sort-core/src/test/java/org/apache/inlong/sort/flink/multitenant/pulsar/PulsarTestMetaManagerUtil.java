f23927e5438df680bb244309bd78dfc116644a3f
TianqiWan <52151238+chantccc@users.noreply.github.com>
2022-01-26 16:49:56 +0800

    [INLONG-2331] Introduce sort-connectors module (#2332)
    Co-authored-by: tianqiwan <tianqiwan@tencent.com>

---
+++ inlong-sort/sort-core/src/test/java/org/apache/inlong/sort/flink/multitenant/pulsar/PulsarTestMetaManagerUtil.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.flink.multitenant.pulsar;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.inlong.sort.ZkTools;
+import org.apache.inlong.sort.formats.common.StringFormatInfo;
+import org.apache.inlong.sort.protocol.DataFlowInfo;
+import org.apache.inlong.sort.protocol.FieldInfo;
+import org.apache.inlong.sort.protocol.deserialization.CsvDeserializationInfo;
+import org.apache.inlong.sort.protocol.sink.HiveSinkInfo;
+import org.apache.inlong.sort.protocol.sink.HiveSinkInfo.HivePartitionInfo;
+import org.apache.inlong.sort.protocol.sink.HiveSinkInfo.TextFileFormat;
+import org.apache.inlong.sort.protocol.source.PulsarSourceInfo;
+import org.apache.inlong.sort.util.TestMetaManagerUtil;
+
+public class PulsarTestMetaManagerUtil extends TestMetaManagerUtil {
+
+    public PulsarTestMetaManagerUtil() throws Exception {
+        super();
+    }
+
+    @Override
+    public void addOrUpdateDataFlowInfo(long dataFlowId, String... args) throws Exception {
+        ZkTools.addDataFlowToCluster(cluster, dataFlowId, ZOOKEEPER.getConnectString(), zkRoot);
+        ZkTools.updateDataFlowInfo(
+                prepareDataFlowInfo(dataFlowId, args[0], args[1], args[2], args[3]),
+                cluster,
+                dataFlowId,
+                ZOOKEEPER.getConnectString(),
+                zkRoot);
+    }
+
+    @Override
+    public void initDataFlowInfo(String... args) throws Exception {
+        addOrUpdateDataFlowInfo(1L, args[0], args[1], args[2], args[3]);
+    }
+
+    @Override
+    public DataFlowInfo prepareDataFlowInfo(long dataFlowId, String... args) {
+
+        FieldInfo[] pulsarFields = new FieldInfo[]{
+                new FieldInfo("f1", StringFormatInfo.INSTANCE),
+                new FieldInfo("f2", StringFormatInfo.INSTANCE)
+        };
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("pulsar.source.consumer.bootstrap-mode", "earliest");
+
+        return new DataFlowInfo(
+                dataFlowId,
+                new PulsarSourceInfo(
+                        args[0],
+                        args[1],
+                        args[2],
+                        args[3],
+                        new CsvDeserializationInfo(','),
+                        pulsarFields,
+                        null),
+                new HiveSinkInfo(
+                        new FieldInfo[0],
+                        "testServerJdbcUrl",
+                        "testDatabaseName",
+                        "testTableName",
+                        "testUsername",
+                        "testPassword",
+                        "testDataPath",
+                        new HivePartitionInfo[0],
+                        new TextFileFormat(',')),
+                config
+        );
+    }
+}
---
+++ inlong-sort/sort-core/src/test/java/org/apache/inlong/sort/flink/multitenant/tubemq/TubeSubscriptionDescriptionTest.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.flink.multitenant.tubemq;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import org.apache.inlong.sort.protocol.FieldInfo;
+import org.apache.inlong.sort.protocol.deserialization.InLongMsgCsvDeserializationInfo;
+import org.apache.inlong.sort.protocol.source.TubeSourceInfo;
+import org.apache.inlong.sort.util.TestLogger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link TubeSubscriptionDescription}.
+ */
+public class TubeSubscriptionDescriptionTest extends TestLogger {
+
+    private final String topic = "topic";
+
+    private final String masterAddress = "localhost:9527";
+
+    private final String tid = "10001";
+
+    private final long dataFlowId = 9527L;
+
+    @Test
+    public void testCopyConstructor() {
+        final TubeSubscriptionDescription description = new TubeSubscriptionDescription(topic, masterAddress, null);
+        final TubeSourceInfo tubeSourceInfo = new TubeSourceInfo(
+                topic,
+                masterAddress,
+                null,
+                new InLongMsgCsvDeserializationInfo(tid, ',', true),
+                new FieldInfo[0]);
+        description.addDataFlow(dataFlowId, tubeSourceInfo);
+
+        final TubeSubscriptionDescription copied = new TubeSubscriptionDescription(description);
+        assertEquals(description, copied);
+
+        copied.removeDataFlow(dataFlowId);
+        assertNotEquals(description, copied);
+    }
+
+    @Test
+    public void testAddAndRemoveDataFlow() {
+        final TubeSubscriptionDescription description = new TubeSubscriptionDescription(topic, masterAddress, null);
+        final TubeSourceInfo tubeSourceInfo1 = new TubeSourceInfo(
+                topic,
+                masterAddress,
+                null,
+                new InLongMsgCsvDeserializationInfo(tid, ',', true),
+                new FieldInfo[0]);
+        description.addDataFlow(dataFlowId, tubeSourceInfo1);
+        final String tid2 = "10002";
+        final String consumerGroup = "consumerGroup";
+        final TubeSourceInfo tubeSourceInfo2 = new TubeSourceInfo(
+                topic,
+                masterAddress,
+                consumerGroup,
+                new InLongMsgCsvDeserializationInfo(tid2, ',', true),
+                new FieldInfo[0]);
+        final long dataFlowId2 = 10086L;
+        description.addDataFlow(dataFlowId2, tubeSourceInfo2);
+        final List<String> expectedTids = new ArrayList<>();
+        expectedTids.add(tid);
+        expectedTids.add(tid2);
+        Collections.sort(expectedTids);
+        final List<String> tids = description.getTids();
+        Collections.sort(tids);
+        assertEquals(expectedTids, tids);
+
+        description.removeDataFlow(dataFlowId);
+        assertEquals(Collections.singletonList(tid2), description.getTids());
+
+        description.removeDataFlow(dataFlowId2);
+        try {
+            description.getTids();
+            fail("Should throw exception when get tids from an empty description");
+        } catch (IllegalStateException expected) {
+
+        }
+    }
+}
