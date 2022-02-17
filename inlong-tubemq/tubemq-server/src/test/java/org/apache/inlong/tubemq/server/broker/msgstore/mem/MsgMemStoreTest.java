ae14bc259a8163ff7445f75abf34ea137e19b3ee
gosonzhang <4675739@qq.com>
2022-02-17 11:35:47 +0800

    [INLONG-2538][TubeMQ] Optimize message write cache logic

--- inlong-tubemq/tubemq-server/src/test/java/org/apache/inlong/tubemq/server/broker/msgstore/mem/MsgMemStoreTest.java
+++ inlong-tubemq/tubemq-server/src/test/java/org/apache/inlong/tubemq/server/broker/msgstore/mem/MsgMemStoreTest.java
@@ -19,6 +19,7 @@ package org.apache.inlong.tubemq.server.broker.msgstore.mem;
 
 import java.nio.ByteBuffer;
 import org.apache.inlong.tubemq.server.broker.stats.MsgStoreStatsHolder;
+import org.apache.inlong.tubemq.server.broker.utils.DataStoreUtils;
 import org.apache.inlong.tubemq.server.common.utils.AppendResult;
 import org.junit.Test;
 
@@ -29,30 +30,78 @@ public class MsgMemStoreTest {
 
     @Test
     public void appendMsg() {
-        int maxCacheSize = 2 * 1024 * 1024;
-        int maxMsgCount = 10000;
 
-        MsgMemStore msgMemStore = new MsgMemStore(maxCacheSize, maxMsgCount, null);
-        MsgStoreStatsHolder memStatsHolder = new MsgStoreStatsHolder();
-        ByteBuffer bf = ByteBuffer.allocate(1024);
-        bf.put("abc".getBytes());
+        byte[] testData = "abcabdcdsdsdasdfasdfasdfsadfasdfasdfasdfasdfaaaaaaaaaaa".getBytes();
+        // build data buffer
+        final ByteBuffer dataBuffer =
+                ByteBuffer.allocate(DataStoreUtils.STORE_DATA_HEADER_LEN + testData.length);
+        dataBuffer.putInt(DataStoreUtils.STORE_DATA_PREFX_LEN + testData.length);
+        dataBuffer.putInt(DataStoreUtils.STORE_DATA_TOKER_BEGIN_VALUE);
+        dataBuffer.putInt(33);
+        dataBuffer.putInt(0);
+        dataBuffer.putLong(-1L);
+        dataBuffer.putLong(2222L);
+        dataBuffer.putInt(255555);
+        dataBuffer.putInt(11);
+        dataBuffer.putLong(222L);
+        dataBuffer.putInt(1);
+        dataBuffer.put(testData);
+        dataBuffer.flip();
+        // build index buffer
+        ByteBuffer indexBuffer =
+                ByteBuffer.allocate(DataStoreUtils.STORE_INDEX_HEAD_LEN);
+        indexBuffer.putInt(0);
+        indexBuffer.putLong(-1L);
+        indexBuffer.putInt(3);
+        indexBuffer.putInt(32);
+        indexBuffer.putLong(System.currentTimeMillis());
+        indexBuffer.flip();
         AppendResult appendResult = new AppendResult();
         // append data
+        int maxCacheSize = 2 * 1024 * 1024;
+        int maxMsgCount = 10000;
+        MsgMemStore msgMemStore = new MsgMemStore(maxCacheSize, maxMsgCount, null);
+        msgMemStore.resetStartPos(0, 0);
+        MsgStoreStatsHolder memStatsHolder = new MsgStoreStatsHolder();
         msgMemStore.appendMsg(memStatsHolder, 0, 0,
-                System.currentTimeMillis(), 3, bf, appendResult);
+                System.currentTimeMillis(), indexBuffer, 3, dataBuffer, appendResult);
     }
 
     @Test
     public void getMessages() {
+        byte[] testData = "abcabdcdsdsdasdfasdfasdfsadfasdfasdfasdfasdfaaaaaaaaaaa".getBytes();
+        // build data buffer
+        final ByteBuffer dataBuffer =
+                ByteBuffer.allocate(DataStoreUtils.STORE_DATA_HEADER_LEN + testData.length);
+        dataBuffer.putInt(DataStoreUtils.STORE_DATA_PREFX_LEN + testData.length);
+        dataBuffer.putInt(DataStoreUtils.STORE_DATA_TOKER_BEGIN_VALUE);
+        dataBuffer.putInt(33);
+        dataBuffer.putInt(0);
+        dataBuffer.putLong(-1L);
+        dataBuffer.putLong(2222L);
+        dataBuffer.putInt(255555);
+        dataBuffer.putInt(11);
+        dataBuffer.putLong(222L);
+        dataBuffer.putInt(1);
+        dataBuffer.put(testData);
+        dataBuffer.flip();
+        // build index buffer
+        ByteBuffer indexBuffer =
+                ByteBuffer.allocate(DataStoreUtils.STORE_INDEX_HEAD_LEN);
+        indexBuffer.putInt(0);
+        indexBuffer.putLong(-1L);
+        indexBuffer.putInt(3);
+        indexBuffer.putInt(32);
+        indexBuffer.putLong(System.currentTimeMillis());
+        indexBuffer.flip();
+        AppendResult appendResult = new AppendResult();
         int maxCacheSize = 2 * 1024 * 1024;
         int maxMsgCount = 10000;
         MsgMemStore msgMemStore = new MsgMemStore(maxCacheSize, maxMsgCount, null);
+        msgMemStore.resetStartPos(0, 0);
         MsgStoreStatsHolder memStatsHolder = new MsgStoreStatsHolder();
-        ByteBuffer bf = ByteBuffer.allocate(1024);
-        bf.put("abc".getBytes());
-        AppendResult appendResult = new AppendResult();
         msgMemStore.appendMsg(memStatsHolder, 0, 0,
-                System.currentTimeMillis(), 3, bf, appendResult);
+                System.currentTimeMillis(), indexBuffer, 3, dataBuffer, appendResult);
         // get messages
         GetCacheMsgResult getCacheMsgResult = msgMemStore.getMessages(0, 2, 1024, 1000, 0, false, false, null, 0);
     }
