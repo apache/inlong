0451738536d653d73944f699276ad12f7b88ba60
Kevin Wen <kevinswork007@gmail.com>
2022-02-22 19:40:06 +0800

    [INLONG-2614][Sort] Support array and map data structures in Hive sink and ClickHouse sink (#2615)

--- inlong-sort/sort-connectors/src/main/java/org/apache/inlong/sort/flink/hive/formats/parquet/ParquetRowWriter.java
+++ inlong-sort/sort-connectors/src/main/java/org/apache/inlong/sort/flink/hive/formats/parquet/ParquetRowWriter.java
@@ -20,7 +20,11 @@ package org.apache.inlong.sort.flink.hive.formats.parquet;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Map;
+
+import org.apache.flink.table.types.logical.ArrayType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.MapType;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.types.Row;
 import org.apache.parquet.io.api.Binary;
@@ -30,6 +34,12 @@ import org.apache.parquet.schema.Type;
 
 /** Writes a record to the Parquet API with the expected schema in order to be written to a file. */
 public class ParquetRowWriter {
+
+    static final String ARRAY_FIELD_NAME = "list";
+
+    static final String MAP_ENTITY_FIELD_NAME = "key_value";
+    static final String MAP_KEY_FIELD_NAME = "key";
+    static final String MAP_VALUE_FIELD_NAME = "value";
 
     private final RecordConsumer recordConsumer;
 
@@ -104,7 +114,15 @@ public class ParquetRowWriter {
                     throw new UnsupportedOperationException("Unsupported type: " + type);
             }
         } else {
-            throw new IllegalArgumentException("Unsupported  data type: " + t);
+            switch (t.getTypeRoot()) {
+                case ARRAY:
+                    return new ArrayWriter(((ArrayType) t).getElementType(),
+                            type.asGroupType().getType(0).asGroupType().getType(0));
+                case MAP:
+                    return new MapWriter((MapType) t, type);
+                default:
+                    throw new UnsupportedOperationException("Unsupported type: " + type);
+            }
         }
     }
 
@@ -183,5 +201,279 @@ public class ParquetRowWriter {
         public void write(Row row, int ordinal) {
             recordConsumer.addBinary(Binary.fromReusedByteArray((byte[]) row.getField(ordinal)));
         }
+    }
+
+    private class ArrayWriter implements FieldWriter {
+
+        private final LogicalType elementTypeFlink;
+
+        private final Type elementTypeParquet;
+
+        public ArrayWriter(LogicalType elementTypeFlink, Type elementTypeParquet) {
+            this.elementTypeFlink = elementTypeFlink;
+            this.elementTypeParquet = elementTypeParquet;
+        }
+
+        @Override
+        public void write(Row row, int ordinal) {
+            if (elementTypeParquet.isPrimitive()) {
+                switch (elementTypeFlink.getTypeRoot()) {
+                    case CHAR:
+                    case VARCHAR:
+                    case DECIMAL:
+                    case DATE:
+                    case TIME_WITHOUT_TIME_ZONE:
+                    case TIMESTAMP_WITHOUT_TIME_ZONE:
+                        writeObjectArray(row.getField(ordinal));
+                        break;
+                    case BOOLEAN:
+                        writeBooleanArray(row.getField(ordinal));
+                        break;
+                    case TINYINT:
+                        writeTinyIntArray(row.getField(ordinal));
+                        break;
+                    case SMALLINT:
+                        writeShortArray(row.getField(ordinal));
+                        break;
+                    case INTEGER:
+                        writeIntArray(row.getField(ordinal));
+                        break;
+                    case BIGINT:
+                        writeLongArray(row.getField(ordinal));
+                        break;
+                    case FLOAT:
+                        writeFloatArray(row.getField(ordinal));
+                        break;
+                    case DOUBLE:
+                        writeDoubleArray(row.getField(ordinal));
+                        break;
+                    default:
+                        throw new UnsupportedOperationException(
+                                "Unsupported element type in array: " + elementTypeParquet);
+                }
+            } else {
+                throw new UnsupportedOperationException("Unsupported element type in array: " + elementTypeParquet);
+            }
+        }
+
+        private void writeObjectArray(Object input) {
+            recordConsumer.startGroup();
+            if (input != null) {
+                Object[] inputArray = (Object[]) input;
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (Object ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+            }
+            recordConsumer.endGroup();
+        }
+
+        private void writeBooleanArray(Object input) {
+            if (input instanceof boolean[]) {
+                boolean[] inputArray = (boolean[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (boolean ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeTinyIntArray(Object input) {
+            if (input instanceof byte[]) {
+                byte[] inputArray = (byte[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (byte ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeShortArray(Object input) {
+            if (input instanceof short[]) {
+                short[] inputArray = (short[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (short ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeIntArray(Object input) {
+            if (input instanceof int[]) {
+                int[] inputArray = (int[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (int ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeLongArray(Object input) {
+            if (input instanceof long[]) {
+                long[] inputArray = (long[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (long ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeFloatArray(Object input) {
+            if (input instanceof float[]) {
+                float[] inputArray = (float[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (float ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+        private void writeDoubleArray(Object input) {
+            if (input instanceof double[]) {
+                double[] inputArray = (double[]) input;
+                recordConsumer.startGroup();
+                if (inputArray.length > 0) {
+                    recordConsumer.startField(ARRAY_FIELD_NAME, 0);
+                    for (double ele : inputArray) {
+                        startGroupAndField(elementTypeParquet.getName());
+                        FieldWriter writer = createWriter(elementTypeFlink, elementTypeParquet);
+                        writer.write(Row.of(ele), 0);
+                        endGroupAndField(elementTypeParquet.getName());
+                    }
+                    recordConsumer.endField(ARRAY_FIELD_NAME, 0);
+                }
+                recordConsumer.endGroup();
+            } else {
+                writeObjectArray(input);
+            }
+        }
+
+    }
+
+    private class MapWriter implements FieldWriter {
+
+        private final LogicalType keyTypeFlink;
+
+        private final LogicalType valueTypeFlink;
+
+        private final Type keyTypeParquet;
+
+        private final Type valueTypeParquet;
+
+        public MapWriter(MapType mapTypeFlink, Type mapTypeParquet) {
+            this.keyTypeFlink = mapTypeFlink.getKeyType();
+            this.valueTypeFlink = mapTypeFlink.getValueType();
+            GroupType groupType = mapTypeParquet.asGroupType().getType(0).asGroupType();
+            this.keyTypeParquet = groupType.getType(0);
+            this.valueTypeParquet = groupType.getType(1);
+        }
+
+        @Override
+        public void write(Row row, int ordinal) {
+            recordConsumer.startGroup();
+            Object inputField = row.getField(ordinal);
+            if (inputField != null) {
+                Map<?, ?> inputMap = (Map<?, ?>) inputField;
+                if (inputMap.size() > 0) {
+                    FieldWriter keyWriter = createWriter(keyTypeFlink, keyTypeParquet);
+                    FieldWriter valueWriter = createWriter(valueTypeFlink, valueTypeParquet);
+                    recordConsumer.startField(MAP_ENTITY_FIELD_NAME, 0);
+                    for (Map.Entry<?, ?> entry : inputMap.entrySet()) {
+                        recordConsumer.startGroup();
+
+                        recordConsumer.startField(MAP_KEY_FIELD_NAME, 0);
+                        keyWriter.write(Row.of(entry.getKey()), 0);
+                        recordConsumer.endField(MAP_KEY_FIELD_NAME,0);
+
+                        Object value = entry.getValue();
+                        if (value != null) {
+                            recordConsumer.startField(MAP_VALUE_FIELD_NAME, 1);
+                            valueWriter.write(Row.of(value), 0);
+                            recordConsumer.endField(MAP_VALUE_FIELD_NAME, 1);
+                        }
+
+                        recordConsumer.endGroup();
+                    }
+                    recordConsumer.endField(MAP_ENTITY_FIELD_NAME, 0);
+                }
+            }
+            recordConsumer.endGroup();
+        }
+    }
+
+    private void startGroupAndField(String fieldName) {
+        recordConsumer.startGroup();
+        recordConsumer.startField(fieldName, 0);
+    }
+
+    private void endGroupAndField(String fieldName) {
+        recordConsumer.endField(fieldName, 0);
+        recordConsumer.endGroup();
     }
 }
