0d9752e2c5f376060166137b53e780b804f06832
wardli <nblcan@163.com>
2022-02-23 10:58:51 +0800

    [INLONG-2382][Sort-sdk] add UT
    [INLONG-2382][Sort-sdk] add UT

--- inlong-sdk/sort-sdk/src/test/java/org/apache/inlong/sdk/sort/impl/InLongTopicManagerImplTest.java
+++ inlong-sdk/sort-sdk/src/test/java/org/apache/inlong/sdk/sort/impl/InLongTopicManagerImplTest.java
@@ -34,12 +34,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.management.*")
 public class InLongTopicManagerImplTest {
 
     private InLongTopic inLongTopic;
