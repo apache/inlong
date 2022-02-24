e4d91bf0d380141709bcfe8ab2a7a989b81c9882
kipshi <48468934+kipshi@users.noreply.github.com>
2022-02-24 20:20:09 +0800

    [INLONG-2690][manager] Optimize group state in workflow (#2691)

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/InlongGroupServiceImpl.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/InlongGroupServiceImpl.java
@@ -431,9 +431,9 @@ public class InlongGroupServiceImpl implements InlongGroupService {
         String middlewareType = approveInfo.getMiddlewareType();
         Preconditions.checkNotNull(middlewareType, "Middleware type is empty");
 
-        // Update status to [GROUP_CONFIG_ING]
+        // Update status to [GROUP_APPROVE_PASSED]
         // If you need to change inlong group info after approve, just do in here
-        this.updateStatus(groupId, GroupState.GROUP_CONFIG_ING.getCode(), operator);
+        this.updateStatus(groupId, GroupState.GROUP_APPROVE_PASSED.getCode(), operator);
 
         LOGGER.info("success to update inlong group status after approve for groupId={}", groupId);
         return true;
