1cd4397b3cae3084814c0c410750bf8961249ceb
ciscozhou <45899072+ciscozhou@users.noreply.github.com>
2022-02-26 14:11:08 +0800

    [INLONG-2732][Manager] Support more parameters for Kafka source (#2739)

--- inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/source/kafka/KafkaSourceRequest.java
+++ inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/source/kafka/KafkaSourceRequest.java
@@ -36,10 +36,25 @@ import org.apache.inlong.manager.common.util.JsonTypeDefine;
 @JsonTypeDefine(value = Constant.SOURCE_KAFKA)
 public class KafkaSourceRequest extends SourceRequest {
 
-    @ApiModelProperty("Kafka bootstrap servers")
-    private String address;
+    @ApiModelProperty("Kafka topic")
+    private String topic;
 
-    @ApiModelProperty("Kafka topicName")
-    private String topicName;
+    @ApiModelProperty("Kafka consumer group")
+    private String groupId;
+
+    @ApiModelProperty("Kafka servers address, such as: 127.0.0.1:9092")
+    private String bootstrapServers;
+
+    @ApiModelProperty(value = "Limit the amount of data read per second",
+            notes = "Greater than or equal to 0, equal to zero means no limit")
+    private String recordSpeedLimit;
+
+    @ApiModelProperty(value = "Limit the number of bytes read per second",
+            notes = "Greater than or equal to 0, equal to zero means no limit")
+    private String byteSpeedLimit;
+
+    @ApiModelProperty(value = "Topic partition offset",
+            notes = "For example, '0#100_1#10' means the offset of partition 0 is 100, the offset of partition 1 is 10")
+    private String topicPartitionOffset;
 
 }
