2ae9d48efb406fc03b1362a705269c4f761d641f
kipshi <kipshi@tencent.com>
2022-03-05 10:42:53 +0800

    Fix NPE in manager client

--- inlong-manager/manager-client/src/main/java/org/apache/inlong/manager/client/api/impl/InlongGroupImpl.java
+++ inlong-manager/manager-client/src/main/java/org/apache/inlong/manager/client/api/impl/InlongGroupImpl.java
@@ -17,11 +17,9 @@
 
 package org.apache.inlong.manager.client.api.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.inlong.manager.client.api.InlongGroup;
 import org.apache.inlong.manager.client.api.InlongGroupConf;
@@ -36,14 +34,12 @@ import org.apache.inlong.manager.client.api.util.AssertUtil;
 import org.apache.inlong.manager.client.api.util.GsonUtil;
 import org.apache.inlong.manager.client.api.util.InlongGroupTransfer;
 import org.apache.inlong.manager.client.api.util.InlongParser;
-import org.apache.inlong.manager.client.api.util.InlongStreamSourceTransfer;
 import org.apache.inlong.manager.common.enums.GroupState;
 import org.apache.inlong.manager.common.enums.ProcessStatus;
 import org.apache.inlong.manager.common.pojo.group.InlongGroupApproveRequest;
 import org.apache.inlong.manager.common.pojo.group.InlongGroupInfo;
 import org.apache.inlong.manager.common.pojo.group.InlongGroupRequest;
 import org.apache.inlong.manager.common.pojo.group.InlongGroupResponse;
-import org.apache.inlong.manager.common.pojo.source.SourceListResponse;
 import org.apache.inlong.manager.common.pojo.stream.FullStreamResponse;
 import org.apache.inlong.manager.common.pojo.stream.InlongStreamApproveRequest;
 import org.apache.inlong.manager.common.pojo.workflow.EventLogView;
@@ -206,23 +202,8 @@ public class InlongGroupImpl implements InlongGroup {
 
     private List<InlongStream> fetchDataStreams(String groupId) {
         List<FullStreamResponse> streamResponses = managerClient.listStreamInfo(groupId);
-        List<InlongStream> streamList = new ArrayList<>();
-        if (CollectionUtils.isNotEmpty(streamResponses)) {
-            streamList = streamResponses.stream().map(fullStreamResponse -> {
-                List<SourceListResponse> sourceListResponses = managerClient.listSources(groupId,
-                        fullStreamResponse.getStreamInfo().getInlongStreamId());
-                String streamName = fullStreamResponse.getStreamInfo().getName();
-                InlongStream stream = groupContext.getStream(streamName);
-                InlongStreamImpl inlongStream = new InlongStreamImpl(fullStreamResponse, stream);
-                if (CollectionUtils.isNotEmpty(sourceListResponses)) {
-                    for (SourceListResponse response : sourceListResponses) {
-                        inlongStream.updateSource(
-                                InlongStreamSourceTransfer.parseStreamSource(response));
-                    }
-                }
-                return inlongStream;
-            }).collect(Collectors.toList());
-        }
+        List<InlongStream> streamList = streamResponses.stream()
+                .map(fullStreamResponse -> new InlongStreamImpl(fullStreamResponse)).collect(Collectors.toList());
         return streamList;
     }
 }
