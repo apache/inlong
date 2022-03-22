833365c773c41a6cd0cd393745be7316ea136ed6
gong <genzhedangdang@gmail.com>
2022-03-22 17:10:51 +0800

    [INLONG-3294][manager]modify UT

--- inlong-manager/manager-client/src/test/java/org/apache/inlong/manager/client/api/impl/InnerInlongManagerClientTest.java
+++ inlong-manager/manager-client/src/test/java/org/apache/inlong/manager/client/api/impl/InnerInlongManagerClientTest.java
@@ -30,20 +30,15 @@ import java.util.List;
 @Slf4j
 public class InnerInlongManagerClientTest {
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void testListStreamInfo() {
         String serviceUrl = "127.0.0.1:8083";
         ClientConfiguration configuration = new ClientConfiguration();
         configuration.setAuthentication(new DefaultAuthentication("admin", "inlong"));
         InlongClientImpl inlongClient = new InlongClientImpl(serviceUrl, configuration);
         InnerInlongManagerClient innerInlongManagerClient = new InnerInlongManagerClient(inlongClient);
-        List<FullStreamResponse> fullStreamResponseList = null;
-        try {
-            innerInlongManagerClient.listStreamInfo("test");
-        } catch (RuntimeException e) {
-            log.error("exception:", e);
-            Assert.assertNull(fullStreamResponseList);
-        }
+        List<FullStreamResponse> fullStreamResponseList = innerInlongManagerClient.listStreamInfo("test");
+        Assert.assertNull(fullStreamResponseList);
     }
 
 }
