43633fbe4821ab4660f59079488d10e23aeb06db
kipshi <kipshi@tencent.com>
2022-04-20 15:18:41 +0800

    remove unused import

--- inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/transform/TransformResponse.java
+++ inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/transform/TransformResponse.java
@@ -21,8 +21,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * Response of transform
  */
