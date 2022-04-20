5bd8380e28b0a60c9d1b95b475d24bbe3eb156a4
yunqingmo <yunqingmo@tencent.com>
2022-04-20 19:26:19 +0800

    Add Node interface and derived interface definitions to support transform

---
+++ inlong-sort/sort-common/src/test/java/org/apache/inlong/sort/protocol/node/NodeBaseTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.protocol.node;
+
+import com.google.common.base.Preconditions;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public abstract class NodeBaseTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private String expectSerializeStr;
+    private Node node;
+
+    public abstract Node getNode();
+
+    public abstract String getExpectSerializeStr();
+
+    @Before
+    public void init() {
+        this.node = Preconditions.checkNotNull(getNode());
+        this.expectSerializeStr = Preconditions.checkNotNull(getExpectSerializeStr());
+    }
+
+    @Test
+    public void testSerialize() throws JsonProcessingException {
+        assertEquals(expectSerializeStr, objectMapper.writeValueAsString(node));
+    }
+
+    @Test
+    public void testDeserialize() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Node expected = objectMapper.readValue(expectSerializeStr, node.getClass());
+        assertEquals(expected, node);
+    }
+
+}
