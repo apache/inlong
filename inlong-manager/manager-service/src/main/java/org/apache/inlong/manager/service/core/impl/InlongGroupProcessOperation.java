29b819488416c16b5add4f9229e4e8f5ea5a9328
kipshi <kipshi@tencent.com>
2022-04-27 21:10:39 +0800

    same with last commit

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/InlongGroupProcessOperation.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/core/impl/InlongGroupProcessOperation.java
@@ -237,7 +237,7 @@ public class InlongGroupProcessOperation {
             case LIGHT:
                 LightGroupResourceProcessForm lightForm = genLightGroupProcessForm(groupInfo,
                         GroupOperateType.DELETE);
-                workflowService.start(ProcessName.DELETE_GROUP_PROCESS, operator, lightForm);
+                workflowService.start(ProcessName.DELETE_LIGHT_GROUP_PROCESS, operator, lightForm);
                 break;
             default:
                 throw new WorkflowListenerException(ErrorCodeEnum.GROUP_MODE_UNSUPPORTED.getMessage());
