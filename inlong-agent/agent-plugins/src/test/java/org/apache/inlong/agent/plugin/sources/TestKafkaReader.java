15ad50dfc23d0ffee8eb192e2b2cd8001b655960
healzhou <healchow@gmail.com>
2022-06-10 14:33:09 +0800

    Update TestKafkaReader.java

--- inlong-agent/agent-plugins/src/test/java/org/apache/inlong/agent/plugin/sources/TestKafkaReader.java
+++ inlong-agent/agent-plugins/src/test/java/org/apache/inlong/agent/plugin/sources/TestKafkaReader.java
@@ -17,15 +17,6 @@
 
 package org.apache.inlong.agent.plugin.sources;
 
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import org.apache.inlong.agent.conf.JobProfile;
-import org.apache.inlong.agent.plugin.Message;
-import org.apache.inlong.agent.plugin.Reader;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.MockConsumer;
@@ -33,67 +24,30 @@ import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.common.TopicPartition;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class TestKafkaReader {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestKafkaReader.class);
-
     @Test
     public void testKafkaConsumerInit() {
-        MockConsumer mockConsumer = new MockConsumer(OffsetResetStrategy.EARLIEST);
+        MockConsumer<String, String> mockConsumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
         final String topic = "my_topic";
 
-        mockConsumer.assign(Arrays.asList(new TopicPartition(topic, 0)));
+        mockConsumer.assign(Collections.singletonList(new TopicPartition(topic, 0)));
         HashMap<TopicPartition, Long> beginningOffsets = new HashMap<>();
         beginningOffsets.put(new TopicPartition(topic, 0), 0L);
         mockConsumer.updateBeginningOffsets(beginningOffsets);
 
-        mockConsumer.addRecord(new ConsumerRecord<>(topic,
-                0, 0L, "mykey", "myvalue0"));
+        mockConsumer.addRecord(new ConsumerRecord<>(topic, 0, 0L, "test_key", "test_value"));
         ConsumerRecords<String, String> records = mockConsumer.poll(Duration.ofMillis(1000));
-        Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
-        while (iterator.hasNext()) {
-            ConsumerRecord<String, String> record = iterator.next();
+        for (ConsumerRecord<String, String> record : records) {
             byte[] recordValue = record.value().getBytes(StandardCharsets.UTF_8);
-            Assert.assertArrayEquals("myvalue0".getBytes(StandardCharsets.UTF_8), recordValue);
+            Assert.assertArrayEquals("test_value".getBytes(StandardCharsets.UTF_8), recordValue);
         }
     }
 
-//    @Test
-    public void testKafkaReader() {
-        KafkaSource kafkaSource = new KafkaSource();
-        JobProfile conf = JobProfile.parseJsonStr("{}");
-        conf.set("job.kafkaJob.topic", "test3");
-        conf.set("job.kafkaJob.bootstrap.servers", "127.0.0.1:9092");
-        conf.set("job.kafkaJob.group.id", "test_group1");
-        conf.set("job.kafkaJob.recordspeed.limit", "1");
-        conf.set("job.kafkaJob.bytespeed.limit", "1");
-        conf.set("job.kafkaJob.partition.offset", "0#0");
-        conf.set("job.kafkaJob.autoOffsetReset", "latest");
-        conf.set("proxy.inlongGroupId", "");
-        conf.set("proxy.inlongStreamId", "");
-
-        try {
-            List<Reader> readers = kafkaSource.split(conf);
-            LOGGER.info("total readers by split after:{}", readers.size());
-            readers.forEach(reader -> {
-                reader.init(conf);
-                Runnable runnable = () -> {
-                    while (!reader.isFinished()) {
-                        Message msg = reader.read();
-                        if (msg != null) {
-                            LOGGER.info(new String(msg.getBody()));
-                        }
-                    }
-                    LOGGER.info("reader is finished!");
-                };
-                // start thread
-                new Thread(runnable).start();
-            });
-        } catch (Exception e) {
-            LOGGER.error("get record failed:", e);
-        }
-    }
 }
