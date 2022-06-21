5d608140edb6c7c12d7781848b081e5b459ae7ae
healzhou <healchow@gmail.com>
2022-06-21 17:00:29 +0800

    Update MetaField.java

--- inlong-common/src/main/java/org/apache/inlong/common/enums/MetaField.java
+++ inlong-common/src/main/java/org/apache/inlong/common/enums/MetaField.java
@@ -27,61 +27,75 @@ public enum MetaField {
      * The process time of flink
      */
     PROCESS_TIME,
+
     /**
      * Name of the schema that contain the row, currently used for Oracle, PostgreSQL, SQLSERVER
      */
     SCHEMA_NAME,
+
     /**
      * Name of the database that contain the row.
      */
     DATABASE_NAME,
+
     /**
      * Name of the table that contain the row.
      */
     TABLE_NAME,
+
     /**
      * It indicates the time that the change was made in the database.
      * If the record is read from snapshot of the table instead of the change stream, the value is always 0
      */
     OP_TS,
+
     /**
      * Whether the DDL statement. Currently, it is used for MySQL database.
      */
     IS_DDL,
+
     /**
      * Type of database operation, such as INSERT/DELETE, etc. Currently, it is used for MySQL database.
      */
     OP_TYPE,
+
     /**
      * MySQL binlog data Row. Currently, it is used for MySQL database.
      */
     DATA,
+
     /**
      * The value of the field before update. Currently, it is used for MySQL database.
      */
     UPDATE_BEFORE,
+
     /**
      * Batch id of binlog. Currently, it is used for MySQL database.
      */
     BATCH_ID,
+
     /**
      * Mapping of sql_type table fields to java data type IDs. Currently, it is used for MySQL database.
      */
     SQL_TYPE,
+
     /**
      * The current time when the ROW was received and processed. Currently, it is used for MySQL database.
      */
     TS,
+
     /**
      * The table structure. It is only used for MySQL database
      */
     MYSQL_TYPE,
+
     /**
      * Primary key field name. Currently, it is used for MySQL database.
      */
     PK_NAMES,
+
     /**
-     * Name of the collection that contain the row. For MongoDB
+     * Name of the collection that contain the row, it is only used for MongoDB.
      */
     COLLECTION_NAME;
 
@@ -91,6 +105,6 @@ public enum MetaField {
                 return metaField;
             }
         }
-        throw new UnsupportedOperationException(String.format("Unsupported MetaField=%s for Inlong", name));
+        throw new UnsupportedOperationException(String.format("Unsupported MetaField=%s", name));
     }
 }
