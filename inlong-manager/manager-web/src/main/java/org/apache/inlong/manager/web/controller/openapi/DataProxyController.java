fe5ada98545d7b1143d1c89601e4f140fd21670f
woofyzhao <490467089@qq.com>
2022-06-25 12:18:48 +0800

    [INLONG-4745][Manager] Support list dataproxy nodes by extTag (#4757)

--- inlong-manager/manager-web/src/main/java/org/apache/inlong/manager/web/controller/openapi/DataProxyController.java
+++ inlong-manager/manager-web/src/main/java/org/apache/inlong/manager/web/controller/openapi/DataProxyController.java
@@ -24,12 +24,14 @@ import io.swagger.annotations.ApiOperation;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.inlong.common.pojo.dataproxy.DataProxyConfig;
 import org.apache.inlong.manager.common.beans.Response;
+import org.apache.inlong.manager.common.pojo.cluster.InlongClusterPageRequest;
 import org.apache.inlong.manager.common.pojo.dataproxy.DataProxyNodeInfo;
 import org.apache.inlong.manager.service.cluster.InlongClusterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,22 +47,11 @@ public class DataProxyController {
 
     @Autowired
     private InlongClusterService clusterService;
-
-    /**
-     * Support GET and POST methods,
-     * POST is used for DataProxy requests,
-     * GET is used for quick lookup of IP lists (e.g. via browser requests).
-     */
-    @RequestMapping(value = "/dataproxy/getIpList", method = {RequestMethod.GET, RequestMethod.POST})
-    @ApiOperation(value = "Get data proxy ip list by cluster name")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "clusterTag", value = "cluster tag", dataTypeClass = String.class),
-            @ApiImplicitParam(name = "clusterName", value = "cluster name", dataTypeClass = String.class)
-    })
-    public Response<List<DataProxyNodeInfo>> getIpList(
-            @RequestParam(required = false) String clusterTag,
-            @RequestParam(required = false) String clusterName) {
-        return Response.success(clusterService.getDataProxyNodeList(clusterTag, clusterName));
+    
+    @PostMapping(value = "/dataproxy/getIpList")
+    @ApiOperation(value = "Get data proxy ip list by cluster name and tag")
+    public Response<List<DataProxyNodeInfo>> getIpList(@RequestBody InlongClusterPageRequest request) {
+        return Response.success(clusterService.getDataProxyNodeList(request));
     }
 
     @GetMapping("/dataproxy/getConfig")
