115f0bf1eadff5286aeb2d9a368997d93996bc99
Lizhen <88174078+bluewang@users.noreply.github.com>
2022-07-06 18:07:51 +0800

    [INLONG-4885][Dashboard] Support Greenplum Sink (#4887)
    * [INLONG-4885][Dashboard] Support Greenplum Sink
    
    * make some changes
    
    * make some changes
    
    * make some changes

--- inlong-dashboard/src/components/MetaData/index.ts
+++ inlong-dashboard/src/components/MetaData/index.ts
@@ -24,6 +24,7 @@ import { StorageClickhouse } from './StorageClickhouse';
 import { StorageKafka } from './StorageKafka';
 import { StorageIceberg } from './StorageIceberg';
 import { StorageEs } from './StorageEs';
+import { StorageGreenplum } from './StorageGreenplum';
 
 export interface StoragesType {
   label: string;
@@ -65,5 +66,10 @@ export const Storages: StoragesType[] = [
     label: 'Elasticsearch',
     value: 'ELASTICSEARCH',
     ...StorageEs,
+  },
+  {
+    label: 'Greenplum',
+    value: 'GREENPLUM',
+    ...StorageGreenplum,
   },
 ];
