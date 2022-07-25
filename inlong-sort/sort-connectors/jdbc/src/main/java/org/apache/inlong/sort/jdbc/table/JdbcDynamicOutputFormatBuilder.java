70d90bbac2d4518c3f91450808de955a9ec08d72
onealliu <onealliu@tencent.com>
2022-07-25 20:57:15 +0800

    format

--- inlong-sort/sort-connectors/jdbc/src/main/java/org/apache/inlong/sort/jdbc/table/JdbcDynamicOutputFormatBuilder.java
+++ inlong-sort/sort-connectors/jdbc/src/main/java/org/apache/inlong/sort/jdbc/table/JdbcDynamicOutputFormatBuilder.java
@@ -23,7 +23,6 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.connector.jdbc.dialect.JdbcDialect;
-import org.apache.inlong.sort.jdbc.internal.JdbcBatchingOutputFormat;
 import org.apache.flink.connector.jdbc.internal.connection.SimpleJdbcConnectionProvider;
 import org.apache.flink.connector.jdbc.internal.converter.JdbcRowConverter;
 import org.apache.flink.connector.jdbc.internal.executor.JdbcBatchStatementExecutor;
@@ -39,6 +38,7 @@ import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.RowType;
+import org.apache.inlong.sort.jdbc.internal.JdbcBatchingOutputFormat;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -68,85 +68,6 @@ public class JdbcDynamicOutputFormatBuilder implements Serializable {
 
     public JdbcDynamicOutputFormatBuilder() {
 
-    }
-
-    public JdbcDynamicOutputFormatBuilder setAppendMode(boolean appendMode) {
-        this.appendMode = appendMode;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setJdbcOptions(JdbcOptions jdbcOptions) {
-        this.jdbcOptions = jdbcOptions;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setJdbcExecutionOptions(
-            JdbcExecutionOptions executionOptions) {
-        this.executionOptions = executionOptions;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setJdbcDmlOptions(JdbcDmlOptions dmlOptions) {
-        this.dmlOptions = dmlOptions;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setRowDataTypeInfo(
-            TypeInformation<RowData> rowDataTypeInfo) {
-        this.rowDataTypeInformation = rowDataTypeInfo;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setFieldDataTypes(DataType[] fieldDataTypes) {
-        this.fieldDataTypes = fieldDataTypes;
-        return this;
-    }
-
-    public JdbcDynamicOutputFormatBuilder setInLongMetric(String inLongMetric) {
-        this.inLongMetric = inLongMetric;
-        return this;
-    }
-
-    public JdbcBatchingOutputFormat<RowData, ?, ?> build() {
-        checkNotNull(jdbcOptions, "jdbc options can not be null");
-        checkNotNull(dmlOptions, "jdbc dml options can not be null");
-        checkNotNull(executionOptions, "jdbc execution options can not be null");
-
-        final LogicalType[] logicalTypes =
-                Arrays.stream(fieldDataTypes)
-                        .map(DataType::getLogicalType)
-                        .toArray(LogicalType[]::new);
-        if (dmlOptions.getKeyFields().isPresent() && dmlOptions.getKeyFields().get().length > 0 && !appendMode) {
-            // upsert query
-            return new JdbcBatchingOutputFormat<>(
-                    new SimpleJdbcConnectionProvider(jdbcOptions),
-                    executionOptions,
-                    ctx ->
-                            createBufferReduceExecutor(
-                                    dmlOptions, ctx, rowDataTypeInformation, logicalTypes),
-                    JdbcBatchingOutputFormat.RecordExtractor.identity(),
-                    inLongMetric);
-        } else {
-            // append only query
-            final String sql =
-                    dmlOptions
-                            .getDialect()
-                            .getInsertIntoStatement(
-                                    dmlOptions.getTableName(), dmlOptions.getFieldNames());
-            return new JdbcBatchingOutputFormat<>(
-                    new SimpleJdbcConnectionProvider(jdbcOptions),
-                    executionOptions,
-                    ctx ->
-                            createSimpleBufferedExecutor(
-                                    ctx,
-                                    dmlOptions.getDialect(),
-                                    dmlOptions.getFieldNames(),
-                                    logicalTypes,
-                                    sql,
-                                    rowDataTypeInformation),
-                    JdbcBatchingOutputFormat.RecordExtractor.identity(),
-                    inLongMetric);
-        }
     }
 
     private static JdbcBatchStatementExecutor<RowData> createBufferReduceExecutor(
@@ -280,5 +201,84 @@ public class JdbcDynamicOutputFormatBuilder implements Serializable {
             pkRow.setField(i, fieldGetters[i].getFieldOrNull(row));
         }
         return pkRow;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setAppendMode(boolean appendMode) {
+        this.appendMode = appendMode;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setJdbcOptions(JdbcOptions jdbcOptions) {
+        this.jdbcOptions = jdbcOptions;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setJdbcExecutionOptions(
+            JdbcExecutionOptions executionOptions) {
+        this.executionOptions = executionOptions;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setJdbcDmlOptions(JdbcDmlOptions dmlOptions) {
+        this.dmlOptions = dmlOptions;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setRowDataTypeInfo(
+            TypeInformation<RowData> rowDataTypeInfo) {
+        this.rowDataTypeInformation = rowDataTypeInfo;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setFieldDataTypes(DataType[] fieldDataTypes) {
+        this.fieldDataTypes = fieldDataTypes;
+        return this;
+    }
+
+    public JdbcDynamicOutputFormatBuilder setInLongMetric(String inLongMetric) {
+        this.inLongMetric = inLongMetric;
+        return this;
+    }
+
+    public JdbcBatchingOutputFormat<RowData, ?, ?> build() {
+        checkNotNull(jdbcOptions, "jdbc options can not be null");
+        checkNotNull(dmlOptions, "jdbc dml options can not be null");
+        checkNotNull(executionOptions, "jdbc execution options can not be null");
+
+        final LogicalType[] logicalTypes =
+                Arrays.stream(fieldDataTypes)
+                        .map(DataType::getLogicalType)
+                        .toArray(LogicalType[]::new);
+        if (dmlOptions.getKeyFields().isPresent() && dmlOptions.getKeyFields().get().length > 0 && !appendMode) {
+            // upsert query
+            return new JdbcBatchingOutputFormat<>(
+                    new SimpleJdbcConnectionProvider(jdbcOptions),
+                    executionOptions,
+                    ctx ->
+                            createBufferReduceExecutor(
+                                    dmlOptions, ctx, rowDataTypeInformation, logicalTypes),
+                    JdbcBatchingOutputFormat.RecordExtractor.identity(),
+                    inLongMetric);
+        } else {
+            // append only query
+            final String sql =
+                    dmlOptions
+                            .getDialect()
+                            .getInsertIntoStatement(
+                                    dmlOptions.getTableName(), dmlOptions.getFieldNames());
+            return new JdbcBatchingOutputFormat<>(
+                    new SimpleJdbcConnectionProvider(jdbcOptions),
+                    executionOptions,
+                    ctx ->
+                            createSimpleBufferedExecutor(
+                                    ctx,
+                                    dmlOptions.getDialect(),
+                                    dmlOptions.getFieldNames(),
+                                    logicalTypes,
+                                    sql,
+                                    rowDataTypeInformation),
+                    JdbcBatchingOutputFormat.RecordExtractor.identity(),
+                    inLongMetric);
+        }
     }
 }
