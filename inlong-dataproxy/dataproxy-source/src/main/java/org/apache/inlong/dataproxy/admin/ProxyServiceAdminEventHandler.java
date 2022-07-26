cf0402aedd2e8719d84574a96baaf06f10a765e0
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2022-07-26 22:45:08 +0800

    [feature]:modify format

--- inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/admin/ProxyServiceAdminEventHandler.java
+++ inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/admin/ProxyServiceAdminEventHandler.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,8 +59,8 @@ public class ProxyServiceAdminEventHandler extends AbstractAdminEventHandler {
         String sourceName = event.getHeaders().get(ProxyServiceMBean.KEY_SOURCENAME);
         LOG.info("start to process admin task:{},sourceName:{}", cmd, sourceName);
         switch (cmd) {
-            case ProxyServiceMBean.METHOD_STOPSERVICE:
-            case ProxyServiceMBean.METHOD_RECOVERSERVICE:
+            case ProxyServiceMBean.METHOD_STOPSERVICE :
+            case ProxyServiceMBean.METHOD_RECOVERSERVICE :
                 if (sourceName == null) {
                     break;
                 }
@@ -70,7 +70,7 @@ public class ProxyServiceAdminEventHandler extends AbstractAdminEventHandler {
                     this.processOne(cmd, sourceName, response);
                 }
                 break;
-            default:
+            default :
                 break;
         }
         LOG.info("end to process admin task:{},sourceName:{}", cmd, sourceName);
