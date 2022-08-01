0d58360b95679c352265ce59d7afd0ad2c6003fd
Loveca <loveca@github.com>
2022-08-01 11:04:46 +0800

    Modified DorisColumnInfo.java

--- inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/sink/doris/DorisColumnInfo.java
+++ inlong-manager/manager-common/src/main/java/org/apache/inlong/manager/common/pojo/sink/doris/DorisColumnInfo.java
@@ -1,4 +1,13 @@
 package org.apache.inlong.manager.common.pojo.sink.doris;
 
+import lombok.Data;
+
+@Data
 public class DorisColumnInfo {
+
+    private String name;
+
+    private String type;
+
+    private String comment;
 }
