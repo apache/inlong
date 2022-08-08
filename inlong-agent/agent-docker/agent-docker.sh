258c634a897d33ed81efd96548ae34e7d58b915e
Lucas <100204617+lucaspeng12138@users.noreply.github.com>
2022-08-08 17:00:30 +0800

    [INLONG-5393][Agent] Fix docker image of agent stuck in starting (#5395)

--- inlong-agent/agent-docker/agent-docker.sh
+++ inlong-agent/agent-docker/agent-docker.sh
@@ -36,6 +36,8 @@ audit.proxys=$AUDIT_PROXY_URL
 agent.cluster.tag=$CLUSTER_TAG
 agent.cluster.name=$CLUSTER_NAME
 agent.cluster.inCharges=$CLUSTER_IN_CHARGES
+agent.manager.auth.secretId=
+agent.manager.auth.secretKey=
 EOF
 # start
 bash +x ${file_path}/bin/agent.sh start
