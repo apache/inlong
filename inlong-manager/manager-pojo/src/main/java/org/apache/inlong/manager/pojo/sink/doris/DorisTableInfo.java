7f3463a788f68cd9783a1a9a193e940254ffcaaf
Loveca <loveca@github.com>
2022-08-17 13:43:01 +0800

    add DorisTableInfo

---
+++ inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sink/doris/DorisTableInfo.java
@@ -0,0 +1,23 @@
+package org.apache.inlong.manager.pojo.sink.doris;
+
+import lombok.Data;
+
+
+import java.util.List;
+
+/**
+ * Doris table info.
+ */
+@Data
+public class DorisTableInfo {
+
+    private String tableName;
+
+    private String comment;
+
+    private String primaryKey;
+
+    private String userName;
+
+    private List<DorisColumnInfo> columns;
+}
