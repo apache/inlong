e87bb19d2951ad91c8f16eb19ed76aa716ef4d6e
leosanqing <stormleo@qq.com>
2022-09-07 10:42:35 +0800

    [INLONG-5810] Fix codestyle check

--- inlong-manager/manager-client/src/main/java/org/apache/inlong/manager/client/api/inner/client/WorkflowEventClient.java
+++ inlong-manager/manager-client/src/main/java/org/apache/inlong/manager/client/api/inner/client/WorkflowEventClient.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.inlong.manager.client.api.inner.client;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -67,7 +84,6 @@ public class WorkflowEventClient {
         ClientUtils.assertRespSuccess(response);
     }
 
-
     /**
      * Re-execute the specified listener based on the task ID
      */
@@ -91,7 +107,6 @@ public class WorkflowEventClient {
                 workflowEventApi.triggerProcessEvent(processId, processEvent));
         ClientUtils.assertRespSuccess(response);
     }
-
 
     /**
      * Re-trigger the process event based on the task ID
