0604946174b6d3d2a41550630dc976b5c6dfb015
卢春亮 <946240095@qq.com>
2022-09-15 23:30:11 +0800

    fix checkstyle

--- inlong-sdk/sdk-common/src/main/java/org/apache/inlong/sdk/commons/node/InlongSinkProcessor.java
+++ inlong-sdk/sdk-common/src/main/java/org/apache/inlong/sdk/commons/node/InlongSinkProcessor.java
@@ -91,8 +91,6 @@ public class InlongSinkProcessor implements SinkProcessor {
 
     /**
      * process
-     * @return
-     * @throws EventDeliveryException
      */
     @Override
     public Status process() throws EventDeliveryException {
