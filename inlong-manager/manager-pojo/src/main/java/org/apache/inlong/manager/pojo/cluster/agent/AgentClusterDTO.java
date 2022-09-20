8a965cc08e289dd8ba811bbf193daa46286fc29b
fuwenkai <834260992@qq.com>
2022-09-20 17:18:07 +0800

    [INLONG-5962][Manager] Optimize code

--- inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/cluster/agent/AgentClusterDTO.java
+++ inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/cluster/agent/AgentClusterDTO.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.inlong.manager.pojo.cluster.agent;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -24,8 +41,7 @@ import javax.validation.constraints.NotNull;
 public class AgentClusterDTO {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().configure(
-            DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    ; // thread safe
+            DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // thread safe
 
     @ApiModelProperty(value = "Transfer machine IP, such as: http://127.0.0.1:8080", notes = "Transfer machine IP")
     private String transferIp;
