623a18d6368701ef1f50c83bbeb5141c7f3f9603
keylchen <keylchen@tencent.com>
2022-09-28 11:47:21 +0800

    add logger support and relative annotation and reconstruct prometheus listener according to ZhiYan.

--- inlong-agent/agent-common/src/main/java/org/apache/inlong/agent/metrics/AgentCustomPrometheusListener.java
+++ inlong-agent/agent-common/src/main/java/org/apache/inlong/agent/metrics/AgentCustomPrometheusListener.java
@@ -46,40 +46,55 @@ import io.prometheus.client.exporter.HTTPServer;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.apache.inlong.agent.conf.AgentConfiguration;
 import org.apache.inlong.common.metric.MetricItemValue;
 import org.apache.inlong.common.metric.MetricListener;
 import org.apache.inlong.common.metric.MetricValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * add prometheus listener according to ZhiYan
+ */
 public class AgentCustomPrometheusListener extends Collector implements MetricListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AgentCustomPrometheusListener.class);
     List<MetricFamilySamples> mfs = new ArrayList<>();
     private static final MetricValue ZERO = MetricValue.of(null, 0);
     protected HTTPServer httpServer;
 
-
     public AgentCustomPrometheusListener() {
+        CounterMetricFamily initCounter = new CounterMetricFamily("init",
+                "To begin to collect metrics.",
+                Collections.singletonList("dimension"));
+        mfs.add(initCounter);
         try {
             int metricsServerPort = AgentConfiguration.getAgentConf()
                     .getInt(PROMETHEUS_EXPORTER_PORT, DEFAULT_PROMETHEUS_EXPORTER_PORT);
             httpServer = new HTTPServer(metricsServerPort);
             this.register();
+            LOGGER.info("Starting custom prometheus metrics server on port {}", metricsServerPort);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("exception while register agent custom prometheus http server,error:{}", e.getMessage());
         }
     }
 
-    public List<MetricFamilySamples> getMfs() {
-        return mfs;
-    }
-
+    /**
+     * To collect MetricFamilySamples data.
+     *
+     * @return list of MetricFamilySamples.
+     */
     @Override
     public List<MetricFamilySamples> collect() {
         return mfs;
     }
 
+    /**
+     * use for MetricListenerRunnable.
+     */
     @Override
     public void snapshot(String domain, List<MetricItemValue> itemValues) {
         for (MetricItemValue itemValue : itemValues) {
@@ -91,48 +106,25 @@ public class AgentCustomPrometheusListener extends Collector implements MetricLi
                     Arrays.asList(dimensionMap.getOrDefault(KEY_PLUGIN_ID, "-"),
                             dimensionMap.getOrDefault(KEY_INLONG_GROUP_ID, "-"),
                             dimensionMap.getOrDefault(KEY_INLONG_STREAM_ID, "-"),
-                            dimensionMap.getOrDefault(KEY_COMPONENT_NAME, "-")), Math.random());
-
+                            dimensionMap.getOrDefault(KEY_COMPONENT_NAME, "-")), System.currentTimeMillis());
             MetricFamilySamples samples = new MetricFamilySamples(metricName, Type.GAUGE, "The guage of dimension map",
-                    Arrays.asList(sample));
+                    Collections.singletonList(sample));
             mfs.add(samples);
-
             //add metrics
             CounterMetricFamily metricsCounter = new CounterMetricFamily("group=total", "The metric of  dimension",
-                    Arrays.asList("dimension"));
+                    Collections.singletonList("dimension"));
             Map<String, MetricValue> metricMap = itemValue.getMetrics();
-            metricsCounter.addMetric(Arrays.asList(M_JOB_RUNNING_COUNT),
-                    (double) metricMap.getOrDefault(M_JOB_RUNNING_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_JOB_FATAL_COUNT), (double)
-                    metricMap.getOrDefault(M_JOB_FATAL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_TASK_RUNNING_COUNT),
-                    (double) metricMap.getOrDefault(M_TASK_RUNNING_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_TASK_RETRYING_COUNT),
-                    (double) metricMap.getOrDefault(M_TASK_RETRYING_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_TASK_FATAL_COUNT),
-                    (double) metricMap.getOrDefault(M_TASK_FATAL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_SINK_SUCCESS_COUNT),
-                    (double) metricMap.getOrDefault(M_SINK_SUCCESS_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_SINK_FAIL_COUNT),
-                    (double) metricMap.getOrDefault(M_SINK_FAIL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_SOURCE_SUCCESS_COUNT),
-                    (double) metricMap.getOrDefault(M_SOURCE_SUCCESS_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_SOURCE_FAIL_COUNT),
-                    (double) metricMap.getOrDefault(M_SOURCE_FAIL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_READ_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_READ_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_SEND_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_SEND_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_READ_FAIL_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_READ_FAIL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_SEND_FAIL_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_SEND_FAIL_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_READ_SUCCESS_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_READ_SUCCESS_COUNT, ZERO).value);
-            metricsCounter.addMetric(Arrays.asList(M_PLUGIN_SEND_SUCCESS_COUNT),
-                    (double) metricMap.getOrDefault(M_PLUGIN_SEND_SUCCESS_COUNT, ZERO).value);
+            ArrayList<String> metricStrings = new ArrayList<>(Arrays.asList(M_JOB_RUNNING_COUNT, M_JOB_FATAL_COUNT,
+                    M_TASK_RUNNING_COUNT, M_TASK_RETRYING_COUNT, M_TASK_FATAL_COUNT,
+                    M_SINK_SUCCESS_COUNT, M_SINK_FAIL_COUNT,
+                    M_SOURCE_SUCCESS_COUNT, M_SOURCE_FAIL_COUNT,
+                    M_PLUGIN_READ_COUNT, M_PLUGIN_READ_SUCCESS_COUNT, M_PLUGIN_READ_FAIL_COUNT,
+                    M_PLUGIN_SEND_COUNT, M_PLUGIN_SEND_SUCCESS_COUNT, M_PLUGIN_SEND_FAIL_COUNT));
+            for (String metricString : metricStrings) {
+                metricsCounter.addMetric(Collections.singletonList(metricString),
+                        metricMap.getOrDefault(metricString, ZERO).value);
+            }
             mfs.add(metricsCounter);
-
         }
     }
 }
