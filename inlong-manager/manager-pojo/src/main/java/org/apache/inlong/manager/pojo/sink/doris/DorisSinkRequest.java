280d024170bb50ba2a18805623468899688238f3
Yizhou Yang <32808678+Yizhou-Yang@users.noreply.github.com>
2022-09-30 11:15:17 +0800

    add initial doris sink

---
+++ inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sink/doris/DorisSinkRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.manager.pojo.sink.doris;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.apache.inlong.manager.common.consts.SinkType;
+import org.apache.inlong.manager.pojo.sink.SinkRequest;
+import org.apache.inlong.manager.common.util.JsonTypeDefine;
+
+import java.util.Map;
+
+/**
+ * Greenplum sink request.
+ */
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(value = "Doris sink request")
+@JsonTypeDefine(value = SinkType.DORIS)
+public class DorisSinkRequest extends SinkRequest {
+
+    @ApiModelProperty("Host of the Doris server")
+    private String host;
+
+    @ApiModelProperty("Port of the Doris server")
+    private Integer port;
+
+    @ApiModelProperty("Username of the Doris server")
+    private String username;
+
+    @ApiModelProperty("User password of the Doris server")
+    private String password;
+
+    @ApiModelProperty("Target table name")
+    private String tableName;
+
+    @ApiModelProperty("Primary key")
+    private String primaryKey;
+
+    @ApiModelProperty("Database mapping rule")
+    private String databaseMappingRule;
+
+    @ApiModelProperty("Table mapping rule")
+    private String tableMappingRule;
+
+    @ApiModelProperty("Properties for doris")
+    private Map<String, Object> properties;
+}
