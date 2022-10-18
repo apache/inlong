41fffa7d1b8a8f62fba1c8f204621681dbf97411
EMsnap <zpengag@connect.ust.hk>
2022-10-18 18:02:14 +0800

    [INLONG-6174][Sort] fix ut

--- inlong-sort/sort-core/src/test/java/org/apache/inlong/sort/parser/AllMigrateTest.java
+++ inlong-sort/sort-core/src/test/java/org/apache/inlong/sort/parser/AllMigrateTest.java
@@ -37,6 +37,7 @@ import org.apache.inlong.sort.protocol.node.format.CsvFormat;
 import org.apache.inlong.sort.protocol.node.load.KafkaLoadNode;
 import org.apache.inlong.sort.protocol.transformation.FieldRelation;
 import org.apache.inlong.sort.protocol.transformation.relation.NodeRelation;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
