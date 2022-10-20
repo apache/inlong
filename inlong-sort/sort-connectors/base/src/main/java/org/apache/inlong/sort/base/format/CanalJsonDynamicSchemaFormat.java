d062fb33518dec7693cc3dfd5e3da73a143af942
thesumery <107393625+thesumery@users.noreply.github.com>
2022-10-20 11:17:07 +0800

    [INLONG-6224][Sort] Import schema and data parsing ability for DynamicSchemaFormat (#6225)
    Co-authored-by: thesumery <158971219@qq.com>

--- inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/CanalJsonDynamicSchemaFormat.java
+++ inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/CanalJsonDynamicSchemaFormat.java
@@ -17,12 +17,14 @@
 
 package org.apache.inlong.sort.base.format;
 
+import org.apache.flink.formats.json.JsonToRowDataConverters.JsonToRowDataConverter;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.types.RowKind;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.apache.inlong.sort.base.Constants.DATA;
-import static org.apache.inlong.sort.base.Constants.PK_NAMES;
 
 /**
  * Canal json dynamic format
@@ -30,6 +32,14 @@ import static org.apache.inlong.sort.base.Constants.PK_NAMES;
 public class CanalJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
 
     private static final String IDENTIFIER = "canal-json";
+    private static final String DDL_FLAG = "ddl";
+    private static final String DATA = "data";
+    private static final String PK_NAMES = "pkNames";
+    private static final String SCHEMA = "sqlType";
+    private static final String OP_TYPE = "type";
+    private static final String OP_INSERT = "INSERT";
+    private static final String OP_UPDATE = "UPDATE";
+    private static final String OP_DELETE = "DELETE";
 
     private static final CanalJsonDynamicSchemaFormat FORMAT = new CanalJsonDynamicSchemaFormat();
 
@@ -61,6 +71,47 @@ public class CanalJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
             }
         }
         return pkNames;
+    }
+
+    @Override
+    public boolean extractDDLFlag(JsonNode data) {
+        return data.has(DDL_FLAG) ? data.get(DDL_FLAG).asBoolean(false) : false;
+    }
+
+    @Override
+    public RowType extractSchema(JsonNode data, List<String> pkNames) {
+        JsonNode schema = data.get(SCHEMA);
+        return extractSchemaNode(schema, pkNames);
+    }
+
+    @Override
+    public List<RowData> extractRowData(JsonNode data, RowType rowType) {
+        JsonNode opNode = data.get(OP_TYPE);
+        JsonNode dataNode = data.get(DATA);
+        if (opNode == null || dataNode == null || !dataNode.isArray()) {
+            throw new IllegalArgumentException(String.format("Error opNode: %s, or dataNode: %s", opNode, dataNode));
+        }
+
+        String op = data.get(OP_TYPE).asText();
+        RowKind rowKind;
+        if (OP_INSERT.equals(op)) {
+            rowKind = RowKind.INSERT;
+        } else if (OP_UPDATE.equals(op)) {
+            rowKind = RowKind.UPDATE_AFTER;
+        } else if (OP_DELETE.equals(op)) {
+            rowKind = RowKind.DELETE;
+        } else {
+            throw new IllegalArgumentException("Unsupported op_type: " + op);
+        }
+        JsonToRowDataConverter rowDataConverter = rowDataConverters.createConverter(rowType);
+
+        List<RowData> rowDataList = new ArrayList<>();
+        for (JsonNode row : dataNode) {
+            RowData rowData = (RowData) rowDataConverter.convert(row);
+            rowData.setRowKind(rowKind);
+            rowDataList.add(rowData);
+        }
+        return rowDataList;
     }
 
     /**
--- inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/DebeziumJsonDynamicSchemaFormat.java
+++ inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/DebeziumJsonDynamicSchemaFormat.java
@@ -17,14 +17,14 @@
 
 package org.apache.inlong.sort.base.format;
 
+import org.apache.flink.formats.json.JsonToRowDataConverters.JsonToRowDataConverter;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.types.RowKind;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.apache.inlong.sort.base.Constants.AFTER;
-import static org.apache.inlong.sort.base.Constants.BEFORE;
-import static org.apache.inlong.sort.base.Constants.PK_NAMES;
-import static org.apache.inlong.sort.base.Constants.SOURCE;
 
 /**
  * Debezium json dynamic format
@@ -32,6 +32,18 @@ import static org.apache.inlong.sort.base.Constants.SOURCE;
 public class DebeziumJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
 
     private static final String IDENTIFIER = "debezium-json";
+    private static final String DDL_FLAG = "ddl";
+    private static final String SCHEMA = "sqlType";
+    private static final String AFTER = "after";
+    private static final String BEFORE = "before";
+    private static final String SOURCE = "source";
+    private static final String PK_NAMES = "pkNames";
+    private static final String OP_TYPE = "op";
+    private static final String OP_READ = "r"; // snapshot read
+    private static final String OP_CREATE = "c"; // insert
+    private static final String OP_UPDATE = "u"; // update
+    private static final String OP_DELETE = "d"; // delete
+
 
     private static final DebeziumJsonDynamicSchemaFormat FORMAT = new DebeziumJsonDynamicSchemaFormat();
 
@@ -67,6 +79,49 @@ public class DebeziumJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
             }
         }
         return pkNames;
+    }
+
+    @Override
+    public boolean extractDDLFlag(JsonNode data) {
+        return data.has(DDL_FLAG) ? data.get(DDL_FLAG).asBoolean(false) : false;
+    }
+
+    @Override
+    public RowType extractSchema(JsonNode data, List<String> pkNames) {
+        JsonNode schema = data.get(SCHEMA);
+        return extractSchemaNode(schema, pkNames);
+    }
+
+    @Override
+    public List<RowData> extractRowData(JsonNode data, RowType rowType) {
+        JsonNode opNode = data.get(OP_TYPE);
+        JsonNode dataBeforeNode = data.get(BEFORE);
+        JsonNode dataAfterNode = data.get(AFTER);
+        if (opNode == null || (dataBeforeNode == null && dataAfterNode == null)) {
+            throw new IllegalArgumentException(
+                    String.format("Error opNode: %s, or dataBeforeNode: %s, dataAfterNode",
+                            opNode, dataBeforeNode, dataAfterNode));
+        }
+        List<RowData> rowDataList = new ArrayList<>();
+        JsonToRowDataConverter rowDataConverter = rowDataConverters.createConverter(rowType);
+
+        String op = data.get(OP_TYPE).asText();
+        if (OP_CREATE.equals(op) || OP_READ.equals(op)) {
+            RowData rowData = (RowData) rowDataConverter.convert(dataAfterNode);
+            rowData.setRowKind(RowKind.INSERT);
+            rowDataList.add(rowData);
+        } else if (OP_UPDATE.equals(op)) {
+            RowData rowData = (RowData) rowDataConverter.convert(dataAfterNode);
+            rowData.setRowKind(RowKind.UPDATE_AFTER);
+            rowDataList.add(rowData);
+        } else if (OP_DELETE.equals(op)) {
+            RowData rowData = (RowData) rowDataConverter.convert(dataBeforeNode);
+            rowData.setRowKind(RowKind.DELETE);
+            rowDataList.add(rowData);
+        } else {
+            throw new IllegalArgumentException("Unsupported op_type: " + op);
+        }
+        return rowDataList;
     }
 
     /**
