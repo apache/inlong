d431ba8d50d61ed6f02cf49308448b6619c7f8fb
ganfengtan <ganfengtan@gmail.com>
2022-10-21 12:09:14 +0800

    [INLONG-6251][Agent] Fix UT ConcurrentModification error

--- inlong-agent/agent-common/src/main/java/org/apache/inlong/agent/constant/AgentConstants.java
+++ inlong-agent/agent-common/src/main/java/org/apache/inlong/agent/constant/AgentConstants.java
@@ -43,88 +43,61 @@ public class AgentConstants {
     public static final String DEFAULT_AGENT_ROCKS_DB_PATH = ".rocksdb";
 
     public static final String AGENT_UNIQ_ID = "agent.uniq.id";
-    // default use local ip as uniq id for agent.
-    public static final String DEFAULT_AGENT_UNIQ_ID = AgentUtils.getLocalIp();
-
     public static final String AGENT_DB_INSTANCE_NAME = "agent.db.instance.name";
     public static final String DEFAULT_AGENT_DB_INSTANCE_NAME = "agent";
-
     public static final String AGENT_DB_CLASSNAME = "agent.db.classname";
     public static final String DEFAULT_AGENT_DB_CLASSNAME = "org.apache.inlong.agent.db.RocksDbImp";
-
     // default is empty.
     public static final String AGENT_FETCHER_CLASSNAME = "agent.fetcher.classname";
-
     public static final String AGENT_MESSAGE_FILTER_CLASSNAME = "agent.message.filter.classname";
-
     public static final String AGENT_CONF_PARENT = "agent.conf.parent";
     public static final String DEFAULT_AGENT_CONF_PARENT = "conf";
-
     public static final String AGENT_HTTP_PORT = "agent.http.port";
     public static final int DEFAULT_AGENT_HTTP_PORT = 8008;
-
     public static final String AGENT_ENABLE_HTTP = "agent.http.enable";
     public static final boolean DEFAULT_AGENT_ENABLE_HTTP = false;
-
     public static final String TRIGGER_FETCH_INTERVAL = "trigger.fetch.interval";
     public static final int DEFAULT_TRIGGER_FETCH_INTERVAL = 1;
-
     public static final String TRIGGER_MAX_RUNNING_NUM = "trigger.max.running.num";
     public static final int DEFAULT_TRIGGER_MAX_RUNNING_NUM = 4096;
-
     public static final String AGENT_FETCH_CENTER_INTERVAL_SECONDS = "agent.fetchCenter.interval";
     public static final int DEFAULT_AGENT_FETCH_CENTER_INTERVAL_SECONDS = 5;
-
     public static final String AGENT_TRIGGER_CHECK_INTERVAL_SECONDS = "agent.trigger.check.interval";
     public static final int DEFAULT_AGENT_TRIGGER_CHECK_INTERVAL_SECONDS = 1;
-
     public static final String THREAD_POOL_AWAIT_TIME = "thread.pool.await.time";
     // time in ms
     public static final long DEFAULT_THREAD_POOL_AWAIT_TIME = 300;
-
     public static final String JOB_MONITOR_INTERVAL = "job.monitor.interval";
     public static final int DEFAULT_JOB_MONITOR_INTERVAL = 5;
-
     public static final String JOB_FINISH_CHECK_INTERVAL = "job.finish.checkInterval";
     public static final long DEFAULT_JOB_FINISH_CHECK_INTERVAL = 6L;
-
     public static final String TASK_RETRY_MAX_CAPACITY = "task.retry.maxCapacity";
     public static final int DEFAULT_TASK_RETRY_MAX_CAPACITY = 10000;
-
     public static final String TASK_MONITOR_INTERVAL = "task.monitor.interval";
     public static final int DEFAULT_TASK_MONITOR_INTERVAL = 6;
-
     public static final String TASK_RETRY_SUBMIT_WAIT_SECONDS = "task.retry.submit.waitSeconds";
     public static final int DEFAULT_TASK_RETRY_SUBMIT_WAIT_SECONDS = 5;
-
     public static final String TASK_MAX_RETRY_TIME = "task.maxRetry.time";
     public static final int DEFAULT_TASK_MAX_RETRY_TIME = 3;
-
     public static final String TASK_PUSH_MAX_SECOND = "task.push.maxSecond";
     public static final int DEFAULT_TASK_PUSH_MAX_SECOND = 2;
-
     public static final String TASK_PULL_MAX_SECOND = "task.pull.maxSecond";
     public static final int DEFAULT_TASK_PULL_MAX_SECOND = 2;
-
     public static final String CHANNEL_MEMORY_CAPACITY = "channel.memory.capacity";
-    public static final int DEFAULT_CHANNEL_MEMORY_CAPACITY = 1000;
-
+    public static final int DEFAULT_CHANNEL_MEMORY_CAPACITY = 2000;
     public static final String TRIGGER_CHECK_INTERVAL = "trigger.check.interval";
     public static final int DEFAULT_TRIGGER_CHECK_INTERVAL = 2;
-
     public static final String JOB_DB_CACHE_TIME = "job.db.cache.time";
     // cache for 3 days.
     public static final long DEFAULT_JOB_DB_CACHE_TIME = 3 * 24 * 60 * 60 * 1000;
-
     public static final String JOB_DB_CACHE_CHECK_INTERVAL = "job.db.cache.check.interval";
     public static final int DEFAULT_JOB_DB_CACHE_CHECK_INTERVAL = 60 * 60;
-
     public static final String JOB_NUMBER_LIMIT = "job.number.limit";
     public static final int DEFAULT_JOB_NUMBER_LIMIT = 15;
-
     public static final String AGENT_LOCAL_IP = "agent.local.ip";
     public static final String DEFAULT_LOCAL_IP = "127.0.0.1";
-
+    // default use local ip as uniq id for agent.
+    public static final String DEFAULT_AGENT_UNIQ_ID = AgentUtils.getLocalIp();
     public static final String CUSTOM_FIXED_IP = "agent.custom.fixed.ip";
 
     public static final String AGENT_CLUSTER_NAME = "agent.cluster.name";
