65fd8836172d380c71e71212944be9f8cb935633
yunqingmoswu <skyyun1125@163.com>
2022-10-21 17:14:35 +0800

    [INLONG-6256][Sort] Support debezium-json format with schema parse for DebeziumJsonDynamicSchemaFormat

--- inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/DebeziumJsonDynamicSchemaFormat.java
+++ inlong-sort/sort-connectors/base/src/main/java/org/apache/inlong/sort/base/format/DebeziumJsonDynamicSchemaFormat.java
@@ -23,6 +23,7 @@ import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.types.RowKind;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,10 +40,23 @@ public class DebeziumJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
     private static final String SOURCE = "source";
     private static final String PK_NAMES = "pkNames";
     private static final String OP_TYPE = "op";
-    private static final String OP_READ = "r"; // snapshot read
-    private static final String OP_CREATE = "c"; // insert
-    private static final String OP_UPDATE = "u"; // update
-    private static final String OP_DELETE = "d"; // delete
+    private static final String PAYLOAD = "payload";
+    /**
+     * Snapshot read
+     */
+    private static final String OP_READ = "r";
+    /**
+     * Insert
+     */
+    private static final String OP_CREATE = "c";
+    /**
+     * Update
+     */
+    private static final String OP_UPDATE = "u";
+    /**
+     * Delete
+     */
+    private static final String OP_DELETE = "d";
 
 
     private static final DebeziumJsonDynamicSchemaFormat FORMAT = new DebeziumJsonDynamicSchemaFormat();
@@ -58,32 +72,53 @@ public class DebeziumJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
 
     @Override
     public JsonNode getPhysicalData(JsonNode root) {
-        JsonNode physicalData = root.get(AFTER);
-        if (physicalData == null) {
-            physicalData = root.get(BEFORE);
+        JsonNode payload = root.get(PAYLOAD);
+        if (payload == null) {
+            JsonNode physicalData = root.get(AFTER);
+            if (physicalData == null) {
+                physicalData = root.get(BEFORE);
+            }
+            return physicalData;
         }
-        return physicalData;
+        return getPhysicalData(payload);
     }
 
     @Override
     public List<String> extractPrimaryKeyNames(JsonNode data) {
         List<String> pkNames = new ArrayList<>();
-        JsonNode sourceNode = data.get(SOURCE);
-        if (sourceNode == null) {
+        JsonNode payload = data.get(PAYLOAD);
+        if (payload == null) {
+            JsonNode sourceNode = data.get(SOURCE);
+            if (sourceNode == null) {
+                return pkNames;
+            }
+            JsonNode pkNamesNode = sourceNode.get(PK_NAMES);
+            if (pkNamesNode != null && pkNamesNode.isArray()) {
+                for (int i = 0; i < pkNamesNode.size(); i++) {
+                    pkNames.add(pkNamesNode.get(i).asText());
+                }
+            }
             return pkNames;
         }
-        JsonNode pkNamesNode = sourceNode.get(PK_NAMES);
-        if (pkNamesNode != null && pkNamesNode.isArray()) {
-            for (int i = 0; i < pkNamesNode.size(); i++) {
-                pkNames.add(pkNamesNode.get(i).asText());
-            }
+        return extractPrimaryKeyNames(payload);
+    }
+
+    @Override
+    public String parse(JsonNode rootNode, String pattern) throws IOException {
+        JsonNode payload = rootNode.get(PAYLOAD);
+        if (payload == null) {
+            return super.parse(rootNode, pattern);
         }
-        return pkNames;
+        return super.parse(payload, pattern);
     }
 
     @Override
     public boolean extractDDLFlag(JsonNode data) {
-        return data.has(DDL_FLAG) ? data.get(DDL_FLAG).asBoolean(false) : false;
+        JsonNode payload = data.get(PAYLOAD);
+        if (payload == null) {
+            return data.has(DDL_FLAG) && data.get(DDL_FLAG).asBoolean(false);
+        }
+        return extractDDLFlag(payload);
     }
 
     @Override
@@ -94,34 +129,41 @@ public class DebeziumJsonDynamicSchemaFormat extends JsonDynamicSchemaFormat {
 
     @Override
     public List<RowData> extractRowData(JsonNode data, RowType rowType) {
-        JsonNode opNode = data.get(OP_TYPE);
-        JsonNode dataBeforeNode = data.get(BEFORE);
-        JsonNode dataAfterNode = data.get(AFTER);
-        if (opNode == null || (dataBeforeNode == null && dataAfterNode == null)) {
-            throw new IllegalArgumentException(
-                    String.format("Error opNode: %s, or dataBeforeNode: %s, dataAfterNode",
-                            opNode, dataBeforeNode, dataAfterNode));
-        }
-        List<RowData> rowDataList = new ArrayList<>();
-        JsonToRowDataConverter rowDataConverter = rowDataConverters.createConverter(rowType);
+        JsonNode payload = data.get(PAYLOAD);
+        if (payload == null) {
+            JsonNode opNode = data.get(OP_TYPE);
+            JsonNode dataBeforeNode = data.get(BEFORE);
+            JsonNode dataAfterNode = data.get(AFTER);
+            if (opNode == null || (dataBeforeNode == null && dataAfterNode == null)) {
+                throw new IllegalArgumentException(
+                        String.format("Error opNode: %s, or dataBeforeNode: %s, dataAfterNode: %s",
+                                opNode, dataBeforeNode, dataAfterNode));
+            }
+            List<RowData> rowDataList = new ArrayList<>();
+            JsonToRowDataConverter rowDataConverter = rowDataConverters.createConverter(rowType);
 
-        String op = data.get(OP_TYPE).asText();
-        if (OP_CREATE.equals(op) || OP_READ.equals(op)) {
-            RowData rowData = (RowData) rowDataConverter.convert(dataAfterNode);
-            rowData.setRowKind(RowKind.INSERT);
-            rowDataList.add(rowData);
-        } else if (OP_UPDATE.equals(op)) {
-            RowData rowData = (RowData) rowDataConverter.convert(dataAfterNode);
-            rowData.setRowKind(RowKind.UPDATE_AFTER);
-            rowDataList.add(rowData);
-        } else if (OP_DELETE.equals(op)) {
-            RowData rowData = (RowData) rowDataConverter.convert(dataBeforeNode);
-            rowData.setRowKind(RowKind.DELETE);
-            rowDataList.add(rowData);
-        } else {
-            throw new IllegalArgumentException("Unsupported op_type: " + op);
+            String op = data.get(OP_TYPE).asText();
+            if (OP_CREATE.equals(op) || OP_READ.equals(op)) {
+                RowData rowData = (RowData) rowDataConverter.convert(dataAfterNode);
+                rowData.setRowKind(RowKind.INSERT);
+                rowDataList.add(rowData);
+            } else if (OP_UPDATE.equals(op)) {
+                RowData rowData = (RowData) rowDataConverter.convert(dataBeforeNode);
+                rowData.setRowKind(RowKind.UPDATE_BEFORE);
+                rowDataList.add(rowData);
+                rowData = (RowData) rowDataConverter.convert(dataAfterNode);
+                rowData.setRowKind(RowKind.UPDATE_AFTER);
+                rowDataList.add(rowData);
+            } else if (OP_DELETE.equals(op)) {
+                RowData rowData = (RowData) rowDataConverter.convert(dataBeforeNode);
+                rowData.setRowKind(RowKind.DELETE);
+                rowDataList.add(rowData);
+            } else {
+                throw new IllegalArgumentException("Unsupported op_type: " + op);
+            }
+            return rowDataList;
         }
-        return rowDataList;
+        return extractRowData(payload, rowType);
     }
 
     /**
