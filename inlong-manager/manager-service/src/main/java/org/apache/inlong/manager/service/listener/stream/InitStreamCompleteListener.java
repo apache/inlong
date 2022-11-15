aed678b06dbade2fa55d3596fda5e6353c2345ae
healchow <healchow@gmail.com>
2022-11-15 15:47:01 +0800

    [INLONG-6544][Manager] Should update the source status after the inlong stream is completed

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/listener/stream/InitStreamCompleteListener.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/listener/stream/InitStreamCompleteListener.java
@@ -61,16 +61,19 @@ public class InitStreamCompleteListener implements ProcessEventListener {
         final String streamId = streamInfo.getInlongStreamId();
         final String operator = context.getOperator();
 
-        // Update status of other related configs
-        streamService.updateStatus(groupId, streamId, StreamStatus.CONFIG_SUCCESSFUL.getCode(), operator);
-        streamService.update(streamInfo.genRequest(), operator);
-        if (InlongConstants.LIGHTWEIGHT_MODE.equals(form.getGroupInfo().getLightweight())) {
-            sourceService.updateStatus(groupId, streamId, SourceStatus.SOURCE_NORMAL.getCode(), operator);
-        } else {
-            sourceService.updateStatus(groupId, streamId, SourceStatus.TO_BE_ISSUED_ADD.getCode(), operator);
+        try {
+            // Update status of other related configs
+            streamService.updateStatus(groupId, streamId, StreamStatus.CONFIG_SUCCESSFUL.getCode(), operator);
+            streamService.updateWithoutCheck(streamInfo.genRequest(), operator);
+            if (InlongConstants.LIGHTWEIGHT_MODE.equals(form.getGroupInfo().getLightweight())) {
+                sourceService.updateStatus(groupId, streamId, SourceStatus.SOURCE_NORMAL.getCode(), operator);
+            } else {
+                sourceService.updateStatus(groupId, streamId, SourceStatus.TO_BE_ISSUED_ADD.getCode(), operator);
+            }
+            return ListenerResult.success();
+        } catch (Exception e) {
+            throw new WorkflowListenerException(e);
         }
-
-        return ListenerResult.success();
     }
 
 }
