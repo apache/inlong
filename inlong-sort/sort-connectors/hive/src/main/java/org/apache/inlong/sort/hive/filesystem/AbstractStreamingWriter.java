c6a92e2460040d865fd1cdce1051d5c1937abac0
v_haifxu <xhf1208357848@gmail.com>
2022-12-05 10:20:46 +0800

    [INLONG-6657][Sort] fix code style

--- inlong-sort/sort-connectors/hive/src/main/java/org/apache/inlong/sort/hive/filesystem/AbstractStreamingWriter.java
+++ inlong-sort/sort-connectors/hive/src/main/java/org/apache/inlong/sort/hive/filesystem/AbstractStreamingWriter.java
@@ -56,8 +56,8 @@ import static org.apache.inlong.sort.base.Constants.NUM_RECORDS_OUT;
  */
 public abstract class AbstractStreamingWriter<IN, OUT> extends AbstractStreamOperator<OUT>
         implements
-        OneInputStreamOperator<IN, OUT>,
-        BoundedOneInput {
+            OneInputStreamOperator<IN, OUT>,
+            BoundedOneInput {
 
     private static final long serialVersionUID = 1L;
 
@@ -178,7 +178,7 @@ public abstract class AbstractStreamingWriter<IN, OUT> extends AbstractStreamOpe
             this.metricStateListState = context.getOperatorStateStore().getUnionListState(
                     new ListStateDescriptor<>(
                             INLONG_METRIC_STATE_NAME, TypeInformation.of(new TypeHint<MetricState>() {
-                    })));
+                            })));
         }
         if (context.isRestored()) {
             metricState = MetricStateUtils.restoreMetricState(metricStateListState,
