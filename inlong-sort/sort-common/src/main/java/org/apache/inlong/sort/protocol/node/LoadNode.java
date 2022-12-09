4c6a61b45b29aa49658a86d8799600538f86156e
averyzhang <dearzhangzuofeng@gmail.com>
2022-12-09 19:52:54 +0800

    [INLONG-6784][Sort] Support apache hudi LoadNode (#6789)

--- inlong-sort/sort-common/src/main/java/org/apache/inlong/sort/protocol/node/LoadNode.java
+++ inlong-sort/sort-common/src/main/java/org/apache/inlong/sort/protocol/node/LoadNode.java
@@ -36,6 +36,7 @@ import org.apache.inlong.sort.protocol.node.load.FileSystemLoadNode;
 import org.apache.inlong.sort.protocol.node.load.GreenplumLoadNode;
 import org.apache.inlong.sort.protocol.node.load.HbaseLoadNode;
 import org.apache.inlong.sort.protocol.node.load.HiveLoadNode;
+import org.apache.inlong.sort.protocol.node.load.HudiLoadNode;
 import org.apache.inlong.sort.protocol.node.load.IcebergLoadNode;
 import org.apache.inlong.sort.protocol.node.load.KafkaLoadNode;
 import org.apache.inlong.sort.protocol.node.load.MySqlLoadNode;
@@ -71,7 +72,9 @@ import java.util.Map;
         @JsonSubTypes.Type(value = GreenplumLoadNode.class, name = "greenplumLoad"),
         @JsonSubTypes.Type(value = DLCIcebergLoadNode.class, name = "dlcIcebergLoad"),
         @JsonSubTypes.Type(value = DorisLoadNode.class, name = "dorisLoad"),
-        @JsonSubTypes.Type(value = StarRocksLoadNode.class, name = "starRocksLoad")
+        @JsonSubTypes.Type(value = StarRocksLoadNode.class, name = "starRocksLoad"),
+        @JsonSubTypes.Type(value = DorisLoadNode.class, name = "dorisLoad"),
+        @JsonSubTypes.Type(value = HudiLoadNode.class, name = "hudiLoad"),
 })
 @NoArgsConstructor
 @Data
