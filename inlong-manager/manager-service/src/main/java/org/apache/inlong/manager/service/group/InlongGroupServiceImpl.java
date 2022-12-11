49d4b6210e0ece3e85c6fea993a891f58f213904
vernedeng <dengfan@pku.edu.cn>
2022-12-11 23:16:23 +0800

    [INLONG-6676][Manager] fix

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/group/InlongGroupServiceImpl.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/group/InlongGroupServiceImpl.java
@@ -74,7 +74,6 @@ import org.springframework.validation.annotation.Validated;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
