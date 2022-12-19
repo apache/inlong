ae3aff10af1d1e02fe5c91770e4a5dc14cf6aba7
Schnapps <zpengag@connect.ust.hk>
2022-12-19 14:14:35 +0800

    [INLONG-6949][Sort] support all rowkinds for metric computing and restore metrics state for dirty metric for jdbc connector (#6950)

--- inlong-sort/sort-connectors/jdbc/src/main/java/org/apache/inlong/sort/jdbc/table/JdbcDynamicTableSink.java
+++ inlong-sort/sort-connectors/jdbc/src/main/java/org/apache/inlong/sort/jdbc/table/JdbcDynamicTableSink.java
@@ -27,7 +27,6 @@ import org.apache.flink.table.connector.ChangelogMode;
 import org.apache.flink.table.connector.sink.DynamicTableSink;
 import org.apache.flink.table.connector.sink.SinkFunctionProvider;
 import org.apache.flink.table.data.RowData;
-import org.apache.flink.types.RowKind;
 import org.apache.inlong.sort.base.sink.SchemaUpdateExceptionPolicy;
 import org.apache.inlong.sort.base.dirty.DirtyOptions;
 import org.apache.inlong.sort.base.dirty.sink.DirtySink;
@@ -105,11 +104,7 @@ public class JdbcDynamicTableSink implements DynamicTableSink {
         if (!multipleSink) {
             validatePrimaryKey(requestedMode);
         }
-        return ChangelogMode.newBuilder()
-                .addContainedKind(RowKind.INSERT)
-                .addContainedKind(RowKind.DELETE)
-                .addContainedKind(RowKind.UPDATE_AFTER)
-                .build();
+        return ChangelogMode.all();
     }
 
     private void validatePrimaryKey(ChangelogMode requestedMode) {
