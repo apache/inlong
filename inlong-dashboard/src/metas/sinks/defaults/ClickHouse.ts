8dc1affd1b35281848f9c5a8d8f949942dcf1c2d
wanglz <yydsk9@gmail.com>
2023-01-04 09:58:12 +0800

    [INLONG-7133][Dashboard] The sink's data node supports jumping to the node page

--- inlong-dashboard/src/metas/sinks/defaults/ClickHouse.ts
+++ inlong-dashboard/src/metas/sinks/defaults/ClickHouse.ts
@@ -24,6 +24,7 @@ import i18n from '@/i18n';
 import EditableTable from '@/components/EditableTable';
 import { SinkInfo } from '../common/SinkInfo';
 import { sourceFields } from '../common/sourceFields';
+import NodeSelect from '@/components/NodeSelect';
 
 const { I18n } = DataWithBackend;
 const { FieldDecorator } = RenderRow;
@@ -93,31 +94,11 @@ export default class ClickHouseSink
   enableCreateResource: number;
 
   @FieldDecorator({
-    type: 'select',
+    type: NodeSelect,
     rules: [{ required: true }],
     props: values => ({
-      showSearch: true,
       disabled: [110, 130].includes(values?.status),
-      options: {
-        requestTrigger: ['onOpen', 'onSearch'],
-        requestService: keyword => ({
-          url: '/node/list',
-          method: 'POST',
-          data: {
-            keyword,
-            type: 'CLICKHOUSE',
-            pageNum: 1,
-            pageSize: 20,
-          },
-        }),
-        requestParams: {
-          formatResult: result =>
-            result?.list?.map(item => ({
-              label: item.name,
-              value: item.name,
-            })),
-        },
-      },
+      asyncValueLabel: 'CLICKHOUSE',
     }),
   })
   @I18n('meta.Sinks.ClickHouse.DataNodeName')
