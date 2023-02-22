2c6de2ac9db1cd46c9811cb10c1537e402283b8c
fuweng11 <76141879+fuweng11@users.noreply.github.com>
2023-02-22 11:29:35 +0800

    [INLONG-7401][Manager] Fix that cannot create Pulsar partitions based on numPartitions param (#7402)

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/resource/queue/pulsar/PulsarOperator.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/resource/queue/pulsar/PulsarOperator.java
@@ -49,7 +49,11 @@ import java.util.Map;
 public class PulsarOperator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InlongClusterServiceImpl.class);
-    private static final int MAX_PARTITION = 100;
+    /**
+     * The maximum number of partitions, which is an empirical value,
+     * generally does not exceed 1000 in large clusters.
+     */
+    private static final int MAX_PARTITION = 1000;
     private static final int RETRY_TIMES = 3;
     private static final int DELAY_SECONDS = 5;
 
@@ -164,7 +168,7 @@ public class PulsarOperator {
                 // The number of brokers as the default value of topic partition
                 List<String> clusters = PulsarUtils.getPulsarClusters(pulsarAdmin);
                 Integer numPartitions = topicInfo.getNumPartitions();
-                if (numPartitions < 0 || numPartitions <= MAX_PARTITION) {
+                if (numPartitions < 0 || numPartitions >= MAX_PARTITION) {
                     List<String> brokers = pulsarAdmin.brokers().getActiveBrokers(clusters.get(0));
                     numPartitions = brokers.size();
                 }
