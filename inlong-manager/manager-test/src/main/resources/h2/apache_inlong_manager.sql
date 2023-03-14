56418e01cea19dbc8091653b570096aab7165838
featzhang <featzhang@outlook.com>
2023-03-14 15:42:46 +0800

    fix cr

--- inlong-manager/manager-test/src/main/resources/h2/apache_inlong_manager.sql
+++ inlong-manager/manager-test/src/main/resources/h2/apache_inlong_manager.sql
@@ -786,8 +786,10 @@ VALUES ('audit_sdk_collect', 'SDK', 0, '1'),
        ('audit_sort_hbase_output', 'HBASE', 1, '20'),
        ('audit_sort_doris_input', 'DORIS', 0, '21'),
        ('audit_sort_doris_output', 'DORIS', 1, '22'),
-       ('audit_sort_kudu_input', 'KUDU', 0, '23'),
-       ('audit_sort_kudu_output', 'KUDU', 1, '24');
+       ('audit_sort_doris_output', 'MYSQL', 1, '23'),
+       ('audit_sort_doris_output', 'MYSQL', 1, '24'),
+       ('audit_sort_kudu_input', 'KUDU', 0, '25'),
+       ('audit_sort_kudu_output', 'KUDU', 1, '26');
 
 -- ----------------------------
 
