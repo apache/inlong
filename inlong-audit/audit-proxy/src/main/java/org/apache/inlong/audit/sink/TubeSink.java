b55a6377cd9bfae0a3743ad2caddc7869608a15e
v_haifxu <xhf1208357848@gmail.com>
2023-03-17 11:07:59 +0800

    [INLONG-7413][Audit] Fix code style

--- inlong-audit/audit-proxy/src/main/java/org/apache/inlong/audit/sink/TubeSink.java
+++ inlong-audit/audit-proxy/src/main/java/org/apache/inlong/audit/sink/TubeSink.java
@@ -325,7 +325,7 @@ public class TubeSink extends AbstractSink implements Configurable {
             lastSuccessSendCnt.set(nowCnt);
             t2 = System.currentTimeMillis();
             logger.info("tubesink {}, succ put {} events to tube,"
-                            + " in the past {} millsec",
+                    + " in the past {} millsec",
                     new Object[]{
                             getName(), (nowCnt - oldCnt), (t2 - t1)
                     });
