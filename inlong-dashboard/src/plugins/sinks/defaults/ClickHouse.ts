de4f2b6d57bb8dd16723676bcd4d2b49bfb4a4d0
Lizhen <88174078+bluewang@users.noreply.github.com>
2023-04-06 15:22:46 +0800

    [INLONG-7785][Dashboard] Clickhouse sink engine defaults to MergeTree (#7786)

--- inlong-dashboard/src/plugins/sinks/defaults/ClickHouse.ts
+++ inlong-dashboard/src/plugins/sinks/defaults/ClickHouse.ts
@@ -203,7 +203,7 @@ export default class ClickHouseSink
 
   @FieldDecorator({
     type: 'input',
-    initialValue: 'Log',
+    initialValue: 'MergeTree',
     rules: [{ required: true }],
     props: values => ({
       disabled: [110, 130].includes(values?.status),
@@ -241,6 +241,7 @@ export default class ClickHouseSink
 
   @FieldDecorator({
     type: 'inputnumber',
+    visible: values => values.engine === 'MergeTree',
     suffix: {
       type: 'select',
       name: 'ttlUnit',
