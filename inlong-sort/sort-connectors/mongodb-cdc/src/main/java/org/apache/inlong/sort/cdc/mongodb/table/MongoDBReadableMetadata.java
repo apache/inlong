6a677fd68c3303157537802a0f9276dccf1518ae
emhui <emhui@foxmail.com>
2023-05-06 15:19:04 +0800

    [INLONG-7958][Sort] Fix MongoDB's schema becomes unordered after extracting the row data

--- inlong-sort/sort-connectors/mongodb-cdc/src/main/java/org/apache/inlong/sort/cdc/mongodb/table/MongoDBReadableMetadata.java
+++ inlong-sort/sort-connectors/mongodb-cdc/src/main/java/org/apache/inlong/sort/cdc/mongodb/table/MongoDBReadableMetadata.java
@@ -17,31 +17,22 @@
 
 package org.apache.inlong.sort.cdc.mongodb.table;
 
+import static org.apache.inlong.sort.cdc.mongodb.source.utils.MetaDataUtils.getCanalData;
+import static org.apache.inlong.sort.cdc.mongodb.source.utils.MetaDataUtils.getDebeziumData;
+import static org.apache.inlong.sort.cdc.mongodb.source.utils.MetaDataUtils.getMetaData;
+
 import com.ververica.cdc.connectors.mongodb.internal.MongoDBEnvelope;
 import io.debezium.connector.AbstractSourceInfo;
 import io.debezium.data.Envelope;
-import io.debezium.relational.Table;
 import io.debezium.relational.history.TableChanges;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import javax.annotation.Nullable;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.data.StringData;
 import org.apache.flink.table.data.TimestampData;
 import org.apache.flink.table.types.DataType;
 import org.apache.inlong.sort.cdc.mongodb.debezium.table.MetadataConverter;
-import org.apache.inlong.sort.cdc.mongodb.debezium.utils.RecordUtils;
-import org.apache.inlong.sort.formats.json.canal.CanalJson;
-import org.apache.inlong.sort.formats.json.debezium.DebeziumJson;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 
@@ -57,10 +48,7 @@ public enum MongoDBReadableMetadata {
 
                 @Override
                 public Object read(SourceRecord record) {
-                    Struct value = (Struct) record.value();
-                    Struct to = value.getStruct(MongoDBEnvelope.NAMESPACE_FIELD);
-                    return StringData.fromString(
-                            to.getString(MongoDBEnvelope.NAMESPACE_COLLECTION_FIELD));
+                    return getMetaData(record, MongoDBEnvelope.NAMESPACE_COLLECTION_FIELD);
                 }
             }),
 
@@ -73,10 +61,7 @@ public enum MongoDBReadableMetadata {
 
                 @Override
                 public Object read(SourceRecord record) {
-                    Struct value = (Struct) record.value();
-                    Struct to = value.getStruct(MongoDBEnvelope.NAMESPACE_FIELD);
-                    return StringData.fromString(
-                            to.getString(MongoDBEnvelope.NAMESPACE_DATABASE_FIELD));
+                    return getMetaData(record, MongoDBEnvelope.NAMESPACE_DATABASE_FIELD);
                 }
             }),
 
@@ -111,40 +96,8 @@ public enum MongoDBReadableMetadata {
                 @Override
                 public Object read(SourceRecord record,
                         @Nullable TableChanges.TableChange tableSchema, RowData rowData) {
-
                     // construct debezium json
-                    Struct messageStruct = (Struct) record.value();
-                    Struct to = messageStruct.getStruct(MongoDBEnvelope.NAMESPACE_FIELD);
-                    Struct sourceStruct = messageStruct.getStruct(Envelope.FieldName.SOURCE);
-                    GenericRowData data = (GenericRowData) rowData;
-                    Map<String, Object> field = (Map<String, Object>) data.getField(0);
-                    Map<String, String> mysqlType = (Map<String, String>) data.getField(1);
-                    Map<String, Integer> sqlType = new HashMap<>();
-                    mysqlType.forEach((name, value) -> sqlType.put(name, RecordUtils.getSqlType(value)));
-                    String debeziumOp = getDebeziumOpType(rowData);
-                    if (StringUtils.isBlank(debeziumOp)) {
-                        return null;
-                    }
-                    DebeziumJson.Source source = DebeziumJson.Source.builder()
-                            .db(to.getString(MongoDBEnvelope.NAMESPACE_DATABASE_FIELD))
-                            .table(to.getString(MongoDBEnvelope.NAMESPACE_COLLECTION_FIELD))
-                            .name("mongo_binlog_source")
-                            .mysqlType(mysqlType)
-                            .sqlType(sqlType)
-                            .pkNames(null)
-                            .build();
-                    DebeziumJson debeziumJson = DebeziumJson.builder()
-                            .source(source)
-                            .after(field)
-                            .tsMs((Long) sourceStruct.get(AbstractSourceInfo.TIMESTAMP_KEY))
-                            .op(debeziumOp)
-                            .tableChange(tableSchema)
-                            .build();
-                    try {
-                        return StringData.fromString(OBJECT_MAPPER.writeValueAsString(debeziumJson));
-                    } catch (Exception e) {
-                        throw new IllegalStateException("exception occurs when get meta data", e);
-                    }
+                    return getDebeziumData(record, tableSchema, rowData);
                 }
             }),
 
@@ -164,107 +117,18 @@ public enum MongoDBReadableMetadata {
                 public Object read(SourceRecord record,
                         @Nullable TableChanges.TableChange tableSchema, RowData rowData) {
                     // construct canal json
-                    Struct messageStruct = (Struct) record.value();
-                    Struct to = messageStruct.getStruct(MongoDBEnvelope.NAMESPACE_FIELD);
-                    Struct sourceStruct = messageStruct.getStruct(Envelope.FieldName.SOURCE);
-                    String canalOp = getCanalOpType(rowData);
-                    if (StringUtils.isBlank(canalOp)) {
-                        return null;
-                    }
-                    GenericRowData data = (GenericRowData) rowData;
-                    Map<String, Object> field = (Map<String, Object>) data.getField(0);
-                    Map<String, String> mysqlType = (Map<String, String>) data.getField(1);
-                    Map<String, Integer> sqlType = new HashMap<>();
-                    mysqlType.forEach((name, value) -> sqlType.put(name, RecordUtils.getSqlType(value)));
-                    List<Map<String, Object>> dataList = new ArrayList<>();
-                    dataList.add(field);
-                    CanalJson canalJson = CanalJson.builder()
-                            .data(dataList)
-                            .database(to.getString(MongoDBEnvelope.NAMESPACE_DATABASE_FIELD))
-                            .sql("")
-                            .es((Long) sourceStruct.get(AbstractSourceInfo.TIMESTAMP_KEY))
-                            .isDdl(false)
-                            .pkNames(null)
-                            .mysqlType(getMysqlType(tableSchema))
-                            .table(to.getString(MongoDBEnvelope.NAMESPACE_COLLECTION_FIELD))
-                            .ts((Long) sourceStruct.get(AbstractSourceInfo.TIMESTAMP_KEY))
-                            .type(canalOp)
-                            .sqlType(sqlType)
-                            .build();
-                    try {
-                        return StringData.fromString(OBJECT_MAPPER.writeValueAsString(canalJson));
-                    } catch (Exception e) {
-                        throw new IllegalStateException("exception occurs when get meta data", e);
-                    }
+                    return getCanalData(record, rowData, tableSchema);
                 }
             });
 
     private final String key;
     private final DataType dataType;
     private final MetadataConverter converter;
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     MongoDBReadableMetadata(String key, DataType dataType, MetadataConverter converter) {
         this.key = key;
         this.dataType = dataType;
         this.converter = converter;
-    }
-
-    public static Map<String, String> getMysqlType(@Nullable TableChanges.TableChange tableSchema) {
-        if (tableSchema == null) {
-            return null;
-        }
-        Map<String, String> mysqlType = new LinkedHashMap<>();
-        final Table table = tableSchema.getTable();
-        table.columns()
-                .forEach(
-                        column -> {
-                            mysqlType.put(
-                                    column.name(),
-                                    String.format(
-                                            "%s(%d)",
-                                            column.typeName(),
-                                            column.length()));
-                        });
-        return mysqlType;
-    }
-
-    private static String getDebeziumOpType(RowData rowData) {
-        String opType = null;
-        switch (rowData.getRowKind()) {
-            case INSERT:
-                opType = "c";
-                break;
-            case DELETE:
-                opType = "d";
-                break;
-            case UPDATE_AFTER:
-            case UPDATE_BEFORE:
-                opType = "u";
-                break;
-            default:
-                return null;
-        }
-        return opType;
-    }
-
-    private static String getCanalOpType(RowData rowData) {
-        String opType = null;
-        switch (rowData.getRowKind()) {
-            case INSERT:
-                opType = "INSERT";
-                break;
-            case DELETE:
-                opType = "DELETE";
-                break;
-            case UPDATE_AFTER:
-            case UPDATE_BEFORE:
-                opType = "UPDATE";
-                break;
-            default:
-                return null;
-        }
-        return opType;
     }
 
     public String getKey() {
