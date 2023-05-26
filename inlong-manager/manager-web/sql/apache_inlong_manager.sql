e4acb035fe603150b97a00e7109b016c4a305d57
vernedeng <dengfan@pku.edu.cn>
2023-05-26 10:58:14 +0800

    [INLONG-8087][Manager] Add definition of Inlong tenant table (#8090)

--- inlong-manager/manager-web/sql/apache_inlong_manager.sql
+++ inlong-manager/manager-web/sql/apache_inlong_manager.sql
@@ -789,6 +789,31 @@ CREATE TABLE IF NOT EXISTS `stream_heartbeat`
   DEFAULT CHARSET = utf8 COMMENT ='Inlong stream heartbeat';
 
 -- ----------------------------
+-- Table structure for inlong_tenant
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `inlong_tenant`
+(
+    `id`           int(11)      NOT NULL AUTO_INCREMENT,
+    `name`         varchar(256) NOT NULL COMMENT 'Namespace, not support modification',
+    `description`  varchar(256) DEFAULT '' COMMENT 'Description of tenant',
+    `is_deleted`   int(11)      DEFAULT '0' COMMENT 'Whether to delete, 0 is not deleted, if greater than 0, delete',
+    `creator`      varchar(256) NOT NULL COMMENT 'Creator name',
+    `modifier`     varchar(256) DEFAULT NULL COMMENT 'Modifier name',
+    `create_time`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Create time',
+    `modify_time`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Modify time',
+    `version`      int(11)      NOT NULL DEFAULT '1' COMMENT 'Version number, which will be incremented by 1 after modification',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_user_role_key` (`name`, `is_deleted`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='Inlong tenant table';
+
+-- ----------------------------
+-- Insert inlong_tenant item
+-- ----------------------------
+INSERT INTO `inlong_tenant`(`name`, `description`, `creator`, `modifier`)
+VALUES ('public', 'Default tenant', 'admin', 'admin');
+
+-- ----------------------------
 -- Table structure for audit_base
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `audit_base`
