b3d51d75031699c9f00c5a891903cfdd789a7cab
e-mhui <emhui@foxmail.com>
2023-05-31 16:50:57 +0800

    [INLONG-8125][Sort] Sort by chunkId

--- inlong-sort/sort-flink/sort-flink-v1.13/sort-connectors/mysql-cdc/src/main/java/org/apache/inlong/sort/cdc/mysql/source/assigners/MySqlHybridSplitAssigner.java
+++ inlong-sort/sort-flink/sort-flink-v1.13/sort-connectors/mysql-cdc/src/main/java/org/apache/inlong/sort/cdc/mysql/source/assigners/MySqlHybridSplitAssigner.java
@@ -189,7 +189,8 @@ public class MySqlHybridSplitAssigner implements MySqlSplitAssigner {
     private MySqlBinlogSplit createBinlogSplit() {
         final List<MySqlSchemalessSnapshotSplit> assignedSnapshotSplit =
                 snapshotSplitAssigner.getAssignedSplits().values().stream()
-                        .sorted(Comparator.comparing(MySqlSplit::splitId))
+                        .sorted(Comparator.comparing(split ->
+                                Integer.parseInt(split.splitId().substring(split.splitId().lastIndexOf(":") + 1))))
                         .collect(Collectors.toList());
 
         Map<String, BinlogOffset> splitFinishedOffsets =
