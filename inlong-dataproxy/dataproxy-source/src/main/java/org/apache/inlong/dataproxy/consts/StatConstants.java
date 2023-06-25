ec135b1aae982020626ee3f8a02c7641f4a6c08a
Goson Zhang <4675739@qq.com>
2023-06-25 18:24:42 +0800

    [INLONG-8311][DataProxy] Add event handling support for FlumeEvent type (#8312)

--- inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/consts/StatConstants.java
+++ inlong-dataproxy/dataproxy-source/src/main/java/org/apache/inlong/dataproxy/consts/StatConstants.java
@@ -78,6 +78,9 @@ public class StatConstants {
     public static final java.lang.String EVENT_MSG_V1_POST_SUCCESS = "msg.post.v1.success";
     public static final java.lang.String EVENT_MSG_V1_POST_DROPPED = "msg.post.v1.dropped";
     // sink
+    public static final java.lang.String EVENT_SINK_EVENT_V1_MALFORMED = "sink.event.v1.malformed";
+    public static final java.lang.String EVENT_SINK_EVENT_V1_FILE = "sink.event.v1.file";
+    public static final java.lang.String EVENT_SINK_EVENT_V0_FILE = "sink.event.v1.file";
     public static final java.lang.String EVENT_SINK_CONFIG_TOPIC_MISSING = "sink.topic.missing";
     public static final java.lang.String EVENT_SINK_DEFAULT_TOPIC_MISSING = "default.topic.empty";
     public static final java.lang.String EVENT_SINK_DEFAULT_TOPIC_USED = "default.topic.used";
