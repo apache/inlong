896af2fbf8ce8937693fdd9347088e34125b4121
ryanrliao <liaorui0609@gmail.com>
2023-06-27 16:27:30 +0800

    iceberg connector supports decimal type with correct precision and scale from source cdc like mysql or oracle

--- inlong-sort/sort-flink/base/src/main/java/org/apache/inlong/sort/base/format/JsonDynamicSchemaFormat.java
+++ inlong-sort/sort-flink/base/src/main/java/org/apache/inlong/sort/base/format/JsonDynamicSchemaFormat.java
@@ -17,6 +17,7 @@
 
 package org.apache.inlong.sort.base.format;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.ReadableConfig;
 import org.apache.flink.formats.common.TimestampFormat;
@@ -52,6 +53,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.apache.inlong.sort.base.Constants.SINK_MULTIPLE_TYPE_MAP_COMPATIBLE_WITH_SPARK;
 
@@ -78,6 +80,12 @@ public abstract class JsonDynamicSchemaFormat extends AbstractDynamicSchemaForma
 
     private static final int DEFAULT_DECIMAL_PRECISION = 15;
     private static final int DEFAULT_DECIMAL_SCALE = 5;
+    /**
+     * dialect sql type pattern such as DECIMAL(38, 10) from mysql or oracle etc
+     */
+    private static final Pattern DIALECT_SQL_TYPE_PATTERN = Pattern.compile("\\w+\\(([\\d,\\s]*)\\)");
+
+    private static final Integer ORACLE_TIMESTAMP_TIME_ZONE = -101;
 
     private static final Map<Integer, LogicalType> SQL_TYPE_2_FLINK_TYPE_MAPPING =
             ImmutableMap.<Integer, LogicalType>builder()
@@ -95,6 +103,7 @@ public abstract class JsonDynamicSchemaFormat extends AbstractDynamicSchemaForma
                     .put(java.sql.Types.TIME, new TimeType())
                     .put(java.sql.Types.TIME_WITH_TIMEZONE, new TimeType())
                     .put(java.sql.Types.TIMESTAMP_WITH_TIMEZONE, new LocalZonedTimestampType())
+                    .put(ORACLE_TIMESTAMP_TIME_ZONE, new LocalZonedTimestampType())
                     .put(java.sql.Types.TIMESTAMP, new TimestampType())
                     .put(java.sql.Types.BINARY, new BinaryType())
                     .put(java.sql.Types.VARBINARY, new VarBinaryType())
@@ -127,12 +136,20 @@ public abstract class JsonDynamicSchemaFormat extends AbstractDynamicSchemaForma
                     .put(java.sql.Types.BIT, new BooleanType())
                     .put(java.sql.Types.TIME, new VarCharType())
                     .put(java.sql.Types.TIMESTAMP_WITH_TIMEZONE, new LocalZonedTimestampType())
+                    .put(ORACLE_TIMESTAMP_TIME_ZONE, new LocalZonedTimestampType())
                     .put(java.sql.Types.TIMESTAMP, new LocalZonedTimestampType())
                     .put(java.sql.Types.BINARY, new BinaryType())
                     .put(java.sql.Types.VARBINARY, new VarBinaryType())
                     .put(java.sql.Types.BLOB, new VarBinaryType())
                     .put(java.sql.Types.DATE, new DateType())
                     .put(java.sql.Types.BOOLEAN, new BooleanType())
+                    .put(java.sql.Types.LONGNVARCHAR, new VarCharType())
+                    .put(java.sql.Types.LONGVARBINARY, new VarCharType())
+                    .put(java.sql.Types.LONGVARCHAR, new VarCharType())
+                    .put(java.sql.Types.ARRAY, new VarCharType())
+                    .put(java.sql.Types.NCHAR, new CharType())
+                    .put(java.sql.Types.NCLOB, new VarBinaryType())
+                    .put(java.sql.Types.TINYINT, new TinyIntType())
                     .put(java.sql.Types.OTHER, new VarCharType())
                     .build();
 
@@ -321,19 +338,56 @@ public abstract class JsonDynamicSchemaFormat extends AbstractDynamicSchemaForma
      */
     public abstract String getOpType(JsonNode root);
 
-    protected RowType extractSchemaNode(JsonNode schema, List<String> pkNames) {
+    protected RowType extractSchemaNode(JsonNode schema, JsonNode dialectSchema, List<String> pkNames) {
         Iterator<Entry<String, JsonNode>> schemaFields = schema.fields();
         List<RowField> fields = new ArrayList<>();
         while (schemaFields.hasNext()) {
             Entry<String, JsonNode> entry = schemaFields.next();
             String name = entry.getKey();
             LogicalType type = sqlType2FlinkType(entry.getValue().asInt());
+            String dialectType = dialectSchema.get(name) != null ? dialectSchema.get(name).asText() : null;
+            type = handleDialectSqlType(type, dialectType);
             if (pkNames.contains(name)) {
                 type = type.copy(false);
             }
             fields.add(new RowField(name, type));
         }
         return new RowType(fields);
+    }
+
+    /**
+     * set precision and scale for decimal and other types
+     * @param type
+     * @param dialectType
+     * @return
+     */
+    public LogicalType handleDialectSqlType(LogicalType type, String dialectType) {
+        if (StringUtils.isBlank(dialectType)) {
+            return type;
+        }
+        if (type instanceof DecimalType) {
+            Matcher matcher = DIALECT_SQL_TYPE_PATTERN.matcher(dialectType);
+            int precision;
+            int scale;
+            DecimalType decimalType = new DecimalType(DecimalType.MAX_PRECISION);
+            if (matcher.matches()) {
+                String[] items = matcher.group(1).split(",");
+                precision = Integer.parseInt(items[0].trim());
+                if (precision < DecimalType.MIN_PRECISION || precision > DecimalType.MAX_PRECISION) {
+                    return decimalType;
+                }
+                if (items.length == 2) {
+                    scale = Integer.parseInt(items[1].trim());
+                    if (scale < DecimalType.MIN_SCALE || scale > precision) {
+                        return new DecimalType(precision);
+                    }
+                    return new DecimalType(precision, scale);
+                }
+                return new DecimalType(precision);
+            }
+            return decimalType;
+        }
+        return type;
     }
 
     public LogicalType sqlType2FlinkType(int jdbcType) {
