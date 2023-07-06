5597a99771cdde3ab8aebdd66774a78d02f1707a
v_haifxu <xhf1208357848@gmail.com>
2023-07-06 19:26:15 +0800

    [INLONG-8264][Manager] modify startup.sh

--- inlong-manager/manager-web/bin/startup.sh
+++ inlong-manager/manager-web/bin/startup.sh
@@ -55,7 +55,7 @@ BASE_PATH=$(pwd)
 # If you specify a directory, spring will read all configuration files in the directory
 FLINK_VERSION=$(grep "^flink.version=" ${BASE_PATH}"/plugins/flink-sort-plugin.properties | awk -F= '{print $2}')
 CONFIG_DIR=${BASE_PATH}"/conf/"
-JAR_LIBS=${BASE_PATH}"/lib/*:"${BASE_PATH}"/plugins/flink-v"${FLINK_VERSION}
+JAR_LIBS=${BASE_PATH}"/lib/*:"${BASE_PATH}"/plugins/flink-v"${FLINK_VERSION}"/*"
 JAR_MAIN=${BASE_PATH}"/lib/"${APPLICATION_JAR}
 CLASSPATH=${CONFIG_DIR}:${JAR_LIBS}:${JAR_MAIN}
 MAIN_CLASS=org.apache.inlong.manager.web.InlongManagerMain
