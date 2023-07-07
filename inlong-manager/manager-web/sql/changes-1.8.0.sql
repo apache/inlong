094461bebf43f82f09a99a4e5ee99875faa8ce7d
megruzhao <961889896@qq.com>
2023-07-07 23:16:37 +0800

    [INLONG-8405][Manager] modified changes-1.8.0.sql.

--- inlong-manager/manager-web/sql/changes-1.8.0.sql
+++ inlong-manager/manager-web/sql/changes-1.8.0.sql
@@ -136,11 +136,12 @@ DROP INDEX unique_stream_heartbeat on stream_heartbeat;
 CREATE TABLE IF NOT EXISTS `audit_source`
 (
     `id`          int(11)      NOT NULL AUTO_INCREMENT,
-    `type`        varchar(256) NOT NULL COMMENT 'Source type, including: MYSQL, CLICKHOUSE, ELASTICSEARCH',
-    `url`         varchar(256) NOT NULL COMMENT 'Source URL, for MYSQL or CLICKHOUSE, is jdbcUrl, and for ELASTICSEARCH is the access URL with hostname:port',
+    `name`        varchar(128) NOT NULL COMMENT 'Audit source name',
+    `type`        varchar(256) NOT NULL COMMENT 'Audit source type, including: MYSQL, CLICKHOUSE, ELASTICSEARCH',
+    `url`         varchar(256) NOT NULL COMMENT 'Audit source URL, for MYSQL or CLICKHOUSE, is jdbcUrl, and for ELASTICSEARCH is the access URL with hostname:port',
     `enable_auth` tinyint(1)            DEFAULT '1' COMMENT 'Enable auth or not, 0: disable, 1: enable',
-    `username`    varchar(256) NOT NULL COMMENT 'Source username, needed if auth_enable is 1' ,
-    `password`    varchar(256) NOT NULL COMMENT 'Source password, needed if auth_enable is 1',
+    `username`    varchar(128)          COMMENT 'Source username, needed if auth_enable is 1' ,
+    `password`    varchar(256)          COMMENT 'Source password, needed if auth_enable is 1',
     `status`      smallint(4)  NOT NULL DEFAULT '1' COMMENT 'Whether the audit source is online or offline, 0: offline, 1: online' ,
     `is_deleted`  int(11)               DEFAULT '0' COMMENT 'Whether to delete, 0: not deleted, > 0: deleted',
     `creator`     varchar(64)  NOT NULL COMMENT 'Creator name',
