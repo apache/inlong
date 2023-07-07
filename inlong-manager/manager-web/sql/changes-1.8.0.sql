37c046a72d95819f90655cd3011a6b19a4325810
fuwenkai <834260992@qq.com>
2023-07-07 10:04:11 +0800

    [INLONG-8449][Manager] Fix comment

--- inlong-manager/manager-web/sql/changes-1.8.0.sql
+++ inlong-manager/manager-web/sql/changes-1.8.0.sql
@@ -124,9 +124,13 @@ ALTER TABLE `component_heartbeat` ADD PRIMARY KEY (`component`, `instance`);
 DROP INDEX unique_component_heartbeat on component_heartbeat;
 
 ALTER TABLE group_heartbeat DROP COLUMN id;
+
 ALTER TABLE `group_heartbeat` ADD PRIMARY KEY  (`component`, `instance`, `inlong_group_id`);
+
 DROP INDEX unique_group_heartbeat on group_heartbeat;
 
 ALTER TABLE stream_heartbeat DROP COLUMN id;
+
 ALTER TABLE `stream_heartbeat` ADD PRIMARY KEY (`component`, `instance`, `inlong_group_id`, `inlong_stream_id`);
+
 DROP INDEX unique_stream_heartbeat on stream_heartbeat;
