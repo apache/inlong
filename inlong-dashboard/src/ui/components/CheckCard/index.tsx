78d89f4999e3e0696224ee1b93885fe47f01603f
richardjji <richardjji@tencent.com>
2023-08-19 16:36:48 +0800

    [INLONG-8008][Dashboard] Add default icon for different data nodes

--- inlong-dashboard/src/ui/components/CheckCard/index.tsx
+++ inlong-dashboard/src/ui/components/CheckCard/index.tsx
@@ -40,6 +40,8 @@ const { useToken } = theme;
 const CheckCard: React.FC<CheckCardProps> = ({ options, value, onChange, disabled, span = 6 }) => {
   const [currentValue, setCurrentValue] = useState(value);
 
+  const [logoMap, setLogoMap] = useState({});
+
   const [isExpand, setExpandStatus] = useState(!Boolean(currentValue));
 
   const { token } = useToken();
@@ -52,6 +54,25 @@ const CheckCard: React.FC<CheckCardProps> = ({ options, value, onChange, disable
     // eslint-disable-next-line
   }, [value]);
 
+  useEffect(() => {
+    // vite 不支持 require.context, 故使用import()动态获取每个类型的logo，模块是否存在由promise判断，模块缓存由esm处理
+    (async () => {
+      setLogoMap(
+        (
+          await Promise.allSettled(options.map(option => import(`./logo/${option.label}.png`)))
+        ).reduce((res, item) => {
+          if (item.status === 'fulfilled') {
+            const {
+              value: { default: url },
+            } = item;
+            res[url.split('/').pop().split('.').shift()] = url;
+          }
+          return res;
+        }, {}),
+      );
+    })();
+  }, [options]);
+
   const handleCardClick = newValue => {
     setExpandStatus(false);
     if (newValue !== currentValue) {
@@ -62,8 +83,21 @@ const CheckCard: React.FC<CheckCardProps> = ({ options, value, onChange, disable
     }
   };
 
+  const renderContent = label => (
+    <div className={styles.cardInfo}>
+      {logoMap[label] ? (
+        <img height="100%" alt={label} src={logoMap[label]}></img>
+      ) : (
+        <>
+          <DatabaseOutlined style={{ fontSize: 20 }} />
+          <span>{label}</span>
+        </>
+      )}
+    </div>
+  );
+
   return (
-    <Row gutter={10} className={styles.cardRow}>
+    <Row gutter={15} className={styles.cardRow}>
       {!isExpand ? (
         <>
           <Col span={span} className={styles.cardCol}>
@@ -72,8 +106,7 @@ const CheckCard: React.FC<CheckCardProps> = ({ options, value, onChange, disable
               bodyStyle={{ textAlign: 'center' }}
               className={disabled ? styles.cardDisabled : ''}
             >
-              <DatabaseOutlined style={{ fontSize: 20 }} />
-              <div>{options.find(item => item.value === currentValue)?.label}</div>
+              {renderContent(options.find(item => item.value === currentValue)?.label)}
             </Card>
           </Col>
           {!disabled && (
@@ -94,7 +127,7 @@ const CheckCard: React.FC<CheckCardProps> = ({ options, value, onChange, disable
               onClick={() => handleCardClick(item.value)}
               style={item.value === currentValue ? { borderColor: token.colorPrimary } : {}}
             >
-              {item.label}
+              {renderContent(item.label)}
             </Card>
           </Col>
         ))
