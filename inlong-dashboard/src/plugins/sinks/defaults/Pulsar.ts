6a47a3f04df5d28d1f28a399bd908c63a3675997
wanglz <yydsk9@gmail.com>
2023-09-13 11:14:10 +0800

    Delete field mapping

--- inlong-dashboard/src/plugins/sinks/defaults/Pulsar.ts
+++ inlong-dashboard/src/plugins/sinks/defaults/Pulsar.ts
@@ -20,30 +20,12 @@
 import { DataWithBackend } from '@/plugins/DataWithBackend';
 import { RenderRow } from '@/plugins/RenderRow';
 import { RenderList } from '@/plugins/RenderList';
-import i18n from '@/i18n';
-import EditableTable from '@/ui/components/EditableTable';
 import { SinkInfo } from '../common/SinkInfo';
-import { sourceFields } from '../common/sourceFields';
 import NodeSelect from '@/ui/components/NodeSelect';
 
 const { I18n } = DataWithBackend;
 const { FieldDecorator, SyncField } = RenderRow;
 const { ColumnDecorator } = RenderList;
-
-const pulsarTargetTypes = [
-  'INT8',
-  'INT16',
-  'INT32',
-  'INT64',
-  'FLOAT',
-  'DOUBLE',
-  'BYTES',
-  'STRING',
-  'BOOLEAN',
-].map(item => ({
-  label: item,
-  value: item,
-}));
 
 export default class PulsarSink extends SinkInfo implements DataWithBackend, RenderRow, RenderList {
   @FieldDecorator({
@@ -109,85 +91,4 @@ export default class PulsarSink extends SinkInfo implements DataWithBackend, Ren
   @SyncField()
   @ColumnDecorator()
   dataNodeName: string;
-
-  @FieldDecorator({
-    type: EditableTable,
-    props: values => ({
-      size: 'small',
-      editing: ![110, 130].includes(values?.status),
-      columns: getFieldListColumns(values),
-      canBatchAdd: true,
-      upsertByFieldKey: true,
-    }),
-  })
-  sinkFieldList: Record<string, unknown>[];
 }
-
-const getFieldListColumns = sinkValues => {
-  return [
-    ...sourceFields,
-    {
-      title: `Pulsar${i18n.t('meta.Sinks.Pulsar.FieldName')}`,
-      dataIndex: 'fieldName',
-      initialValue: '',
-      rules: [
-        { required: true },
-        {
-          pattern: /^[a-z][0-9a-z_]*$/,
-          message: i18n.t('meta.Sinks.Pulsar.FieldNameRule'),
-        },
-      ],
-      props: (text, record, idx, isNew) => ({
-        disabled: [110, 130].includes(sinkValues?.status as number) && !isNew,
-      }),
-    },
-    {
-      title: `Pulsar${i18n.t('meta.Sinks.Pulsar.FieldType')}`,
-      dataIndex: 'fieldType',
-      initialValue: pulsarTargetTypes[0].value,
-      type: 'select',
-      props: (text, record, idx, isNew) => ({
-        disabled: [110, 130].includes(sinkValues?.status as number) && !isNew,
-        options: pulsarTargetTypes,
-      }),
-      rules: [{ required: true, message: `${i18n.t('meta.Sinks.FieldTypeMessage')}` }],
-    },
-    {
-      title: i18n.t('meta.Sinks.Pulsar.IsMetaField'),
-      dataIndex: 'isMetaField',
-      initialValue: 0,
-      type: 'select',
-      props: {
-        options: [
-          {
-            label: i18n.t('basic.Yes'),
-            value: 1,
-          },
-          {
-            label: i18n.t('basic.No'),
-            value: 0,
-          },
-        ],
-      },
-    },
-    {
-      title: i18n.t('meta.Sinks.Pulsar.FieldFormat'),
-      dataIndex: 'fieldFormat',
-      initialValue: 0,
-      type: 'autocomplete',
-      props: (text, record, idx, isNew) => ({
-        options: ['MICROSECONDS', 'MILLISECONDS', 'SECONDS', 'SQL', 'ISO_8601'].map(item => ({
-          label: item,
-          value: item,
-        })),
-      }),
-      visible: (text, record) =>
-        ['BIGINT', 'DATE', 'TIMESTAMP'].includes(record.fieldType as string),
-    },
-    {
-      title: i18n.t('meta.Sinks.Pulsar.FieldDescription'),
-      dataIndex: 'fieldComment',
-      initialValue: '',
-    },
-  ];
-};
