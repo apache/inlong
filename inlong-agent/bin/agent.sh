56286d00e3ab891500f6dbe9a7725fe3a01c3eb5
卢春亮 <946240095@qq.com>
2023-09-21 10:42:39 +0800

    [INLONG-8942][Distribution] Support merging multiple module jars to reduce distribution package size (#8943)

--- inlong-agent/bin/agent.sh
+++ inlong-agent/bin/agent.sh
@@ -18,6 +18,12 @@
 
 BASE_DIR=$(dirname $0)/..
 
+# Prepare common dependency
+ROOT_DIR=$BASE_DIR/..
+if [ -e $ROOT_DIR/bin/prepare_module_dependencys.sh ]; then
+    $ROOT_DIR/bin/prepare_module_dependencys.sh ./inlong-agent/lib
+fi
+
 source "${BASE_DIR}"/bin/agent-env.sh
 CONSOLE_OUTPUT_FILE="${LOG_DIR}/agent-out.log"
 
