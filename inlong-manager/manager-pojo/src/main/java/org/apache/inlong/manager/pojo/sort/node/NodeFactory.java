a487f76cf0644ba2eabd0281b3179d1bc4c57bce
printSomething <a617410048@outlook.com>
2023-10-05 15:35:39 +0800

    Resolving conflicts with master branches

--- inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sort/node/NodeFactory.java
+++ inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sort/node/NodeFactory.java
@@ -99,6 +99,7 @@ public class NodeFactory {
      */
     public static List<Node> addBuiltInField(StreamSource sourceInfo, StreamSink sinkInfo,
                                              List<TransformResponse> transformResponses, Map<String, StreamField> constantFieldMap) {
+
         ExtractNodeProvider extractNodeProvider = ExtractNodeProviderFactory.getExtractNodeProvider(
                 sourceInfo.getSourceType());
         LoadNodeProvider loadNodeProvider = LoadNodeProviderFactory.getLoadNodeProvider(sinkInfo.getSinkType());
