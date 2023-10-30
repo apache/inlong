52f9752b1d4b71aacffb6be06ab31bf830f38dc1
wenweihuang <hww_justin@163.com>
2023-10-30 09:48:28 +0800

    [INLONG-9143][Agent] Add log file collect task
    fix ut error

--- inlong-agent/agent-plugins/src/test/java/org/apache/inlong/agent/plugin/task/TestLogfileCollectTask.java
+++ inlong-agent/agent-plugins/src/test/java/org/apache/inlong/agent/plugin/task/TestLogfileCollectTask.java
@@ -74,9 +74,9 @@ public class TestLogfileCollectTask {
     public static void setup() {
         helper = new AgentBaseTestsHelper(TestLogfileCollectTask.class.getName()).setupAgentHome();
         Db basicDb = TaskManager.initDb("/localdb");
-        resourceName = LOADER.getResource("test/20230928_1.log").getPath();
+        resourceName = LOADER.getResource("test/20230928_1.txt").getPath();
         File f = new File(resourceName);
-        String pattern = f.getParent() + "/YYYYMMDD_[0-9]+.log";
+        String pattern = f.getParent() + "/YYYYMMDD_[0-9]+.txt";
         TaskProfile taskProfile = helper.getTaskProfile(1, pattern, true, 0L, 0L, TaskStateEnum.RUNNING);
         try {
             String startStr = "2023-09-20 00:00:00";
