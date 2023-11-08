a03ade87b065b4f1535bedf94877c13c50d14f8a
vernedeng <vernedeng@apache.org>
2023-11-08 16:58:23 +0800

    fix

--- inlong-sort/sort-common/src/main/java/org/apache/inlong/sort/protocol/node/load/IcebergLoadNode.java
+++ inlong-sort/sort-common/src/main/java/org/apache/inlong/sort/protocol/node/load/IcebergLoadNode.java
@@ -43,7 +43,6 @@ import java.io.Serializable;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 @JsonTypeName("icebergLoad")
