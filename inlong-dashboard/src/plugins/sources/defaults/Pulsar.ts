d258808e3575debf37de4f8810bd8714a9659218
wanglz <yydsk9@gmail.com>
2023-12-19 17:04:38 +0800

    [INLONG-9497][Dashboard] Support Pulsar source management

---
+++ inlong-dashboard/src/plugins/sources/defaults/Pulsar.ts
@@ -0,0 +1,172 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { DataWithBackend } from '@/plugins/DataWithBackend';
+import { RenderRow } from '@/plugins/RenderRow';
+import { RenderList } from '@/plugins/RenderList';
+import { SourceInfo } from '../common/SourceInfo';
+
+const { I18n } = DataWithBackend;
+const { FieldDecorator, SyncField } = RenderRow;
+const { ColumnDecorator } = RenderList;
+
+export default class PulsarSource
+  extends SourceInfo
+  implements DataWithBackend, RenderRow, RenderList
+{
+  @FieldDecorator({
+    type: 'input',
+    rules: [{ required: true }],
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @ColumnDecorator()
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.PulsarTenant')
+  pulsarTenant: string;
+
+  @FieldDecorator({
+    type: 'input',
+    rules: [{ required: true }],
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.Namespace')
+  namespace: string;
+
+  @FieldDecorator({
+    type: 'input',
+    rules: [{ required: true }],
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @SyncField()
+  @I18n('Admin url')
+  adminUrl: string;
+
+  @FieldDecorator({
+    type: 'input',
+    rules: [{ required: true }],
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @SyncField()
+  @I18n('Service url')
+  serviceUrl: string;
+
+  @FieldDecorator({
+    type: 'input',
+    rules: [{ required: true }],
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @SyncField()
+  @I18n('Pulsar topic')
+  topic: string;
+
+  @FieldDecorator({
+    type: 'input',
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @ColumnDecorator()
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.PrimaryKey')
+  primaryKey: string;
+
+  @FieldDecorator({
+    type: 'radio',
+    initialValue: 'UTF-8',
+    props: values => ({
+      disabled: values?.status === 101,
+      options: [
+        {
+          label: 'UTF-8',
+          value: 'UTF-8',
+        },
+        {
+          label: 'GBK',
+          value: 'GBK',
+        },
+      ],
+    }),
+  })
+  @ColumnDecorator()
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.DataEncoding')
+  dataEncoding: string;
+
+  @FieldDecorator({
+    type: 'input',
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @ColumnDecorator()
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.DataSeparator')
+  dataSeparator: string;
+
+  @FieldDecorator({
+    type: 'input',
+    props: values => ({
+      disabled: values?.status === 101,
+    }),
+  })
+  @ColumnDecorator()
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.DataEscapeChar')
+  dataEscapeChar: string;
+
+  @FieldDecorator({
+    type: 'radio',
+    initialValue: 'INLONG_MSG_V0',
+    props: values => ({
+      disabled: values?.status === 101,
+      options: [
+        {
+          label: 'InLongMsg V0',
+          value: 'INLONG_MSG_V0',
+        },
+        {
+          label: 'InLongMsg V1',
+          value: 'INLONG_MSG_V1',
+        },
+        {
+          label: 'Raw',
+          value: 'RAW',
+        },
+        {
+          label: 'Etc',
+          value: 'etc',
+        },
+      ],
+    }),
+  })
+  @SyncField()
+  @I18n('meta.Sources.Pulsar.WrapType')
+  wrapType: string;
+}
