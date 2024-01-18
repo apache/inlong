68158bb2c1a8dd604a06a71a3f05735c01f02efd
fuwenkai <834260992@qq.com>
2024-01-18 11:27:19 +0800

    [INLONG-9586][Manager] Fix error

--- inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/cluster/node/InlongClusterNodeInstallOperatorFactory.java
+++ inlong-manager/manager-service/src/main/java/org/apache/inlong/manager/service/cluster/node/InlongClusterNodeInstallOperatorFactory.java
@@ -26,7 +26,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 /**
- * Factory for {@link InlongClusterNodeOperator}.
+ * Factory for {@link InlongClusterNodeInstallOperator}.
  */
 @Service
 public class InlongClusterNodeInstallOperatorFactory {
