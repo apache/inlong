693162d0c715bd225b318aa4e35e261b3c40d853
nicobao <nicobao@tencent.com>
2024-01-22 16:39:46 +0800

    [INLONG-9597][Sort] Support row way of sort InLong message tlog-csv format

---
+++ inlong-sort/sort-formats/format-row/format-inlongmsg-tlogcsv/src/main/java/org/apache/inlong/sort/formats/inlongmsgtlogcsv/InLongMsgTlogCsvMixedFormatDeserializer.java
@@ -0,0 +1,174 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.formats.inlongmsgtlogcsv;
+
+import org.apache.inlong.sort.formats.inlongmsg.AbstractInLongMsgMixedFormatDeserializer;
+import org.apache.inlong.sort.formats.inlongmsg.FailureHandler;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgBody;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgHead;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgTextMixedFormatDeserializerBuilder;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.table.descriptors.DescriptorProperties;
+import org.apache.flink.types.Row;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_DELIMITER;
+
+/**
+ * The deserializer for the records in InLongMsgTlogCsv format.
+ */
+public final class InLongMsgTlogCsvMixedFormatDeserializer
+        extends
+            AbstractInLongMsgMixedFormatDeserializer {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The charset of the text.
+     */
+    private final String charset;
+
+    /**
+     * The delimiter between fields.
+     */
+    @Nonnull
+    private final Character delimiter;
+
+    /**
+     * Escape character. Null if escaping is disabled.
+     */
+    @Nullable
+    private final Character escapeChar;
+
+    /**
+     * Quote character. Null if quoting is disabled.
+     */
+    @Nullable
+    private final Character quoteChar;
+
+    public InLongMsgTlogCsvMixedFormatDeserializer(
+            @Nonnull String charset,
+            @Nonnull Character delimiter,
+            @Nullable Character escapeChar,
+            @Nullable Character quoteChar,
+            @Nonnull Boolean ignoreErrors) {
+        this(charset, delimiter, escapeChar, quoteChar, InLongMsgUtils.getDefaultExceptionHandler(ignoreErrors));
+    }
+
+    public InLongMsgTlogCsvMixedFormatDeserializer(
+            @Nonnull String charset,
+            @Nonnull Character delimiter,
+            @Nullable Character escapeChar,
+            @Nullable Character quoteChar,
+            @Nonnull FailureHandler failureHandler) {
+        super(failureHandler);
+
+        this.delimiter = delimiter;
+        this.charset = charset;
+        this.escapeChar = escapeChar;
+        this.quoteChar = quoteChar;
+    }
+
+    @Override
+    public TypeInformation<Row> getProducedType() {
+        return InLongMsgUtils.MIXED_ROW_TYPE;
+    }
+
+    @Override
+    protected InLongMsgHead parseHead(String attr) throws Exception {
+        return InLongMsgTlogCsvUtils.parseHead(attr);
+    }
+
+    @Override
+    protected List<InLongMsgBody> parseBodyList(byte[] bytes) throws Exception {
+        return Collections.singletonList(
+                InLongMsgTlogCsvUtils.parseBody(bytes, charset, delimiter, escapeChar, quoteChar));
+    }
+
+    @Override
+    protected List<Row> convertRows(InLongMsgHead head, InLongMsgBody body) throws Exception {
+        Row row = InLongMsgUtils.buildMixedRow(head, body, body.getTid());
+        return Collections.singletonList(row);
+    }
+
+    /**
+     * The builder for {@link InLongMsgTlogCsvMixedFormatDeserializer}.
+     */
+    public static class Builder extends InLongMsgTextMixedFormatDeserializerBuilder<Builder> {
+
+        private Character delimiter;
+
+        public Builder setDelimiter(char delimiter) {
+            this.delimiter = delimiter;
+            return this;
+        }
+
+        @Override
+        public Builder configure(DescriptorProperties descriptorProperties) {
+            super.configure(descriptorProperties);
+
+            descriptorProperties.getOptionalCharacter(FORMAT_DELIMITER)
+                    .ifPresent(this::setDelimiter);
+
+            return this;
+        }
+
+        public InLongMsgTlogCsvMixedFormatDeserializer build() {
+            return new InLongMsgTlogCsvMixedFormatDeserializer(
+                    charset,
+                    delimiter,
+                    escapeChar,
+                    quoteChar,
+                    ignoreErrors);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+        InLongMsgTlogCsvMixedFormatDeserializer that = (InLongMsgTlogCsvMixedFormatDeserializer) o;
+        return charset.equals(that.charset) &&
+                delimiter.equals(that.delimiter) &&
+                Objects.equals(escapeChar, that.escapeChar) &&
+                Objects.equals(quoteChar, that.quoteChar);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), charset, delimiter, escapeChar,
+                quoteChar);
+    }
+}
---
+++ inlong-sort/sort-formats/format-row/format-inlongmsg-tlogcsv/src/test/java/org/apache/inlong/sort/formats/inlongmsgtlogcsv/InLongMsgTlogCsvFormatDeserializerTest.java
@@ -0,0 +1,285 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.formats.inlongmsgtlogcsv;
+
+import org.apache.inlong.common.msg.InLongMsg;
+import org.apache.inlong.sort.formats.common.FormatInfo;
+import org.apache.inlong.sort.formats.common.IntFormatInfo;
+import org.apache.inlong.sort.formats.common.RowFormatInfo;
+import org.apache.inlong.sort.formats.common.StringFormatInfo;
+import org.apache.inlong.sort.formats.inlongmsg.FailureHandler;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgBody;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgHead;
+
+import org.apache.flink.api.common.functions.util.ListCollector;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.Collector;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.DEFAULT_CHARSET;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.DEFAULT_DELIMITER;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.DEFAULT_ATTRIBUTES_FIELD_NAME;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.DEFAULT_TIME_FIELD_NAME;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for {@link InLongMsgTlogCsvFormatDeserializer}.
+ */
+public class InLongMsgTlogCsvFormatDeserializerTest {
+
+    private static final RowFormatInfo TEST_ROW_INFO =
+            new RowFormatInfo(
+                    new String[]{"f1", "f2", "f3", "f4", "f5"},
+                    new FormatInfo[]{
+                            IntFormatInfo.INSTANCE,
+                            IntFormatInfo.INSTANCE,
+                            StringFormatInfo.INSTANCE,
+                            StringFormatInfo.INSTANCE,
+                            StringFormatInfo.INSTANCE
+                    });
+
+    @Test
+    public void testExceptionHandler() throws Exception {
+        TestFailureHandler errorHandler = new TestFailureHandler();
+        InLongMsgTlogCsvFormatDeserializer deserializer =
+                new InLongMsgTlogCsvFormatDeserializer(
+                        TEST_ROW_INFO,
+                        DEFAULT_TIME_FIELD_NAME,
+                        DEFAULT_ATTRIBUTES_FIELD_NAME,
+                        DEFAULT_CHARSET,
+                        DEFAULT_DELIMITER,
+                        null,
+                        null,
+                        null,
+                        errorHandler);
+
+        InLongMsg inLongMsg1 = InLongMsg.newInLongMsg(true);
+        String attrs = "m=0&dt=1584806400000&__addcol1_=1&__addcol2_=test";
+        String body1 = "interfaceId1,field1,field2,field3";
+        String body2 = "interfaceId2,field1,field2,field3";
+        inLongMsg1.addMsg(attrs, body1.getBytes());
+        inLongMsg1.addMsg(attrs, body2.getBytes());
+
+        List<Row> actualRows = new ArrayList<>();
+        Collector<Row> collector = new ListCollector<>(actualRows);
+        deserializer.flatMap(inLongMsg1.buildArray(), collector);
+        assertEquals(2, errorHandler.getRowCount());
+
+        InLongMsg inLongMsg1Head = InLongMsg.newInLongMsg();
+        String abNormalAttrs = "m=0&__addcol1_=1&__addcol2_=test";
+        inLongMsg1Head.addMsg(abNormalAttrs, body1.getBytes());
+        inLongMsg1Head.addMsg(abNormalAttrs, body2.getBytes());
+        deserializer.flatMap(inLongMsg1Head.buildArray(), collector);
+        assertEquals(1, errorHandler.getHeadCount());
+    }
+
+    @Test
+    public void testNormal() throws Exception {
+        InLongMsg inLongMsg1 = InLongMsg.newInLongMsg(true);
+        String attrs = "m=0&dt=1584806400000&__addcol1_=1&__addcol2_=2";
+        String body1 = "interfaceId1,field1,field2,field3";
+        String body2 = "interfaceId2,field1,field2,field3";
+        inLongMsg1.addMsg(attrs, body1.getBytes());
+        inLongMsg1.addMsg(attrs, body2.getBytes());
+
+        Map<String, String> expectedAttributes = new HashMap<>();
+        expectedAttributes.put("m", "0");
+        expectedAttributes.put("dt", "1584806400000");
+        expectedAttributes.put("__addcol1_", "1");
+        expectedAttributes.put("__addcol2_", "2");
+
+        Row expectedRow1 = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "field1",
+                "field2",
+                "field3");
+
+        Row expectedRow2 = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "field1",
+                "field2",
+                "field3");
+
+        testRowDeserialization(
+                inLongMsg1.buildArray(),
+                Arrays.asList(expectedRow1, expectedRow2));
+    }
+
+    @Test
+    public void testDeleteHeadDelimiter() throws Exception {
+        InLongMsg inLongMsg1 = InLongMsg.newInLongMsg(true);
+        String attrs = "m=0&dt=1584806400000&__addcol1_=1&__addcol2_=2";
+        String body = ",interfaceId,field1,field2,field3";
+        inLongMsg1.addMsg(attrs, body.getBytes());
+
+        Map<String, String> expectedAttributes = new HashMap<>();
+        expectedAttributes.put("m", "0");
+        expectedAttributes.put("dt", "1584806400000");
+        expectedAttributes.put("__addcol1_", "1");
+        expectedAttributes.put("__addcol2_", "2");
+
+        Row expectedRow = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "field1",
+                "field2",
+                "field3");
+
+        testRowDeserialization(
+                inLongMsg1.buildArray(),
+                Collections.singletonList(expectedRow));
+    }
+
+    @Test
+    public void testUnmatchedFields1() throws Exception {
+        InLongMsg inLongMsg1 = InLongMsg.newInLongMsg(true);
+        String attrs = "m=0&dt=1584806400000&__addcol1_=1&__addcol2_=2";
+        String body1 = "interfaceId1,field1,field2";
+        String body2 = "interfaceId2,field1,field2,field3,field4";
+        inLongMsg1.addMsg(attrs, body1.getBytes());
+        inLongMsg1.addMsg(attrs, body2.getBytes());
+
+        Map<String, String> expectedAttributes = new HashMap<>();
+        expectedAttributes.put("m", "0");
+        expectedAttributes.put("dt", "1584806400000");
+        expectedAttributes.put("__addcol1_", "1");
+        expectedAttributes.put("__addcol2_", "2");
+
+        Row expectedRow1 = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "field1",
+                "field2",
+                null);
+
+        Row expectedRow2 = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "field1",
+                "field2",
+                "field3");
+
+        testRowDeserialization(
+                inLongMsg1.buildArray(),
+                Arrays.asList(expectedRow1, expectedRow2));
+    }
+
+    @Test
+    public void testUnmatchedFields2() throws Exception {
+        InLongMsg inLongMsg1 = InLongMsg.newInLongMsg(true);
+        String attrs = "m=0&dt=1584806400000&__addcol1_=1&__addcol2_=2&" +
+                "__addcol3_=3&__addcol4_=4&__addcol5_=5&__addcol6_=6";
+        String body = "interfaceId1,field1,field2,field3";
+        inLongMsg1.addMsg(attrs, body.getBytes());
+
+        Map<String, String> expectedAttributes = new HashMap<>();
+        expectedAttributes.put("m", "0");
+        expectedAttributes.put("dt", "1584806400000");
+        expectedAttributes.put("__addcol1_", "1");
+        expectedAttributes.put("__addcol2_", "2");
+        expectedAttributes.put("__addcol3_", "3");
+        expectedAttributes.put("__addcol4_", "4");
+        expectedAttributes.put("__addcol5_", "5");
+        expectedAttributes.put("__addcol6_", "6");
+
+        Row expectedRow = Row.of(
+                new Timestamp(1584806400000L),
+                expectedAttributes,
+                1,
+                2,
+                "3",
+                "4",
+                "5");
+
+        testRowDeserialization(
+                inLongMsg1.buildArray(),
+                Collections.singletonList(expectedRow));
+    }
+
+    private void testRowDeserialization(
+            byte[] bytes,
+            List<Row> expectedRows) throws Exception {
+        InLongMsgTlogCsvFormatDeserializer deserializer =
+                new InLongMsgTlogCsvFormatDeserializer.Builder(TEST_ROW_INFO)
+                        .setTimeFieldName("inlongmsg_time")
+                        .setAttributesFieldName("inlongmsg_attributes")
+                        .build();
+
+        List<Row> actualRows = new ArrayList<>();
+        Collector<Row> collector = new ListCollector<>(actualRows);
+
+        deserializer.flatMap(bytes, collector);
+
+        assertEquals(expectedRows, actualRows);
+    }
+
+    private static class TestFailureHandler implements FailureHandler {
+
+        private int headCount = 0;
+        private int bodyCount = 0;
+        private int rowCount = 0;
+
+        public int getHeadCount() {
+            return headCount;
+        }
+
+        public int getBodyCount() {
+            return bodyCount;
+        }
+
+        public int getRowCount() {
+            return rowCount;
+        }
+
+        @Override
+        public void onParsingHeadFailure(String attribute, Exception exception) throws Exception {
+            headCount++;
+        }
+
+        @Override
+        public void onParsingBodyFailure(byte[] body, Exception exception) throws Exception {
+            bodyCount++;
+        }
+
+        @Override
+        public void onConvertingRowFailure(InLongMsgHead head,
+                InLongMsgBody body, Exception exception) throws Exception {
+            rowCount++;
+        }
+    }
+}
