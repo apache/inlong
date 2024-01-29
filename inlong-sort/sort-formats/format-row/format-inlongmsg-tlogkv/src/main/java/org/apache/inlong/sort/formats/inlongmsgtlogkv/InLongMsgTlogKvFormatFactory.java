1b800932b0e46b0ed9a916360cbd626a929ab214
baomingyu <baomingyu_5@163.com>
2024-01-29 12:21:08 +0800

    [INLONG-9596][Sort] Support row way of sort InLong message tlog-kv format (#9620)

---
+++ inlong-sort/sort-formats/format-row/format-inlongmsg-tlogkv/src/main/java/org/apache/inlong/sort/formats/inlongmsgtlogkv/InLongMsgTlogKvFormatFactory.java
@@ -0,0 +1,163 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.formats.inlongmsgtlogkv;
+
+import org.apache.inlong.sort.formats.base.TableFormatDeserializer;
+import org.apache.inlong.sort.formats.base.TableFormatDeserializer.TableFormatContext;
+import org.apache.inlong.sort.formats.base.TableFormatDeserializerFactory;
+import org.apache.inlong.sort.formats.common.RowFormatInfo;
+import org.apache.inlong.sort.formats.inlongmsg.AbstractInLongMsgMixedFormatConverter;
+import org.apache.inlong.sort.formats.inlongmsg.AbstractInLongMsgMixedFormatDeserializer;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgMixedFormatConverterValidator;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgMixedFormatDeserializerValidator;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgMixedFormatFactory;
+
+import org.apache.flink.table.descriptors.DescriptorProperties;
+import org.apache.flink.table.factories.TableFormatFactoryBase;
+import org.apache.flink.types.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_CHARSET;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_DELIMITER;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_ENTRY_DELIMITER;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_ESCAPE_CHARACTER;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_IGNORE_ERRORS;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_KV_DELIMITER;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_NULL_LITERAL;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_QUOTE_CHARACTER;
+import static org.apache.inlong.sort.formats.base.TableFormatConstants.FORMAT_SCHEMA;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.FORMAT_ATTRIBUTES_FIELD_NAME;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.FORMAT_TIME_FIELD_NAME;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.getDataRowFormatInfo;
+import static org.apache.inlong.sort.formats.inlongmsgtlogkv.InLongMsgTlogKv.FORMAT_TYPE_VALUE;
+
+/**
+ * Table format factory for providing configured instances of InLongMsgTlogKv-to-row
+ * serializer and deserializer.
+ */
+public final class InLongMsgTlogKvFormatFactory
+        extends
+            TableFormatFactoryBase<Row>
+        implements
+            TableFormatDeserializerFactory,
+            InLongMsgMixedFormatFactory {
+
+    public InLongMsgTlogKvFormatFactory() {
+        super(FORMAT_TYPE_VALUE, 1, false);
+    }
+
+    @Override
+    public List<String> supportedFormatProperties() {
+        final List<String> properties = new ArrayList<>();
+        properties.add(FORMAT_CHARSET);
+        properties.add(FORMAT_DELIMITER);
+        properties.add(FORMAT_ENTRY_DELIMITER);
+        properties.add(FORMAT_KV_DELIMITER);
+        properties.add(FORMAT_ESCAPE_CHARACTER);
+        properties.add(FORMAT_QUOTE_CHARACTER);
+        properties.add(FORMAT_NULL_LITERAL);
+        properties.add(FORMAT_IGNORE_ERRORS);
+        properties.add(FORMAT_SCHEMA);
+        properties.add(FORMAT_TIME_FIELD_NAME);
+        properties.add(FORMAT_ATTRIBUTES_FIELD_NAME);
+        return properties;
+    }
+
+    @Override
+    public InLongMsgTlogKvFormatDeserializer createFormatDeserializer(
+            Map<String, String> properties) {
+        final DescriptorProperties descriptorProperties =
+                new DescriptorProperties(true);
+        descriptorProperties.putProperties(properties);
+
+        InLongMsgTlogKvValidator validator = new InLongMsgTlogKvValidator();
+        validator.validate(descriptorProperties);
+
+        RowFormatInfo rowFormatInfo = getDataRowFormatInfo(descriptorProperties);
+
+        InLongMsgTlogKvFormatDeserializer.Builder builder =
+                new InLongMsgTlogKvFormatDeserializer.Builder(rowFormatInfo);
+        builder.configure(descriptorProperties);
+
+        return builder.build();
+    }
+
+    @Override
+    public TableFormatDeserializer createFormatDeserializer(TableFormatContext context) {
+        TableFormatDeserializer deserializer =
+                createFormatDeserializer(context.getFormatProperties());
+        deserializer.init(context);
+        return deserializer;
+    }
+
+    @Override
+    public InLongMsgTlogKvMixedFormatDeserializer createMixedFormatDeserializer(
+            Map<String, String> properties) {
+        final DescriptorProperties descriptorProperties =
+                new DescriptorProperties(true);
+        descriptorProperties.putProperties(properties);
+
+        InLongMsgMixedFormatDeserializerValidator validator =
+                new InLongMsgMixedFormatDeserializerValidator();
+        validator.validate(descriptorProperties);
+
+        InLongMsgTlogKvMixedFormatDeserializer.Builder builder =
+                new InLongMsgTlogKvMixedFormatDeserializer.Builder();
+        builder.configure(descriptorProperties);
+
+        return builder.build();
+    }
+
+    @Override
+    public AbstractInLongMsgMixedFormatConverter createMixedFormatConverter(
+            AbstractInLongMsgMixedFormatConverter.TableFormatContext context) {
+        return createMixedFormatConverter(context.getFormatProperties());
+    }
+
+    @Override
+    public AbstractInLongMsgMixedFormatDeserializer createMixedFormatDeserializer(
+            TableFormatContext context) {
+        InLongMsgTlogKvMixedFormatDeserializer deserializer =
+                createMixedFormatDeserializer(context.getFormatProperties());
+        deserializer.init(context);
+        return deserializer;
+    }
+
+    @Override
+    public InLongMsgTlogKvMixedFormatConverter createMixedFormatConverter(
+            Map<String, String> properties) {
+        final DescriptorProperties descriptorProperties =
+                new DescriptorProperties(true);
+        descriptorProperties.putProperties(properties);
+
+        InLongMsgMixedFormatConverterValidator validator =
+                new InLongMsgMixedFormatConverterValidator();
+        validator.validate(descriptorProperties);
+
+        RowFormatInfo rowFormatInfo = getDataRowFormatInfo(descriptorProperties);
+
+        InLongMsgTlogKvMixedFormatConverter.Builder builder =
+                new InLongMsgTlogKvMixedFormatConverter.Builder(rowFormatInfo);
+        builder.configure(descriptorProperties);
+
+        return builder.build();
+    }
+}
---
+++ inlong-sort/sort-formats/format-row/format-inlongmsg-tlogkv/src/main/java/org/apache/inlong/sort/formats/inlongmsgtlogkv/InLongMsgTlogKvUtils.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.formats.inlongmsgtlogkv;
+
+import org.apache.inlong.sort.formats.common.FormatInfo;
+import org.apache.inlong.sort.formats.common.RowFormatInfo;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgBody;
+import org.apache.inlong.sort.formats.inlongmsg.InLongMsgHead;
+
+import org.apache.flink.types.Row;
+
+import java.nio.charset.Charset;
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.inlong.sort.formats.base.TableFormatUtils.deserializeBasicField;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.INLONGMSG_ATTR_TIME_DT;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.INLONGMSG_ATTR_TIME_T;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.getPredefinedFields;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.parseAttr;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.parseDateTime;
+import static org.apache.inlong.sort.formats.inlongmsg.InLongMsgUtils.parseEpochTime;
+import static org.apache.inlong.sort.formats.util.StringUtils.splitCsv;
+import static org.apache.inlong.sort.formats.util.StringUtils.splitKv;
+
+/**
+ * Utilities for {@link InLongMsgTlogKv}.
+ */
+public class InLongMsgTlogKvUtils {
+
+    public static final String DEFAULT_INLONGMSG_TLOGKV_CHARSET = "ISO_8859_1";
+
+    public static InLongMsgHead parseHead(String attr) {
+        Map<String, String> attributes = parseAttr(attr);
+
+        Timestamp time;
+        if (attributes.containsKey(INLONGMSG_ATTR_TIME_T)) {
+            String date = attributes.get(INLONGMSG_ATTR_TIME_T).trim();
+            time = parseDateTime(date);
+        } else if (attributes.containsKey(INLONGMSG_ATTR_TIME_DT)) {
+            String epoch = attributes.get(INLONGMSG_ATTR_TIME_DT).trim();
+            time = parseEpochTime(epoch);
+        } else {
+            throw new IllegalArgumentException(
+                    "Could not find " + INLONGMSG_ATTR_TIME_T +
+                            " or " + INLONGMSG_ATTR_TIME_DT + " in attributes!");
+        }
+
+        List<String> predefinedFields = getPredefinedFields(attributes);
+
+        return new InLongMsgHead(attributes, null, time, predefinedFields);
+    }
+
+    public static InLongMsgBody parseBody(
+            byte[] bytes,
+            String charset,
+            char delimiter,
+            char entryDelimiter,
+            char kvDelimiter,
+            Character escapeChar,
+            Character quoteChar) {
+        String text;
+        if (bytes[0] == delimiter) {
+            text = new String(bytes, 1, bytes.length - 1, Charset.forName(charset));
+        } else {
+            text = new String(bytes, Charset.forName(charset));
+        }
+
+        String[] segments = splitCsv(text, delimiter, escapeChar, quoteChar);
+
+        String tid = segments[0];
+
+        Map<String, String> entries;
+        if (segments.length > 1) {
+            entries = splitKv(segments[1], entryDelimiter, kvDelimiter, escapeChar, quoteChar);
+        } else {
+            entries = Collections.emptyMap();
+        }
+
+        return new InLongMsgBody(bytes, tid, Collections.emptyList(), entries);
+    }
+
+    /**
+     * Deserializes the row from the given entries.
+     *
+     * @param rowFormatInfo The format information of the row.
+     * @param nullLiteral The literal for null values.
+     * @param predefinedFields The predefined fields.
+     * @param entries The entries.
+     * @return The row deserialized from the given entries.
+     */
+    public static Row deserializeRow(
+            RowFormatInfo rowFormatInfo,
+            String nullLiteral,
+            List<String> predefinedFields,
+            Map<String, String> entries) {
+        String[] fieldNames = rowFormatInfo.getFieldNames();
+        FormatInfo[] fieldFormatInfos = rowFormatInfo.getFieldFormatInfos();
+
+        Row row = new Row(fieldNames.length);
+
+        for (int i = 0; i < predefinedFields.size(); ++i) {
+
+            if (i >= fieldNames.length) {
+                break;
+            }
+
+            String fieldName = fieldNames[i];
+            FormatInfo fieldFormatInfo = fieldFormatInfos[i];
+
+            String fieldText = predefinedFields.get(i);
+
+            Object field =
+                    deserializeBasicField(
+                            fieldName,
+                            fieldFormatInfo,
+                            fieldText,
+                            nullLiteral);
+            row.setField(i, field);
+        }
+
+        for (int i = predefinedFields.size(); i < fieldNames.length; ++i) {
+            String fieldName = fieldNames[i];
+            FormatInfo fieldFormatInfo = fieldFormatInfos[i];
+
+            String fieldText = entries.get(fieldName);
+
+            Object field =
+                    deserializeBasicField(
+                            fieldName,
+                            fieldFormatInfo,
+                            fieldText,
+                            nullLiteral);
+            row.setField(i, field);
+        }
+
+        return row;
+    }
+
+}
