27dc5020ecece6ffae3c8580303b7aa829ad7f42
wenweihuang <hww_justin@163.com>
2024-04-02 11:36:52 +0800

    [INLONG-9909][Agent] Add unit test for installer

---
+++ inlong-agent/agent-installer/src/test/java/installer/BaseTestsHelper.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package installer;
+
+import org.apache.inlong.agent.conf.TaskProfile;
+import org.apache.inlong.agent.constant.AgentConstants;
+import org.apache.inlong.agent.constant.FetcherConstants;
+import org.apache.inlong.agent.installer.conf.InstallerConfiguration;
+import org.apache.inlong.agent.pojo.FileTask.FileTaskConfig;
+import org.apache.inlong.common.enums.TaskStateEnum;
+import org.apache.inlong.common.pojo.agent.DataConfig;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * common environment setting up for test cases.
+ */
+public class BaseTestsHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTestsHelper.class);
+    private static final GsonBuilder gsonBuilder = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final Gson GSON = gsonBuilder.create();
+    private final String className;
+    private Path testRootDir;
+
+    public BaseTestsHelper(String className) {
+        this.className = className;
+    }
+
+    public BaseTestsHelper setupAgentHome() {
+        testRootDir = Paths
+                .get("/tmp", BaseTestsHelper.class.getSimpleName(), className);
+        teardownAgentHome();
+        boolean result = testRootDir.toFile().mkdirs();
+        LOGGER.info("try to create {}, result is {}", testRootDir, result);
+        InstallerConfiguration.getInstallerConf().set(AgentConstants.AGENT_HOME, testRootDir.toString());
+        InstallerConfiguration.getInstallerConf().set(FetcherConstants.AGENT_MANAGER_ADDR, "");
+        return this;
+    }
+
+    public Path getTestRootDir() {
+        return testRootDir;
+    }
+
+    public void teardownAgentHome() {
+        if (testRootDir != null) {
+            try {
+                FileUtils.deleteDirectory(testRootDir.toFile());
+            } catch (Exception ignored) {
+                LOGGER.warn("deleteDirectory error ", ignored);
+            }
+        }
+    }
+
+    public TaskProfile getTaskProfile(int taskId, String pattern, boolean retry, Long startTime, Long endTime,
+            TaskStateEnum state, String timeZone) {
+        DataConfig dataConfig = getDataConfig(taskId, pattern, retry, startTime, endTime, state, timeZone);
+        TaskProfile profile = TaskProfile.convertToTaskProfile(dataConfig);
+        return profile;
+    }
+
+    private DataConfig getDataConfig(int taskId, String pattern, boolean retry, Long startTime, Long endTime,
+            TaskStateEnum state, String timeZone) {
+        DataConfig dataConfig = new DataConfig();
+        dataConfig.setInlongGroupId("testGroupId");
+        dataConfig.setInlongStreamId("testStreamId");
+        dataConfig.setDataReportType(1);
+        dataConfig.setTaskType(3);
+        dataConfig.setTaskId(taskId);
+        dataConfig.setTimeZone(timeZone);
+        dataConfig.setState(state.ordinal());
+        FileTaskConfig fileTaskConfig = new FileTaskConfig();
+        fileTaskConfig.setPattern(pattern);
+        fileTaskConfig.setTimeOffset("0h");
+        fileTaskConfig.setMaxFileCount(100);
+        fileTaskConfig.setCycleUnit("h");
+        fileTaskConfig.setRetry(retry);
+        fileTaskConfig.setStartTime(startTime);
+        fileTaskConfig.setEndTime(endTime);
+        dataConfig.setExtParams(GSON.toJson(fileTaskConfig));
+        return dataConfig;
+    }
+}
---
+++ inlong-agent/agent-installer/src/test/java/installer/TestModuleManager.java
@@ -0,0 +1,204 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package installer;
+
+import org.apache.inlong.agent.installer.ModuleManager;
+import org.apache.inlong.common.pojo.agent.installer.ConfigResult;
+import org.apache.inlong.common.pojo.agent.installer.ModuleConfig;
+import org.apache.inlong.common.pojo.agent.installer.ModuleStateEnum;
+import org.apache.inlong.common.pojo.agent.installer.PackageConfig;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ModuleManager.class)
+@PowerMockIgnore({"javax.management.*"})
+public class TestModuleManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestModuleManager.class);
+    private static BaseTestsHelper helper;
+    private static final ClassLoader LOADER = TestModuleManager.class.getClassLoader();
+    private static ModuleManager manager;
+    private static List<Action> realActionList = new ArrayList<>();
+    private static List<Action> expectedActionList = new ArrayList<>();
+    private static String OLD_MD5 = "95648c83b45971dce503d5d844496cfc";
+    private static String NEW_MD5 = "e573f399da60ddeff09904bb95bdc307";
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Action {
+
+        public ModuleActionTypeEnum type;
+        public String md5;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Action action = (Action) o;
+            return Objects.equals(type, action.type) &&
+                    Objects.equals(md5, action.md5);
+        }
+    }
+
+    @BeforeClass
+    public static void setup() {
+        helper = new BaseTestsHelper(TestModuleManager.class.getName()).setupAgentHome();
+        manager = PowerMockito.spy(new ModuleManager());
+    }
+
+    @AfterClass
+    public static void teardown() throws Exception {
+        manager.stop();
+    }
+
+    private void fillExpectedActionList() {
+        expectedActionList.add(new Action(ModuleActionTypeEnum.STOP, OLD_MD5));
+        expectedActionList.add(new Action(ModuleActionTypeEnum.UNINSTALL, OLD_MD5));
+        expectedActionList.add(new Action(ModuleActionTypeEnum.DOWNLOAD, NEW_MD5));
+        expectedActionList.add(new Action(ModuleActionTypeEnum.INSTALL, NEW_MD5));
+        expectedActionList.add(new Action(ModuleActionTypeEnum.START, NEW_MD5));
+    }
+
+    private void mockFunctions() {
+        try {
+            PowerMockito.doAnswer(invocation -> {
+                ModuleConfig module = invocation.getArgument(0);
+                realActionList.add(new Action(ModuleActionTypeEnum.DOWNLOAD, module.getPackageConfig().getMd5()));
+                return true;
+            }).when(manager, "downloadModule", Mockito.any());
+
+            PowerMockito.doAnswer(invocation -> {
+                ModuleConfig module = invocation.getArgument(0);
+                realActionList.add(new Action(ModuleActionTypeEnum.INSTALL, module.getPackageConfig().getMd5()));
+                return true;
+            }).when(manager, "installModule", Mockito.any());
+
+            PowerMockito.doAnswer(invocation -> {
+                ModuleConfig module = invocation.getArgument(0);
+                realActionList.add(new Action(ModuleActionTypeEnum.UNINSTALL, module.getPackageConfig().getMd5()));
+                return true;
+            }).when(manager, "uninstallModule", Mockito.any());
+
+            PowerMockito.doAnswer(invocation -> {
+                ModuleConfig module = invocation.getArgument(0);
+                realActionList.add(new Action(ModuleActionTypeEnum.START, module.getPackageConfig().getMd5()));
+                return true;
+            }).when(manager, "startModule", Mockito.any());
+
+            PowerMockito.doAnswer(invocation -> {
+                ModuleConfig module = invocation.getArgument(0);
+                realActionList.add(new Action(ModuleActionTypeEnum.STOP, module.getPackageConfig().getMd5()));
+                return true;
+            }).when(manager, "stopModule", Mockito.any());
+
+            PowerMockito.doReturn(null).when(manager, "getHttpManager", Mockito.any());
+        } catch (Exception e) {
+            LOGGER.error("mock downloadModule error", e);
+            Assert.assertTrue(false);
+            return;
+        }
+    }
+
+    @Test
+    public void testModuleManager() {
+        fillExpectedActionList();
+        mockFunctions();
+        String confPath = LOADER.getResource("conf/").getPath();
+        manager.restoreFromLocalFile(confPath);
+        Assert.assertTrue(manager.getModule(1).getPackageConfig().getMd5().equals(OLD_MD5));
+        manager.submitConfig(getConfig());
+        try {
+            manager.start();
+        } catch (Exception e) {
+            LOGGER.error("start module manager error", e);
+            Assert.assertTrue("start module manager error", false);
+        }
+        await().atMost(3, TimeUnit.SECONDS).until(() -> manager.getModule(1) != null);
+        Assert.assertTrue(realActionList.size() == expectedActionList.size());
+        for (Integer i = 0; i < expectedActionList.size(); i++) {
+            LOGGER.info("{} {}", realActionList.get(i), expectedActionList.get(i));
+            Assert.assertTrue(i.toString(), realActionList.get(i).equals(expectedActionList.get(i)));
+        }
+    }
+
+    private ConfigResult getConfig() {
+        List<ModuleConfig> configs = new ArrayList<>();
+        configs.add(getModuleConfig(1, "inlong-agent", "inlong-agent-md5-185454", "1.0", 1,
+                "cd ~/inlong-agent/bin;sh agent.sh start", "cd ~/inlong-agent/bin;sh agent.sh stop",
+                "ps aux | grep core.AgentMain | grep java | grep -v grep | awk '{print $2}'",
+                "cd ~/inlong-agent/bin;sh agent.sh stop;rm -rf ~/inlong-agent/;mkdir ~/inlong-agent;cd /tmp;tar -xzvf agent-release-1.12.0-SNAPSHOT-bin.tar.gz -C ~/inlong-agent;cd ~/inlong-agent/bin;sh agent.sh start",
+                "echo empty uninstall cmd", "agent-release-1.12.0-SNAPSHOT-bin.tar.gz",
+                "http://11.151.252.111:8083/inlong/manager/openapi/agent/download/agent-release-1.12.0-SNAPSHOT-bin.tar.gz",
+                NEW_MD5));
+        return ConfigResult.builder().moduleList(configs).moduleNum(1).md5("config-result-md5-193603").build();
+    }
+
+    private ModuleConfig getModuleConfig(int id, String name, String md5, String version, Integer procNum,
+            String startCmd, String stopCmd, String checkCmd, String installCmd, String uninstallCmd, String fileName,
+            String downloadUrl,
+            String packageMd5) {
+        ModuleConfig moduleConfig = new ModuleConfig();
+        moduleConfig.setId(id);
+        moduleConfig.setName(name);
+        moduleConfig.setVersion(version);
+        moduleConfig.setMd5(md5);
+        moduleConfig.setProcessesNum(procNum);
+        moduleConfig.setStartCommand(startCmd);
+        moduleConfig.setStopCommand(stopCmd);
+        moduleConfig.setCheckCommand(checkCmd);
+        moduleConfig.setInstallCommand(installCmd);
+        moduleConfig.setUninstallCommand(uninstallCmd);
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setFileName(fileName);
+        packageConfig.setDownloadUrl(downloadUrl);
+        packageConfig.setStoragePath("/tmp");
+        packageConfig.setMd5(packageMd5);
+        moduleConfig.setPackageConfig(packageConfig);
+        moduleConfig.setState(ModuleStateEnum.NEW);
+        return moduleConfig;
+    }
+}
