19e75702d15a4549d8fff79c7167b81a980e6bcd
doleyzi <doleyzi@qq.com>
2024-04-02 16:06:26 +0800

    Audit-service add codes of config

---
+++ inlong-audit/audit-service/src/main/java/channel/DataQueue.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package channel;
+
+import entities.StatData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Data queue. use in source and sink.
+ */
+public class DataQueue {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DataQueue.class);
+
+    private final LinkedBlockingQueue<StatData> queue;
+
+    public DataQueue(int capacity) {
+        queue = new LinkedBlockingQueue<>(capacity);
+    }
+
+    /**
+     *  Push data
+     * @param statDataPo
+     */
+    public void push(StatData statDataPo) {
+        try {
+            queue.put(statDataPo);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Pull data
+     * @param timeout
+     * @param unit
+     * @return
+     */
+    public StatData pull(long timeout, TimeUnit unit) {
+        try {
+            StatData message = queue.poll(timeout, unit);
+            return message;
+        } catch (InterruptedException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    /**
+     * destroy
+     */
+    public void destroy() {
+        if (queue != null) {
+            queue.clear();
+        }
+        LOG.info("destroy channel!");
+    }
+}
