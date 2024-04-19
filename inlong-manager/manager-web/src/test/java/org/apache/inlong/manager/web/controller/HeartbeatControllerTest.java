5bf8196efd0606af6229d9de85e2834795c61e4f
XiaoYou201 <xiaoyou201@foxmail.com>
2024-04-19 20:39:43 +0800

    [INLONG-9881][Manager] Change test function

--- inlong-manager/manager-web/src/test/java/org/apache/inlong/manager/web/controller/HeartbeatControllerTest.java
+++ inlong-manager/manager-web/src/test/java/org/apache/inlong/manager/web/controller/HeartbeatControllerTest.java
@@ -17,190 +17,285 @@
 
 package org.apache.inlong.manager.web.controller;
 
+import org.apache.inlong.common.heartbeat.GroupHeartbeat;
+import org.apache.inlong.common.heartbeat.StreamHeartbeat;
+import org.apache.inlong.manager.common.util.JsonUtils;
 import org.apache.inlong.manager.pojo.common.PageResult;
 import org.apache.inlong.manager.pojo.common.Response;
 import org.apache.inlong.manager.pojo.heartbeat.ComponentHeartbeatResponse;
 import org.apache.inlong.manager.pojo.heartbeat.GroupHeartbeatResponse;
-import org.apache.inlong.manager.pojo.heartbeat.HeartbeatPageRequest;
 import org.apache.inlong.manager.pojo.heartbeat.HeartbeatQueryRequest;
+import org.apache.inlong.manager.pojo.heartbeat.HeartbeatReportRequest;
 import org.apache.inlong.manager.pojo.heartbeat.StreamHeartbeatResponse;
 import org.apache.inlong.manager.service.core.HeartbeatService;
+import org.apache.inlong.manager.web.WebBaseTest;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class HeartbeatControllerTest {
+public class HeartbeatControllerTest extends WebBaseTest {
 
-    @Mock
+    @Autowired
     private HeartbeatService heartbeatService;
 
     @InjectMocks
     private HeartbeatController heartbeatController;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    private static final String TEST_PWD = "test_#$%%Y@UI$123";
+    private static final String INSTANCE_IP = "192.168.1.3";
+    private static final String COMPONENT_TYPE = "DATAPROXY";
+    private static final String INLONG_GROUP_ID_1 = "test_group_heartbeat_1";
+    private static final String INLONG_GROUP_ID_2 = "test_group_heartbeat_2";
+    private static final String INLONG_STREAM_ID_1 = "test_stream_heartbeat_1";
+    private static final String INLONG_STREAM_ID_2 = "test_stream_heartbeat_2";
+    private static final String STATUS = "enabled";
+    private static final String CLUSTER_NAME = "test_cluster";
+    private static final String CLUSTER_TAG = "test_cluster";
+    private static final String EXT_TAG = "test_ext_tag";
+    private static final String PORT = "12345";
+    private static final Long REPORT_TIME =
+            LocalDate.now().atStartOfDay().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+    @BeforeAll
+    void setup() {
+        addHeartbeat();
+    }
+    private void addHeartbeat() {
+        HeartbeatReportRequest request = new HeartbeatReportRequest();
+        request.setIp(INSTANCE_IP);
+        request.setComponentType(COMPONENT_TYPE);
+        request.setReportTime(REPORT_TIME);
+        List<GroupHeartbeat> groupList = new ArrayList<GroupHeartbeat>() {
+
+            {
+                add(new GroupHeartbeat(INLONG_GROUP_ID_1, STATUS, null));
+                add(new GroupHeartbeat(INLONG_GROUP_ID_2, STATUS, null));
+            }
+        };
+        request.setGroupHeartbeats(groupList);
+        List<StreamHeartbeat> steamList = new ArrayList<StreamHeartbeat>() {
+
+            {
+                add(new StreamHeartbeat(INLONG_GROUP_ID_1, INLONG_STREAM_ID_1, STATUS, null));
+                add(new StreamHeartbeat(INLONG_GROUP_ID_2, INLONG_STREAM_ID_2, STATUS, null));
+            }
+        };
+        request.setStreamHeartbeats(steamList);
+        request.setClusterName(CLUSTER_NAME);
+        request.setClusterTag(CLUSTER_TAG);
+        request.setExtTag(EXT_TAG);
+        request.setPort(PORT);
+        heartbeatService.reportHeartbeat(request);
     }
 
     @Test
-    public void testGetComponentHeartbeat() {
+    public void testGetComponentHeartbeat() throws Exception {
+
         // Mock the request and response
         HeartbeatQueryRequest request = new HeartbeatQueryRequest();
-        ComponentHeartbeatResponse data = new ComponentHeartbeatResponse();
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.getComponentHeartbeat(request)).thenReturn(data);
-
+        ComponentHeartbeatResponse expect = new ComponentHeartbeatResponse();
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
         // Call the controller method
-        Response<ComponentHeartbeatResponse> response = heartbeatController.getComponentHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/component/get")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        Response<ComponentHeartbeatResponse> response = getResBody(mvcResult, ComponentHeartbeatResponse.class);
         // Verify the result
         Assertions.assertNotNull(response);
         Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(), response.getData());
+        ComponentHeartbeatResponse result = response.getData();
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        result.setModifyTime(null);
+        Assertions.assertEquals(expect, result);
     }
 
     @Test
-    public void testGetGroupHeartbeat() {
+    public void testGetGroupHeartbeat() throws Exception {
         // Mock the request and response
         HeartbeatQueryRequest request = new HeartbeatQueryRequest();
-        GroupHeartbeatResponse data = new GroupHeartbeatResponse();
-        data.setId(1);
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
+        request.setInlongGroupId(INLONG_GROUP_ID_1);
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.getGroupHeartbeat(request)).thenReturn(data);
-
+        GroupHeartbeatResponse expect = new GroupHeartbeatResponse();
+        expect.setInlongGroupId(INLONG_GROUP_ID_1);
+        expect.setStatusHeartbeat(STATUS);
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
         // Call the controller method
-        Response<GroupHeartbeatResponse> response = heartbeatController.getGroupHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/group/get")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        Response<GroupHeartbeatResponse> response = getResBody(mvcResult, GroupHeartbeatResponse.class);
         // Verify the result
         Assertions.assertNotNull(response);
         Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(), response.getData());
+        GroupHeartbeatResponse result = response.getData();
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        result.setModifyTime(null);
+        Assertions.assertEquals(expect, result);
     }
 
     @Test
-    public void testGetStreamHeartbeat() {
+    public void testGetStreamHeartbeat() throws Exception {
         // Mock the request and response
         HeartbeatQueryRequest request = new HeartbeatQueryRequest();
-        StreamHeartbeatResponse data = new StreamHeartbeatResponse();
-        data.setId(1);
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
+        request.setInlongGroupId(INLONG_GROUP_ID_1);
+        request.setInlongStreamId(INLONG_STREAM_ID_1);
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.getStreamHeartbeat(request)).thenReturn(data);
-
+        StreamHeartbeatResponse expect = new StreamHeartbeatResponse();
+        expect.setInlongGroupId(INLONG_GROUP_ID_1);
+        expect.setInlongStreamId(INLONG_STREAM_ID_1);
+        expect.setStatusHeartbeat(STATUS);
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
         // Call the controller method
-        Response<StreamHeartbeatResponse> response = heartbeatController.getStreamHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/stream/get")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        Response<StreamHeartbeatResponse> response = getResBody(mvcResult, StreamHeartbeatResponse.class);
         // Verify the result
         Assertions.assertNotNull(response);
         Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(), response.getData());
+        StreamHeartbeatResponse result = response.getData();
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        result.setModifyTime(null);
+        Assertions.assertEquals(expect, result);
 
     }
 
     @Test
-    public void testListComponentHeartbeat() {
+    public void testListComponentHeartbeat() throws Exception {
         // Mock the request and response
-        HeartbeatPageRequest request = new HeartbeatPageRequest();
-        PageResult<ComponentHeartbeatResponse> dataPage = new PageResult<>();
-        ComponentHeartbeatResponse data = new ComponentHeartbeatResponse();
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
-        List<ComponentHeartbeatResponse> dataList = Collections.singletonList(data);
-        dataPage.setList(dataList);
-        dataPage.setPageSize(20);
-        dataPage.setPageNum(1);
+        HeartbeatQueryRequest request = new HeartbeatQueryRequest();
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.listComponentHeartbeat(request)).thenReturn(dataPage);
+        ComponentHeartbeatResponse expect = new ComponentHeartbeatResponse();
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
 
+        PageResult<ComponentHeartbeatResponse> expectPage = new PageResult<>();
+        List<ComponentHeartbeatResponse> dataList = Collections.singletonList(expect);
+        expectPage.setList(dataList);
         // Call the controller method
-        Response<PageResult<ComponentHeartbeatResponse>> response = heartbeatController.listComponentHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/component/list")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<ComponentHeartbeatResponse> response = getResBodyPageList(mvcResult, ComponentHeartbeatResponse.class);
         // Verify the result
         Assertions.assertNotNull(response);
-        Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(),
-                response.getData().getList().get(0));
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        response.forEach(c -> c.setModifyTime(null));
+        Assertions.assertEquals(expectPage.getList(), response);
     }
 
     @Test
-    public void testListGroupHeartbeat() {
+    public void testListGroupHeartbeat() throws Exception {
         // Mock the request and response
-        HeartbeatPageRequest request = new HeartbeatPageRequest();
-        PageResult<GroupHeartbeatResponse> dataPage = new PageResult<>();
-        GroupHeartbeatResponse data = new GroupHeartbeatResponse();
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
-        List<GroupHeartbeatResponse> dataList = Collections.singletonList(data);
-        dataPage.setList(dataList);
-        dataPage.setPageSize(20);
-        dataPage.setPageNum(1);
+        HeartbeatQueryRequest request = new HeartbeatQueryRequest();
+        request.setInlongGroupId(INLONG_GROUP_ID_1);
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.listGroupHeartbeat(request)).thenReturn(dataPage);
+        GroupHeartbeatResponse expect = new GroupHeartbeatResponse();
+        expect.setInlongGroupId(INLONG_GROUP_ID_1);
+        expect.setStatusHeartbeat(STATUS);
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
 
+        PageResult<GroupHeartbeatResponse> expectPage = new PageResult<>();
+        List<GroupHeartbeatResponse> dataList = Collections.singletonList(expect);
+        expectPage.setList(dataList);
         // Call the controller method
-        Response<PageResult<GroupHeartbeatResponse>> response = heartbeatController.listGroupHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/group/list")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<GroupHeartbeatResponse> result = getResBodyPageList(mvcResult, GroupHeartbeatResponse.class);
         // Verify the result
-        Assertions.assertNotNull(response);
-        Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(),
-                response.getData().getList().get(0));
+        Assertions.assertNotNull(result);
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        result.forEach(g -> g.setModifyTime(null));
+        Assertions.assertEquals(expectPage.getList(), result);
     }
 
     @Test
-    public void testListStreamHeartbeat() {
+    public void testListStreamHeartbeat() throws Exception {
         // Mock the request and response
-        HeartbeatPageRequest request = new HeartbeatPageRequest();
-        PageResult<StreamHeartbeatResponse> dataPage = new PageResult<>();
-        StreamHeartbeatResponse data = new StreamHeartbeatResponse();
-        data.setComponent("Test component");
-        data.setInstance("Test instance");
-        data.setStatusHeartbeat("Test statusHeartbeat");
-        data.setMetricHeartbeat("Test metricHeartbeat");
-        List<StreamHeartbeatResponse> dataList = Collections.singletonList(data);
-        dataPage.setList(dataList);
-        dataPage.setPageSize(20);
-        dataPage.setPageNum(1);
+        HeartbeatQueryRequest request = new HeartbeatQueryRequest();
+        request.setInlongGroupId(INLONG_GROUP_ID_1);
+        request.setInlongStreamId(INLONG_STREAM_ID_1);
+        request.setComponent(COMPONENT_TYPE);
+        request.setInstance(INSTANCE_IP);
 
-        // Mock the service method
-        when(heartbeatService.listStreamHeartbeat(request)).thenReturn(dataPage);
+        StreamHeartbeatResponse expect = new StreamHeartbeatResponse();
+        expect.setInlongGroupId(INLONG_GROUP_ID_1);
+        expect.setInlongStreamId(INLONG_STREAM_ID_1);
+        expect.setStatusHeartbeat(STATUS);
+        expect.setComponent(COMPONENT_TYPE);
+        expect.setInstance(INSTANCE_IP);
+        expect.setReportTime(REPORT_TIME);
 
+        PageResult<StreamHeartbeatResponse> expectPage = new PageResult<>();
+        List<StreamHeartbeatResponse> dataList = Collections.singletonList(expect);
+        expectPage.setList(dataList);
         // Call the controller method
-        Response<PageResult<StreamHeartbeatResponse>> response = heartbeatController.listStreamHeartbeat(request);
-
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/heartbeat/stream/list")
+                        .content(JsonUtils.toJsonString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<StreamHeartbeatResponse> result = getResBodyPageList(mvcResult, StreamHeartbeatResponse.class);
         // Verify the result
-        Assertions.assertNotNull(response);
-        Assertions.assertTrue(response.isSuccess());
-        Assertions.assertEquals(Response.success(data).getData(),
-                response.getData().getList().get(0));
+        Assertions.assertNotNull(result);
+        // The program will modify the field modifyTime according to the process time, So this field can't assert
+        result.forEach(s -> s.setModifyTime(null));
+        Assertions.assertEquals(expectPage.getList(), result);
     }
 }
