c4cac2e981bad34d2a7490300a6992530a2bc4ce
卢春亮 <luchunliang@apache.org>
2024-05-07 10:38:01 +0800

    [INLONG-10117][SDK] Support to transform from PB protocol to CSV/KV protocol by single SQL (#10127)

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformProcessor.java
@@ -23,6 +23,7 @@ import org.apache.inlong.sdk.transform.pojo.FieldInfo;
 import org.apache.inlong.sdk.transform.pojo.JsonSourceInfo;
 import org.apache.inlong.sdk.transform.pojo.KvSinkInfo;
 import org.apache.inlong.sdk.transform.pojo.KvSourceInfo;
+import org.apache.inlong.sdk.transform.pojo.PbSourceInfo;
 import org.apache.inlong.sdk.transform.pojo.SinkInfo;
 import org.apache.inlong.sdk.transform.pojo.SourceInfo;
 import org.apache.inlong.sdk.transform.pojo.TransformConfig;
@@ -31,6 +32,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 
@@ -101,19 +103,7 @@ public class TestTransformProcessor {
     @Test
     public void testJson2Csv() {
         try {
-            List<FieldInfo> fields = new ArrayList<>();
-            FieldInfo sid = new FieldInfo();
-            sid.setName("sid");
-            fields.add(sid);
-            FieldInfo packageID = new FieldInfo();
-            packageID.setName("packageID");
-            fields.add(packageID);
-            FieldInfo msgTime = new FieldInfo();
-            msgTime.setName("msgTime");
-            fields.add(msgTime);
-            FieldInfo msg = new FieldInfo();
-            msg.setName("msg");
-            fields.add(msg);
+            List<FieldInfo> fields = this.getTestFieldList();
             SourceInfo jsonSource = new JsonSourceInfo("UTF-8", "msgs");
             SinkInfo csvSink = new CsvSinkInfo("UTF-8", "|", "\\", fields);
             String transformSql = "select $root.sid,$root.packageID,$child.msgTime,$child.msg from source";
@@ -140,19 +130,7 @@ public class TestTransformProcessor {
     @Test
     public void testJson2CsvForOne() {
         try {
-            List<FieldInfo> fields = new ArrayList<>();
-            FieldInfo sid = new FieldInfo();
-            sid.setName("sid");
-            fields.add(sid);
-            FieldInfo packageID = new FieldInfo();
-            packageID.setName("packageID");
-            fields.add(packageID);
-            FieldInfo msgTime = new FieldInfo();
-            msgTime.setName("msgTime");
-            fields.add(msgTime);
-            FieldInfo msg = new FieldInfo();
-            msg.setName("msg");
-            fields.add(msg);
+            List<FieldInfo> fields = this.getTestFieldList();
             SourceInfo jsonSource = new JsonSourceInfo("UTF-8", "");
             SinkInfo csvSink = new CsvSinkInfo("UTF-8", "|", "\\", fields);
             String transformSql =
@@ -200,6 +178,93 @@ public class TestTransformProcessor {
             TransformProcessor processor2 = new TransformProcessor(configString2);
             List<String> output2 = processor2.transform("ftime=2024-04-28 00:00:00&extinfo=ok", new HashMap<>());
             Assert.assertTrue(output2.size() == 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testPb2Csv() {
+        try {
+            List<FieldInfo> fields = this.getTestFieldList();
+            String transformBase64 = this.getPbTestDescription();
+            SourceInfo pbSource = new PbSourceInfo("UTF-8", transformBase64, "SdkDataRequest", "msgs");
+            SinkInfo csvSink = new CsvSinkInfo("UTF-8", "|", "\\", fields);
+            String transformSql = "select $root.sid,$root.packageID,$child.msgTime,$child.msg from source";
+            TransformConfig config = new TransformConfig(pbSource, csvSink, transformSql);
+            // case1
+            TransformProcessor processor = new TransformProcessor(config);
+            byte[] srcBytes = this.getPbTestData();
+            List<String> output = processor.transform(srcBytes, new HashMap<>());
+            Assert.assertTrue(output.size() == 2);
+            Assert.assertEquals(output.get(0), "sid|1|1713243918000|msgValue4");
+            Assert.assertEquals(output.get(1), "sid|1|1713243918002|msgValue42");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<FieldInfo> getTestFieldList() {
+        List<FieldInfo> fields = new ArrayList<>();
+        FieldInfo sid = new FieldInfo();
+        sid.setName("sid");
+        fields.add(sid);
+        FieldInfo packageID = new FieldInfo();
+        packageID.setName("packageID");
+        fields.add(packageID);
+        FieldInfo msgTime = new FieldInfo();
+        msgTime.setName("msgTime");
+        fields.add(msgTime);
+        FieldInfo msg = new FieldInfo();
+        msg.setName("msg");
+        fields.add(msg);
+        return fields;
+    }
+
+    private byte[] getPbTestData() {
+        String srcString =
+                "CgNzaWQSIAoJbXNnVmFsdWU0ELCdrqruMRoMCgNrZXkSBXZhbHVlEiMKCm1zZ1ZhbHVlNDIQsp2uqu4xGg4KBGtleTISBnZhbHVlMhgB";
+        byte[] srcBytes = Base64.getDecoder().decode(srcString);
+        return srcBytes;
+    }
+
+    private String getPbTestDescription() {
+        final String transformProto = "syntax = \"proto3\";\n"
+                + "package test;\n"
+                + "message SdkMessage {\n"
+                + "  bytes msg = 1;\n"
+                + "  int64 msgTime = 2;\n"
+                + "  map<string, string> extinfo = 3;\n"
+                + "}\n"
+                + "message SdkDataRequest {\n"
+                + "  string sid = 1;\n"
+                + "  repeated SdkMessage msgs = 2;\n"
+                + "  uint64 packageID = 3;\n"
+                + "}";
+        String transformBase64 = "CrcCCg90cmFuc2Zvcm0ucHJvdG8SBHRlc3QirQEKClNka01lc3NhZ2USEAoDbXNnGAEgASgMUg"
+                + "Ntc2cSGAoHbXNnVGltZRgCIAEoA1IHbXNnVGltZRI3CgdleHRpbmZvGAMgAygLMh0udGVzdC5TZGtNZXNzYWdlLk"
+                + "V4dGluZm9FbnRyeVIHZXh0aW5mbxo6CgxFeHRpbmZvRW50cnkSEAoDa2V5GAEgASgJUgNrZXkSFAoFdmFsdWUY"
+                + "AiABKAlSBXZhbHVlOgI4ASJmCg5TZGtEYXRhUmVxdWVzdBIQCgNzaWQYASABKAlSA3NpZBIkCgRtc2dzGAIgAygLMh"
+                + "AudGVzdC5TZGtNZXNzYWdlUgRtc2dzEhwKCXBhY2thZ2VJRBgDIAEoBFIJcGFja2FnZUlEYgZwcm90bzM=";
+        return transformBase64;
+    }
+
+    @Test
+    public void testPb2CsvForOne() {
+        try {
+            List<FieldInfo> fields = this.getTestFieldList();
+            String transformBase64 = this.getPbTestDescription();
+            SourceInfo pbSource = new PbSourceInfo("UTF-8", transformBase64, "SdkDataRequest", null);
+            SinkInfo csvSink = new CsvSinkInfo("UTF-8", "|", "\\", fields);
+            String transformSql =
+                    "select $root.sid,$root.packageID,$root.msgs(1).msgTime,$root.msgs(0).msg from source";
+            TransformConfig config = new TransformConfig(pbSource, csvSink, transformSql);
+            // case1
+            TransformProcessor processor = new TransformProcessor(config);
+            byte[] srcBytes = this.getPbTestData();
+            List<String> output = processor.transform(srcBytes, new HashMap<>());
+            Assert.assertTrue(output.size() == 1);
+            Assert.assertEquals(output.get(0), "sid|1|1713243918002|msgValue4");
         } catch (Exception e) {
             e.printStackTrace();
         }
