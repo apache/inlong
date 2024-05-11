302df4b5f6047e3a7c464440bf65ceb2bcc1c0b3
dockerzhang <dockerzhang@apache.org>
2024-05-11 12:49:43 +0800

    [INLONG-10124][Script] Update the scripts for standalone deployment

--- bin/init-config.sh
+++ bin/init-config.sh
@@ -51,8 +51,8 @@ detect_sed_command
 init_inlong_agent() {
   echo "Init agent configuration parameters"
   cd $INLONG_HOME/inlong-agent/conf
-  $SED_COMMAND 's/agent.local.ip=.*/'''agent.local.ip=${local_ip}'''/g' agent.properties
-  $SED_COMMAND 's/agent.manager.addr=.*/'''agent.manager.addr=${manager_server_hostname}:${manager_server_port}'''/g' agent.properties
+  $SED_COMMAND "s|agent.local.ip=.*|agent.local.ip=${local_ip}|g" agent.properties
+  $SED_COMMAND "s|agent.manager.addr=.*|agent.manager.addr=http://${manager_server_hostname}:${manager_server_port}|g" agent.properties
   $SED_COMMAND "s/audit.enable=.*$/audit.enable=true/g" agent.properties
   $SED_COMMAND 's/audit.proxys=.*/'''audit.proxys=${audit_proxy_ip}:${audit_proxy_port}'''/g' agent.properties
 }
