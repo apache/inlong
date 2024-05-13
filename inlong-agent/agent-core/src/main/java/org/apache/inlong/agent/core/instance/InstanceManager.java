093b3e807ec3ab636cc5d5a1d18909cb8bdd6438
justinwwhuang <hww_justin@163.com>
2024-05-13 10:24:12 +0800

    Update inlong-agent/agent-core/src/main/java/org/apache/inlong/agent/core/instance/InstanceManager.java
    Co-authored-by: AloysZhang <lofterzhang@gmail.com>

--- inlong-agent/agent-core/src/main/java/org/apache/inlong/agent/core/instance/InstanceManager.java
+++ inlong-agent/agent-core/src/main/java/org/apache/inlong/agent/core/instance/InstanceManager.java
@@ -242,7 +242,7 @@ public class InstanceManager extends AbstractDaemon {
                 deleteFromMemory(instance.getInstanceId());
             }
             if (AgentUtils.getCurrentTime() - instance.getLastHeartbeatTime() > INSTANCE_KEEP_ALIVE_MS) {
-                LOGGER.error("instance heartbeat timeout {} will delete from memory", instance.getInstanceId());
+                LOGGER.error("instance heartbeat timeout, id: {}, will be deleted from memory", instance.getInstanceId());
                 deleteFromMemory(instance.getInstanceId());
             }
         });
