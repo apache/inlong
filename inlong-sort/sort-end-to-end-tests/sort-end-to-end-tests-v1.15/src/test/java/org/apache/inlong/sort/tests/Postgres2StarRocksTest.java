6dd204529dd2fca52a09791067a8212ea0cde165
vinnerzhang <vinnerzhang@tencent.com>
2024-05-29 18:31:15 +0800

    [INLONG-10296] Fix Postgres AuditOperator was not serialized bug

--- inlong-sort/sort-end-to-end-tests/sort-end-to-end-tests-v1.15/src/test/java/org/apache/inlong/sort/tests/Postgres2StarRocksTest.java
+++ inlong-sort/sort-end-to-end-tests/sort-end-to-end-tests-v1.15/src/test/java/org/apache/inlong/sort/tests/Postgres2StarRocksTest.java
@@ -158,11 +158,10 @@ public class Postgres2StarRocksTest extends FlinkContainerTestEnvJRE11 {
                         STAR_ROCKS.getDriverClassName());
         List<String> expectResult =
                 Arrays.asList("2,tom,Big 2-wheel scooter ");
-//        proxy.checkResultWithTimeout(
-//                expectResult,
-//                "test_output1",
-//                3,
-//                60000L);
-        while(true){}
+        proxy.checkResultWithTimeout(
+                expectResult,
+                "test_output1",
+                3,
+                60000L);
     }
 }
