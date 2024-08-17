5d3eb3925f063cf1364d29202a21fa39aa5f045a
yfsn666 <lwy2306@163.com>
2024-08-17 17:09:58 +0800

    [INLONG-10781][SDK] Transform SQL support temporal functions(Including form_unixtime, unix_timestamp and to_timestamp)

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformTemporalFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformTemporalFunctionsProcessor.java
@@ -234,4 +234,132 @@ public class TestTransformTemporalFunctionsProcessor {
         Assert.assertEquals(1, output3.size());
         Assert.assertEquals(output3.get(0), "result=34");
     }
+
+    @Test
+    public void testFromUnixTimeFunction() throws Exception {
+        String transformSql1 = "select from_unixtime(numeric1) from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case1: from_unixtime(44)
+        List<String> output1 = processor1.transform("can|apple|cloud|44|1|3", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=1970-01-01 08:00:44");
+
+        String transformSql2 = "select from_unixtime(numeric1, 'yyyy/MM/dd HH:mm:ss') from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case2: from_unixtime(44, 'yyyy/MM/dd HH:mm:ss')
+        List<String> output2 = processor2.transform("can|apple|cloud|44|1|3", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=1970/01/01 08:00:44");
+
+        String transformSql3 = "select from_unixtime(numeric1, 'MMdd-yyyy') from source";
+        TransformConfig config3 = new TransformConfig(transformSql3);
+        TransformProcessor<String, String> processor3 = TransformProcessor
+                .create(config3, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case3: from_unixtime(44, 'MMdd-yyyy')
+        List<String> output3 = processor3.transform("can|apple|cloud|44|1|3", new HashMap<>());
+        Assert.assertEquals(1, output3.size());
+        Assert.assertEquals(output3.get(0), "result=0101-1970");
+
+        String transformSql4 = "select from_unixtime(numeric1, 'yyyyMMddHHss') from source";
+        TransformConfig config4 = new TransformConfig(transformSql4);
+        TransformProcessor<String, String> processor4 = TransformProcessor
+                .create(config4, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case4: from_unixtime(44, 'yyyyMMddHHss')
+        List<String> output4 = processor4.transform("can|apple|cloud|44|1|3", new HashMap<>());
+        Assert.assertEquals(1, output4.size());
+        Assert.assertEquals(output4.get(0), "result=197001010844");
+    }
+
+    @Test
+    public void testUnixTimestampFunction() throws Exception {
+        String transformSql1 = "select unix_timestamp() from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case1: unix_timestamp()
+        List<String> output1 = processor1.transform("", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+
+        String transformSql2 = "select unix_timestamp(string1, 'yyyy/MM/dd HH:mm:ss') from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case2: unix_timestamp('1970/01/01 08:00:44', 'yyyy/MM/dd HH:mm:ss')
+        List<String> output2 = processor2.transform("1970/01/01 08:00:44", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=44");
+
+        String transformSql3 = "select unix_timestamp(string1) from source";
+        TransformConfig config3 = new TransformConfig(transformSql3);
+        TransformProcessor<String, String> processor3 = TransformProcessor
+                .create(config3, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case3: unix_timestamp('1970/01/01 08:00:44')
+        List<String> output3 = processor3.transform("1970-01-01 08:00:44", new HashMap<>());
+        Assert.assertEquals(1, output3.size());
+        Assert.assertEquals(output3.get(0), "result=44");
+
+        String transformSql4 = "select unix_timestamp(string1, 'yyyyMMddHHmmss') from source";
+        TransformConfig config4 = new TransformConfig(transformSql4);
+        TransformProcessor<String, String> processor4 = TransformProcessor
+                .create(config4, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case4: unix_timestamp('19700101080044', 'yyyyMMddHHss')
+        List<String> output4 = processor4.transform("19700101080044", new HashMap<>());
+        Assert.assertEquals(1, output4.size());
+        Assert.assertEquals(output4.get(0), "result=44");
+    }
+
+    @Test
+    public void testToTimestampFunction() throws Exception {
+        String transformSql1 = "select to_timestamp(string1) from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case1: to_timestamp('1970-01-01 00:00:44')
+        List<String> output1 = processor1.transform("1970-01-01 00:00:44", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=1970-01-01 00:00:44.0");
+
+        String transformSql2 = "select to_timestamp(string1, 'yyyy/MM/dd HH:mm:ss') from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case2: to_timestamp('1970/01/01 00:00:44', 'yyyy/MM/dd HH:mm:ss')
+        List<String> output2 = processor2.transform("1970/01/01 00:00:44", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=1970-01-01 00:00:44.0");
+
+        String transformSql3 = "select to_timestamp(string1, 'yyyyMMddHHmmss') from source";
+        TransformConfig config3 = new TransformConfig(transformSql3);
+        TransformProcessor<String, String> processor3 = TransformProcessor
+                .create(config3, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case3: to_timestamp('19700101000044', 'yyyyMMddHHmmss')
+        List<String> output3 = processor3.transform("19700101000044", new HashMap<>());
+        Assert.assertEquals(1, output3.size());
+        Assert.assertEquals(output3.get(0), "result=1970-01-01 00:00:44.0");
+
+        String transformSql4 = "select to_timestamp(string1, 'yyyy-MM-dd') from source";
+        TransformConfig config4 = new TransformConfig(transformSql4);
+        TransformProcessor<String, String> processor4 = TransformProcessor
+                .create(config4, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case4: to_timestamp('1970-01-01', 'yyyy-MM-dd')
+        List<String> output4 = processor4.transform("1970-01-01", new HashMap<>());
+        Assert.assertEquals(1, output4.size());
+        Assert.assertEquals(output4.get(0), "result=1970-01-01 00:00:00.0");
+    }
 }
