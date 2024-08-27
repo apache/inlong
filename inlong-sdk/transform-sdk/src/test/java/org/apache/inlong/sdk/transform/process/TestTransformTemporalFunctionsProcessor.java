916e6c1532ac4f740abb7b4c0babf76ef855baee
Xincheng Huang <60057611+ying-hua@users.noreply.github.com>
2024-08-27 19:35:32 +0800

    [INLONG-10920][SDK] Transform support LocalTime function (#10922)

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformTemporalFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformTemporalFunctionsProcessor.java
@@ -28,6 +28,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -361,5 +365,48 @@ public class TestTransformTemporalFunctionsProcessor {
         List<String> output4 = processor4.transform("1970-01-01", new HashMap<>());
         Assert.assertEquals(1, output4.size());
         Assert.assertEquals(output4.get(0), "result=1970-01-01 00:00:00.0");
+    }
+
+    @Test
+    public void testLocalTimeFunction() throws Exception {
+        String transformSql1 = "select localTime() from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        DateTimeFormatter fomatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        // case1: localTime()
+        List<String> output1 = processor1.transform("", new HashMap<>());
+        LocalTime expectedTime1 = LocalTime.now().withNano(0);
+        LocalTime actualTime1 = LocalTime.parse(output1.get(0).split("=")[1], fomatter);
+        Duration duration1 = Duration.between(expectedTime1, actualTime1);
+        Assert.assertEquals(1, output1.size());
+        Assert.assertTrue(duration1.getSeconds() < 1);
+
+        // case2: currentTime("UTC")
+        String transformSql2 = "select currentTime('UTC') from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        List<String> output2 = processor2.transform("", new HashMap<>());
+        LocalTime expectedTime2 = LocalTime.now(ZoneId.of("UTC")).withNano(0);
+        LocalTime actualTime2 = LocalTime.parse(output2.get(0).split("=")[1], fomatter);
+        Duration duration2 = Duration.between(expectedTime2, actualTime2);
+        Assert.assertEquals(1, output2.size());
+        Assert.assertTrue(duration2.getSeconds() < 1);
+
+        // case 3: localTime("America/New_York")
+        String transformSql3 = "select localTime('America/New_York') from source";
+        TransformConfig config3 = new TransformConfig(transformSql3);
+        TransformProcessor<String, String> processor3 = TransformProcessor
+                .create(config3, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        List<String> output3 = processor3.transform("", new HashMap<>());
+        LocalTime expectedTime3 = LocalTime.now(ZoneId.of("America/New_York")).withNano(0);
+        LocalTime actualTime3 = LocalTime.parse(output3.get(0).split("=")[1], fomatter);
+        Duration duration3 = Duration.between(expectedTime3, actualTime3);
+        Assert.assertEquals(1, output3.size());
+        Assert.assertTrue(duration3.getSeconds() < 1);
     }
 }
