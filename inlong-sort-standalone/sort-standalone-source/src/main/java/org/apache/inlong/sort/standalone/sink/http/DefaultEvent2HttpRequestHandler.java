8be8834a788d0a4794ac845e3df744f8fbe1762d
yfsn666 <61183968+yfsn666@users.noreply.github.com>
2024-08-27 13:00:28 +0800

    [INLONG-10831][Sort] SortStandalone support HTTP sink (#10837)

---
+++ inlong-sort-standalone/sort-standalone-source/src/main/java/org/apache/inlong/sort/standalone/sink/http/DefaultEvent2HttpRequestHandler.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.standalone.sink.http;
+
+import org.apache.inlong.sort.standalone.channel.ProfileEvent;
+import org.apache.inlong.sort.standalone.utils.InlongLoggerFactory;
+import org.apache.inlong.sort.standalone.utils.UnescapeHelper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.slf4j.Logger;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DefaultEvent2HttpRequestHandler implements IEvent2HttpRequestHandler {
+
+    public static final Logger LOG = InlongLoggerFactory.getLogger(DefaultEvent2HttpRequestHandler.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    public static final String HTTP_PREFIX = "http://";
+    public static final String HTTPS_PREFIX = "https://";
+    public static final String INLONG_GROUP_ID_HEADER = "inlongGroupId";
+    public static final String INLONG_STREAM_ID_HEADER = "inlongStreamId";
+
+    @Override
+    public HttpRequest parse(HttpSinkContext context, ProfileEvent event)
+            throws URISyntaxException, JsonProcessingException {
+        String uid = event.getUid();
+        HttpIdConfig idConfig = context.getIdConfig(uid);
+        if (idConfig == null) {
+            context.addSendResultMetric(event, context.getTaskName(), false, System.currentTimeMillis());
+            return null;
+        }
+        // get the delimiter
+        String delimiter = idConfig.getSeparator();
+        char cDelimiter = delimiter.charAt(0);
+        // for tab separator
+        byte[] bodyBytes = event.getBody();
+        String strContext = new String(bodyBytes, idConfig.getSourceCharset());
+        // unescape
+        List<String> columnValues = UnescapeHelper.toFiledList(strContext, cDelimiter);
+        int valueLength = columnValues.size();
+        List<String> fieldList = idConfig.getFieldList();
+        int columnLength = fieldList.size();
+        // get field value
+        Map<String, String> fieldMap = new HashMap<>();
+        for (int i = 0; i < columnLength; ++i) {
+            String fieldName = fieldList.get(i);
+            String fieldValue = i < valueLength ? columnValues.get(i) : "";
+            fieldMap.put(fieldName, fieldValue);
+        }
+
+        // build
+        String uriString = context.getBaseUrl() + idConfig.getPath();
+        if (!uriString.startsWith(HTTP_PREFIX) && !uriString.startsWith(HTTPS_PREFIX)) {
+            uriString = HTTP_PREFIX + uriString;
+        }
+        URI uri = new URI(uriString);
+        String jsonData;
+        HttpUriRequest request;
+        String requestMethod = idConfig.getMethod().toUpperCase();
+        switch (requestMethod) {
+            case "GET":
+                String params = fieldMap.entrySet().stream()
+                        .map(entry -> {
+                            try {
+                                return entry.getKey() + "="
+                                        + URLEncoder.encode(entry.getValue(), idConfig.getSinkCharset().name());
+                            } catch (UnsupportedEncodingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .collect(Collectors.joining("&"));
+                request = new HttpGet(uri + "?" + params);
+                for (Map.Entry<String, String> entry : idConfig.getHeaders().entrySet()) {
+                    request.setHeader(entry.getKey(), entry.getValue());
+                }
+                request.setHeader(INLONG_GROUP_ID_HEADER, idConfig.getInlongGroupId());
+                request.setHeader(INLONG_STREAM_ID_HEADER, idConfig.getInlongStreamId());
+                break;
+            case "POST":
+                request = new HttpPost(uri);
+                for (Map.Entry<String, String> entry : idConfig.getHeaders().entrySet()) {
+                    request.setHeader(entry.getKey(), entry.getValue());
+                }
+                request.setHeader(INLONG_GROUP_ID_HEADER, idConfig.getInlongGroupId());
+                request.setHeader(INLONG_STREAM_ID_HEADER, idConfig.getInlongStreamId());
+                jsonData = objectMapper.writeValueAsString(fieldMap);
+                setEntity((HttpEntityEnclosingRequestBase) request, jsonData);
+                break;
+            case "PUT":
+                request = new HttpPut(uri);
+                for (Map.Entry<String, String> entry : idConfig.getHeaders().entrySet()) {
+                    request.setHeader(entry.getKey(), entry.getValue());
+                }
+                request.setHeader(INLONG_GROUP_ID_HEADER, idConfig.getInlongGroupId());
+                request.setHeader(INLONG_STREAM_ID_HEADER, idConfig.getInlongStreamId());
+                jsonData = objectMapper.writeValueAsString(fieldMap);
+                setEntity((HttpEntityEnclosingRequestBase) request, jsonData);
+                break;
+            default:
+                LOG.error("Unsupported request method: {}", requestMethod);
+                return null;
+        }
+        return new HttpRequest(request, event, idConfig.getMaxRetryTimes());
+    }
+
+    private static void setEntity(HttpEntityEnclosingRequestBase request, String jsonData) {
+        StringEntity requestEntity = new StringEntity(jsonData, ContentType.APPLICATION_JSON);
+        request.setEntity(requestEntity);
+    }
+}
---
+++ inlong-sort-standalone/sort-standalone-source/src/main/java/org/apache/inlong/sort/standalone/sink/http/HttpIdConfig.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sort.standalone.sink.http;
+
+import org.apache.inlong.common.pojo.sort.dataflow.DataFlowConfig;
+import org.apache.inlong.common.pojo.sort.dataflow.field.FieldConfig;
+import org.apache.inlong.common.pojo.sort.dataflow.sink.HttpSinkConfig;
+import org.apache.inlong.sort.standalone.config.pojo.IdConfig;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Slf4j
+public class HttpIdConfig extends IdConfig {
+
+    private String path;
+    private String method;
+    private Map<String, String> headers;
+    private Integer maxRetryTimes;
+    private String separator = "|";
+    private List<String> fieldList;
+    private Charset sourceCharset;
+    private Charset sinkCharset;
+
+    public static HttpIdConfig create(DataFlowConfig dataFlowConfig) {
+        HttpSinkConfig sinkConfig = (HttpSinkConfig) dataFlowConfig.getSinkConfig();
+        List<String> fields = sinkConfig.getFieldConfigs()
+                .stream()
+                .map(FieldConfig::getName)
+                .collect(Collectors.toList());
+        Charset sourceCharset, sinkCharset;
+        try {
+            sinkCharset = Charset.forName(sinkConfig.getEncodingType());
+        } catch (Throwable t) {
+            log.warn("do not support field encoding type={}, dataflow id={}",
+                    sinkConfig.getEncodingType(), dataFlowConfig.getDataflowId());
+            sinkCharset = Charset.defaultCharset();
+        }
+        try {
+            sourceCharset = Charset.forName(dataFlowConfig.getSourceConfig().getEncodingType());
+        } catch (Throwable t) {
+            log.warn("do not support context encoding type={}, dataflow id={}",
+                    dataFlowConfig.getSourceConfig().getEncodingType(), dataFlowConfig.getDataflowId());
+            sourceCharset = Charset.defaultCharset();
+        }
+        return HttpIdConfig.builder()
+                .inlongGroupId(dataFlowConfig.getInlongGroupId())
+                .inlongStreamId(dataFlowConfig.getInlongStreamId())
+                .path(sinkConfig.getPath())
+                .method(sinkConfig.getMethod())
+                .headers(sinkConfig.getHeaders())
+                .maxRetryTimes(sinkConfig.getMaxRetryTimes())
+                .separator("|")
+                .fieldList(fields)
+                .sinkCharset(sinkCharset)
+                .sourceCharset(sourceCharset)
+                .build();
+    }
+}
