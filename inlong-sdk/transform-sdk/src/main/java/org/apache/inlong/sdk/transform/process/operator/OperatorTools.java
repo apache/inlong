b695fffd3fec00b2334947ca3ced009f2ddc9b71
Zkplo <87751516+Zkplo@users.noreply.github.com>
2024-08-28 15:09:23 +0800

    [INLONG-10864][SDK] Transform SQL support md5 function (#10865)

--- inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/process/operator/OperatorTools.java
+++ inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/process/operator/OperatorTools.java
@@ -39,6 +39,7 @@ import org.apache.inlong.sdk.transform.process.function.Log10Function;
 import org.apache.inlong.sdk.transform.process.function.Log2Function;
 import org.apache.inlong.sdk.transform.process.function.LogFunction;
 import org.apache.inlong.sdk.transform.process.function.LowerFunction;
+import org.apache.inlong.sdk.transform.process.function.Md5Function;
 import org.apache.inlong.sdk.transform.process.function.ModuloFunction;
 import org.apache.inlong.sdk.transform.process.function.NowFunction;
 import org.apache.inlong.sdk.transform.process.function.PowerFunction;
@@ -179,6 +180,7 @@ public class OperatorTools {
         functionMap.put("left", LeftFunction::new);
         functionMap.put("right", RightFunction::new);
         functionMap.put("timestampadd", TimestampAddFunction::new);
+        functionMap.put("md5", Md5Function::new);
     }
 
     public static ExpressionOperator buildOperator(Expression expr) {
