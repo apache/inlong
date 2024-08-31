b2324367e1c8845b20e6bb28dd7651914bf94b3a
Unqyan <2369492695@qq.com>
2024-08-31 17:50:30 +0800

    [INLONG-10963][SDK] Transform SQL support CONTAINS function.(#10963)

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
@@ -724,4 +724,36 @@ public class TestTransformStringFunctionsProcessor {
         Assert.assertEquals("result=1278", output5.get(0));
     }
 
+    @Test
+    public void testContainsFunction() throws Exception {
+        String transformSql = "select contains(string1, string2) from source";
+        TransformConfig config = new TransformConfig(transformSql);
+        TransformProcessor<String, String> processor = TransformProcessor
+                .create(config, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+        // case1: contains('Transform SQL', 'SQL')
+        List<String> output1 = processor.transform("Transform SQL|SQL", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=true");
+        // case2: contains('', 'SQL')
+        List<String> output2 = processor.transform("|SQL", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=false");
+        // case3: contains('Transform SQL', '')
+        List<String> output3 = processor.transform("Transform SQL|", new HashMap<>());
+        Assert.assertEquals(1, output3.size());
+        Assert.assertEquals(output3.get(0), "result=true");
+        // case4: contains('Transform SQL', 'Transformer')
+        List<String> output4 = processor.transform("Transform SQL|Transformer", new HashMap<>());
+        Assert.assertEquals(1, output4.size());
+        Assert.assertEquals(output4.get(0), "result=false");
+        // case5: contains('Transform SQL', 'm S')
+        List<String> output5 = processor.transform("Transform SQL|m S", new HashMap<>());
+        Assert.assertEquals(1, output5.size());
+        Assert.assertEquals(output5.get(0), "result=true");
+        // case6: contains('', '')
+        List<String> output6 = processor.transform("|", new HashMap<>());
+        Assert.assertEquals(1, output6.size());
+        Assert.assertEquals(output6.get(0), "result=true");
+    }
 }
