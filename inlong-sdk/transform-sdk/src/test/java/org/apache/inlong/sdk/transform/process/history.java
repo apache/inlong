b6dec70dcffdb7895788a36ea7810370eff37f75
Zhang Tianrui <15903703590@163.com>
2024-09-02 20:50:52 +0800

    [INLONG-10873][SDK]Transform support factorial function

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/history.java
+++
@@ -1,6 +0,0 @@
-public static void main(String[] args) {
-    BigDecimal number = new BigDecimal("5.5");
-    if (number.scale() > 0) {
-        throw new IllegalArgumentException("Factorial is only defined for non-negative integers.");
-    }
-}
