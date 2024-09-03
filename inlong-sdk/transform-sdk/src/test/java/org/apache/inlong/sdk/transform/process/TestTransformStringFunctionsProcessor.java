32b67c2c679c1c33eee5633dc460f6964e7bb358
Moon <2267016840@qq.com>
2024-09-03 08:29:16 +0800

    [INLONG-10882][SDK] Transform SQL support ASCII function

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
@@ -690,4 +690,34 @@ public class TestTransformStringFunctionsProcessor {
         Assert.assertEquals(output3.get(0), "result=Apache Inlong");
     }
 
+    @Test
+    public void testAsciiFunction() throws Exception {
+        String transformSql1 = "select ascii(string1) from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+
+        // case1: ascii('abc') -> 97
+        List<String> output1 = processor1.transform("abc|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=97");
+
+        // case1: ascii('A') -> 65
+        output1 = processor1.transform("A|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=65");
+
+        String transformSql2 = "select ascii(stringX) from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+
+        // case3: ascii(null) -> null
+        List<String> output2 = processor2.transform("A|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=null");
+    }
+
 }
