a3cb361826c47036fd982cef4ad5385e281f505b
Moon <2267016840@qq.com>
2024-09-03 08:29:16 +0800

    [INLONG-10882][SDK] Transform SQL support ASCII function

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
@@ -697,7 +697,6 @@ public class TestTransformStringFunctionsProcessor {
         TransformProcessor<String, String> processor1 = TransformProcessor
                 .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
                         SinkEncoderFactory.createKvEncoder(kvSink));
-
         // case1: insert('12345678', 3, 4, 'word') -> '12word78'
         List<String> output1 = processor1.transform("12345678|word|cloud|3|4|0", new HashMap<>());
         Assert.assertEquals(1, output1.size());
@@ -722,6 +721,41 @@ public class TestTransformStringFunctionsProcessor {
         List<String> output5 = processor1.transform("12345678||cloud|3|4|0", new HashMap<>());
         Assert.assertEquals(1, output5.size());
         Assert.assertEquals("result=1278", output5.get(0));
+    }
+//=======
+
+    @Test
+    public void testAsciiFunction() throws Exception {
+        String transformSql1 = "select ascii(string1) from source";
+//>>>>>>> 32b67c2c6 ([INLONG-10882][SDK] Transform SQL support ASCII function)
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+
+//<<<<<<< HEAD
+//=======
+        // case1: ascii('abc') -> 97
+        List<String> output1 = processor1.transform("abc|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=97");
+
+        // case1: ascii('A') -> 65
+        output1 = processor1.transform("A|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals(output1.get(0), "result=65");
+
+        String transformSql2 = "select ascii(stringX) from source";
+        TransformConfig config2 = new TransformConfig(transformSql2);
+        TransformProcessor<String, String> processor2 = TransformProcessor
+                .create(config2, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+
+        // case3: ascii(null) -> null
+        List<String> output2 = processor2.transform("A|el|EL|2|1|3", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals(output2.get(0), "result=null");
+//>>>>>>> 32b67c2c6 ([INLONG-10882][SDK] Transform SQL support ASCII function)
     }
 
 }
