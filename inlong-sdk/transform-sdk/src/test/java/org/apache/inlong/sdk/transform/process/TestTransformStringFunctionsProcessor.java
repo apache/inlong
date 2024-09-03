777d29cc29a94cd8108a6bce04a959a6c5b9d9ed
Xincheng Huang <60057611+ying-hua@users.noreply.github.com>
2024-09-03 10:25:52 +0800

    [INLONG-10971][SDK] Transform support INSERT function (#10972)

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformStringFunctionsProcessor.java
@@ -714,4 +714,38 @@ public class TestTransformStringFunctionsProcessor {
         Assert.assertEquals(output3.get(0), "result=Apache Inlong");
     }
 
+    @Test
+    public void testInsertFunction() throws Exception {
+        String transformSql1 = "select insert(string1, numeric1, numeric2, string2) from source";
+        TransformConfig config1 = new TransformConfig(transformSql1);
+        TransformProcessor<String, String> processor1 = TransformProcessor
+                .create(config1, SourceDecoderFactory.createCsvDecoder(csvSource),
+                        SinkEncoderFactory.createKvEncoder(kvSink));
+
+        // case1: insert('12345678', 3, 4, 'word') -> '12word78'
+        List<String> output1 = processor1.transform("12345678|word|cloud|3|4|0", new HashMap<>());
+        Assert.assertEquals(1, output1.size());
+        Assert.assertEquals("result=12word78", output1.get(0));
+
+        // case2: insert('12345678', -1, 4, 'word') -> '12345678'
+        List<String> output2 = processor1.transform("12345678|word|cloud|-1|4|0", new HashMap<>());
+        Assert.assertEquals(1, output2.size());
+        Assert.assertEquals("result=12345678", output2.get(0));
+
+        // case3: insert('12345678', 3, 100, 'word') -> '12word'
+        List<String> output3 = processor1.transform("12345678|word|cloud|3|100|0", new HashMap<>());
+        Assert.assertEquals(1, output3.size());
+        Assert.assertEquals("result=12word", output3.get(0));
+
+        // case4: insert('', 3, 4, 'word') -> ''
+        List<String> output4 = processor1.transform("|word|cloud|3|4|0", new HashMap<>());
+        Assert.assertEquals(1, output4.size());
+        Assert.assertEquals("result=", output4.get(0));
+
+        // case5: insert('12345678', 3, 4, '') -> '1278'
+        List<String> output5 = processor1.transform("12345678||cloud|3|4|0", new HashMap<>());
+        Assert.assertEquals(1, output5.size());
+        Assert.assertEquals("result=1278", output5.get(0));
+    }
+
 }
