3c0179e6485f0992636de499e950aba33d10b909
卢春亮 <946240095@qq.com>
2024-09-05 15:03:58 +0800

    Add ignoreConfigError config

--- inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformProcessor.java
+++ inlong-sdk/transform-sdk/src/test/java/org/apache/inlong/sdk/transform/process/TestTransformProcessor.java
@@ -28,6 +28,7 @@ import org.apache.inlong.sdk.transform.pojo.KvSourceInfo;
 import org.apache.inlong.sdk.transform.pojo.PbSourceInfo;
 import org.apache.inlong.sdk.transform.pojo.TransformConfig;
 
+import net.sf.jsqlparser.JSQLParserException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -462,5 +463,21 @@ public class TestTransformProcessor {
                 processor1.transform("key1=string11&key2=string12&key3=number11&key4=number12", new HashMap<>());
         Assert.assertEquals(1, output1.size());
         Assert.assertEquals("field1=string11&field2=&field3=number12", output1.get(0));
+    }
+
+    @Test
+    public void testKv2KvForConfigError() throws Exception {
+        List<FieldInfo> sourceFields = this.getTestFieldList("key1", "key2", "key3", "key4");
+        KvSourceInfo kvSource = new KvSourceInfo("UTF-8", sourceFields);
+        List<FieldInfo> sinkFields = this.getTestFieldList("field1", "field2", "field3");
+        KvSinkInfo kvSink = new KvSinkInfo("UTF-8", sinkFields);
+        String transformSql = "select key4 as field3, key2 as field6, key1 as field1";
+        TransformConfig config = new TransformConfig(transformSql, new HashMap<>(), false, false);
+        // case1
+        Assert.assertThrows(JSQLParserException.class, () -> {
+            TransformProcessor
+                    .create(config, SourceDecoderFactory.createKvDecoder(kvSource),
+                            SinkEncoderFactory.createKvEncoder(kvSink));
+        });
     }
 }
