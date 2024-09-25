c8573433389514c2b96d3376420364bf012e0e49
emptyOVO <gaoyifei040229@163.com>
2024-09-25 22:05:57 +0800

    fix: use local sender to resend messages

--- inlong-sdk/dataproxy-sdk/src/main/java/org/apache/inlong/sdk/dataproxy/DefaultMessageSender.java
+++ inlong-sdk/dataproxy-sdk/src/main/java/org/apache/inlong/sdk/dataproxy/DefaultMessageSender.java
@@ -66,7 +66,6 @@ public class DefaultMessageSender implements MessageSender {
     private boolean isReport = false;
     private boolean isSupportLF = false;
     private int cpsSize = ConfigConstants.COMPRESS_SIZE;
-    private boolean sendSuccess = true;
     private final int senderMaxRetry;
 
     public DefaultMessageSender(ProxyClientConfig configure) throws Exception {
@@ -195,80 +194,39 @@ public class DefaultMessageSender implements MessageSender {
         return ConfigConstants.PROXY_SDK_VERSION;
     }
 
-    public void setSendSuccess(boolean sendSuccess) {
-        this.sendSuccess = sendSuccess;
-    }
-
-    public boolean isSendSuccess() {
-        return sendSuccess;
-    }
-
-    private SendResult retryWhenSendMessageFail(Function<DefaultMessageSender, SendResult> sendOperation,
-            DefaultMessageSender initialSender) {
+    private SendResult retryWhenSendMessageFail(Function<Sender, SendResult> sendOperation) {
         int attempts = 0;
         SendResult sendResult = null;
-        DefaultMessageSender currentSender = initialSender;
-        while (attempts < this.senderMaxRetry) {
-            sendResult = sendOperation.apply(currentSender);
+        while (attempts < this.senderMaxRetry + 1) {
+            sendResult = sendOperation.apply(sender);
             if (sendResult != null && sendResult.equals(SendResult.OK)) {
-                currentSender.setSendSuccess(true);
                 return sendResult;
-            }
-            currentSender.setSendSuccess(false);
-            // try to get another success sender
-            DefaultMessageSender anotherSender = getAnotherSuccessSender(currentSender);
-            if (anotherSender != null) {
-                currentSender = anotherSender;
-            } else {
-                break;
             }
             attempts++;
         }
-
         return sendResult;
     }
 
-    private String retryWhenSendMessageIndexFail(Function<DefaultMessageSender, String> sendOperation,
-            DefaultMessageSender initialSender) {
+    private String retryWhenSendMessageIndexFail(Function<Sender, String> sendOperation) {
         int attempts = 0;
         String sendIndexResult = null;
-        DefaultMessageSender currentSender = initialSender;
-        while (attempts < this.senderMaxRetry) {
-            sendIndexResult = sendOperation.apply(currentSender);
+        while (attempts < this.senderMaxRetry + 1) {
+            sendIndexResult = sendOperation.apply(sender);
             if (sendIndexResult != null && sendIndexResult.startsWith(SendResult.OK.toString())) {
-                currentSender.setSendSuccess(true);
                 return sendIndexResult;
-            }
-            currentSender.setSendSuccess(false);
-            // try to get success sender
-            DefaultMessageSender randomSuccessSender = getAnotherSuccessSender(currentSender);
-            if (randomSuccessSender != null) {
-                currentSender = randomSuccessSender;
-            } else {
-                break;
             }
             attempts++;
         }
-
         return sendIndexResult;
-    }
-
-    private DefaultMessageSender getAnotherSuccessSender(DefaultMessageSender currentSender) {
-        for (DefaultMessageSender sender : CACHE_SENDER.values()) {
-            if (sender != null && sender.isSendSuccess() && !sender.equals(currentSender)) {
-                return sender;
-            }
-        }
-        return null;
     }
 
     @Deprecated
     public SendResult sendMessage(byte[] body, String attributes, String msgUUID,
             long timeout, TimeUnit timeUnit) {
-        Function<DefaultMessageSender, SendResult> sendOperation =
-                (currentSender) -> currentSender.sender.syncSendMessage(
-                        new EncodeObject(body, attributes, idGenerator.getNextId()), msgUUID, timeout, timeUnit);
-        return retryWhenSendMessageFail(sendOperation, this);
+        Function<Sender, SendResult> sendOperation =
+                (sender) -> sender.syncSendMessage(new EncodeObject(body, attributes, idGenerator.getNextId()), msgUUID,
+                        timeout, timeUnit);
+        return retryWhenSendMessageFail(sendOperation);
     }
 
     public SendResult sendMessage(byte[] body, String groupId, String streamId, long dt, String msgUUID,
@@ -308,29 +266,31 @@ public class DefaultMessageSender implements MessageSender {
             EncodeObject encodeObject = new EncodeObject(body, msgtype, isCompressEnd, isReport,
                     isGroupIdTransfer, dt / 1000, idGenerator.getNextInt(), groupId, streamId, proxySend);
             encodeObject.setSupportLF(isSupportLF);
-            Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessage(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageFail(sendOperation, this);
+            Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageFail(sendOperation);
         } else if (msgtype == 3 || msgtype == 5) {
             if (isProxySend) {
                 proxySend = "&" + proxySend;
             }
             final String finalProxySend = proxySend;
             final long finalDt = dt;
+            Function<Sender, SendResult> sendOperation;
             if (isCompressEnd) {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(body, "groupId=" + groupId + "&streamId="
-                                + streamId + "&dt=" + finalDt + "&cp=snappy" + finalProxySend, idGenerator.getNextId(),
-                                this.getMsgtype(), true, groupId), msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(body,
+                        "groupId=" + groupId + "&streamId=" + streamId + "&dt=" + finalDt + "&cp=snappy"
+                                + finalProxySend,
+                        idGenerator.getNextId(), this.getMsgtype(),
+                        true, groupId), msgUUID, timeout, timeUnit);
             } else {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(body,
-                                "groupId=" + groupId + "&streamId=" + streamId + "&dt=" + finalDt + finalProxySend,
-                                idGenerator.getNextId(), this.getMsgtype(), false, groupId), msgUUID, timeout,
-                                timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(body,
+                        "groupId=" + groupId + "&streamId=" + streamId + "&dt=" + finalDt
+                                + finalProxySend,
+                        idGenerator.getNextId(), this.getMsgtype(),
+                        false, groupId), msgUUID, timeout, timeUnit);
+
             }
+            return retryWhenSendMessageFail(sendOperation);
         }
 
         return null;
@@ -376,24 +336,22 @@ public class DefaultMessageSender implements MessageSender {
                     isGroupIdTransfer, dt / 1000,
                     idGenerator.getNextInt(), groupId, streamId, attrs.toString());
             encodeObject.setSupportLF(isSupportLF);
-            Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessage(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageFail(sendOperation, this);
+            Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageFail(sendOperation);
         } else if (msgtype == 3 || msgtype == 5) {
             attrs.append("&groupId=").append(groupId).append("&streamId=").append(streamId).append("&dt=").append(dt);
             if (isCompressEnd) {
                 attrs.append("&cp=snappy");
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(body, attrs.toString(),
-                                idGenerator.getNextId(), this.getMsgtype(), true, groupId),
-                                msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(body,
+                        attrs.toString(), idGenerator.getNextId(), this.getMsgtype(), true, groupId),
+                        msgUUID, timeout, timeUnit);
+                return retryWhenSendMessageFail(sendOperation);
             } else {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(body, attrs.toString(),
-                                idGenerator.getNextId(), this.getMsgtype(), false, groupId), msgUUID,
-                                timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(body,
+                        attrs.toString(), idGenerator.getNextId(), this.getMsgtype(), false, groupId),
+                        msgUUID, timeout, timeUnit);
+                return retryWhenSendMessageFail(sendOperation);
             }
         }
         return null;
@@ -436,29 +394,30 @@ public class DefaultMessageSender implements MessageSender {
                     isGroupIdTransfer, dt / 1000,
                     idGenerator.getNextInt(), groupId, streamId, proxySend);
             encodeObject.setSupportLF(isSupportLF);
-            Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessage(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageFail(sendOperation, this);
+            Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageFail(sendOperation);
         } else if (msgtype == 3 || msgtype == 5) {
             if (isProxySend) {
                 proxySend = "&" + proxySend;
             }
             final long finalDt = dt;
             final String finalProxySend = proxySend;
+            Function<Sender, SendResult> sendOperation;
             if (isCompress) {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(bodyList, "groupId=" + groupId + "&streamId=" + streamId
-                                + "&dt=" + finalDt + "&cp=snappy" + "&cnt=" + bodyList.size() + finalProxySend,
-                                idGenerator.getNextId(), this.getMsgtype(), true, groupId), msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(bodyList,
+                        "groupId=" + groupId + "&streamId=" + streamId + "&dt=" + finalDt + "&cp=snappy" + "&cnt="
+                                + bodyList.size() + finalProxySend,
+                        idGenerator.getNextId(), this.getMsgtype(),
+                        true, groupId), msgUUID, timeout, timeUnit);
             } else {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(bodyList, "groupId=" + groupId + "&streamId=" + streamId
-                                + "&dt=" + finalDt + "&cnt=" + bodyList.size() + finalProxySend,
-                                idGenerator.getNextId(),
-                                this.getMsgtype(), false, groupId), msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                sendOperation = (sender) -> sender.syncSendMessage(new EncodeObject(bodyList,
+                        "groupId=" + groupId + "&streamId=" + streamId + "&dt=" + finalDt + "&cnt=" + bodyList.size()
+                                + finalProxySend,
+                        idGenerator.getNextId(), this.getMsgtype(),
+                        false, groupId), msgUUID, timeout, timeUnit);
             }
+            return retryWhenSendMessageFail(sendOperation);
         }
         return null;
     }
@@ -500,25 +459,24 @@ public class DefaultMessageSender implements MessageSender {
                     isGroupIdTransfer, dt / 1000,
                     idGenerator.getNextInt(), groupId, streamId, attrs.toString());
             encodeObject.setSupportLF(isSupportLF);
-            Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessage(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageFail(sendOperation, this);
+            Function<Sender, SendResult> sendOperation = (sender) -> sender.syncSendMessage(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageFail(sendOperation);
         } else if (msgtype == 3 || msgtype == 5) {
             attrs.append("&groupId=").append(groupId).append("&streamId=").append(streamId)
                     .append("&dt=").append(dt).append("&cnt=").append(bodyList.size());
             if (isCompress) {
                 attrs.append("&cp=snappy");
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(bodyList, attrs.toString(),
-                                idGenerator.getNextId(), this.getMsgtype(), true, groupId),
-                                msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                Function<Sender, SendResult> sendOperation =
+                        (sender) -> sender.syncSendMessage(new EncodeObject(bodyList, attrs.toString(),
+                                idGenerator.getNextId(), this.getMsgtype(), true, groupId), msgUUID, timeout, timeUnit);
+                return retryWhenSendMessageFail(sendOperation);
             } else {
-                Function<DefaultMessageSender, SendResult> sendOperation = (currentSender) -> currentSender.sender
-                        .syncSendMessage(new EncodeObject(bodyList, attrs.toString(),
-                                idGenerator.getNextId(), this.getMsgtype(), false, groupId),
-                                msgUUID, timeout, timeUnit);
-                return retryWhenSendMessageFail(sendOperation, this);
+                Function<Sender, SendResult> sendOperation =
+                        (sender) -> sender.syncSendMessage(new EncodeObject(bodyList, attrs.toString(),
+                                idGenerator.getNextId(), this.getMsgtype(), false, groupId), msgUUID, timeout,
+                                timeUnit);
+                return retryWhenSendMessageFail(sendOperation);
             }
         }
         return null;
@@ -909,9 +867,9 @@ public class DefaultMessageSender implements MessageSender {
                     isReport, isGroupIdTransfer, dt / 1000,
                     sid, groupId, streamId, attrs.toString(), "data", "");
             encodeObject.setSupportLF(isSupportLF);
-            Function<DefaultMessageSender, String> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessageIndex(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageIndexFail(sendOperation, this);
+            Function<Sender, String> sendOperation = (sender) -> sender.syncSendMessageIndex(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageIndexFail(sendOperation);
         }
         return null;
     }
@@ -926,9 +884,9 @@ public class DefaultMessageSender implements MessageSender {
         if (msgtype == 7 || msgtype == 8) {
             EncodeObject encodeObject = new EncodeObject(body, msgtype, false, isReport,
                     isGroupIdTransfer, dt / 1000, sid, groupId, streamId, "", messageKey, ip);
-            Function<DefaultMessageSender, String> sendOperation = (currentSender) -> currentSender.sender
-                    .syncSendMessageIndex(encodeObject, msgUUID, timeout, timeUnit);
-            return retryWhenSendMessageIndexFail(sendOperation, this);
+            Function<Sender, String> sendOperation = (sender) -> sender.syncSendMessageIndex(encodeObject, msgUUID,
+                    timeout, timeUnit);
+            return retryWhenSendMessageIndexFail(sendOperation);
         }
         return null;
     }
