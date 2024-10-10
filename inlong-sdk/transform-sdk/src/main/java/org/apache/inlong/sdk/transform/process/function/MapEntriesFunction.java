25e786b82886a682d36cd3d9cd045c1e2d3a9bfe
emptyOVO <118812562+emptyOVO@users.noreply.github.com>
2024-10-10 11:33:44 +0800

    [INLONG-11283][SDK]Transform enhance Map-related Collection Functions (#11317)
    * [INLONG-11283][SDK]Transform enhance Map-related Collection Functions
    
    * feat: add description for functions

---
+++ inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/process/function/MapEntriesFunction.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sdk.transform.process.function;
+
+import org.apache.inlong.sdk.transform.decode.SourceData;
+import org.apache.inlong.sdk.transform.process.Context;
+import org.apache.inlong.sdk.transform.process.operator.OperatorTools;
+import org.apache.inlong.sdk.transform.process.parser.ValueParser;
+
+import net.sf.jsqlparser.expression.Function;
+
+import java.util.Arrays;
+import java.util.Map;
+/**
+ * MapEntriesFunction
+ * description: MAP_ENTRIES(map)--Returns an array of all entries in the given map. No order guaranteed.
+ * for example: map_entries(Map('he',1,'xxd','cloud'))--return [he=1, xxd=cloud]
+ *              map_entries(Map(1,2,'cloud','xxd'))--return [xxd=cloud, 1=2]
+ */
+@TransformFunction(names = {"map_entries"})
+public class MapEntriesFunction implements ValueParser {
+
+    private final ValueParser mapParser;
+
+    public MapEntriesFunction(Function expr) {
+        this.mapParser = OperatorTools.buildParser(expr.getParameters().getExpressions().get(0));
+    }
+
+    @Override
+    public Object parse(SourceData sourceData, int rowIndex, Context context) {
+        Object mapObj = mapParser.parse(sourceData, rowIndex, context);
+        if (mapObj == null) {
+            return null;
+        }
+        if (mapObj instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) mapObj;
+            return Arrays.toString(map.entrySet().toArray(new Map.Entry[0]));
+        }
+        return null;
+    }
+}
---
+++ inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/process/function/MapValueFunction.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.sdk.transform.process.function;
+
+import org.apache.inlong.sdk.transform.decode.SourceData;
+import org.apache.inlong.sdk.transform.process.Context;
+import org.apache.inlong.sdk.transform.process.operator.OperatorTools;
+import org.apache.inlong.sdk.transform.process.parser.ValueParser;
+
+import net.sf.jsqlparser.expression.Function;
+
+import java.util.Arrays;
+import java.util.Map;
+/**
+ * MapValueFunction
+ * description: MAP_VALUES(map)--Returns the values of the map as array. No order guaranteed.
+ * for example: map_values(Map('he',1,'xxd','cloud'))--return [1, cloud]
+ *              map_values(Map('xxd','cloud',map(1,2),map(3,'apple')))--return [cloud, {3=apple}]
+ */
+@TransformFunction(names = {"map_values"})
+public class MapValueFunction implements ValueParser {
+
+    private final ValueParser mapParser;
+
+    public MapValueFunction(Function expr) {
+        this.mapParser = OperatorTools.buildParser(expr.getParameters().getExpressions().get(0));
+    }
+
+    @Override
+    public Object parse(SourceData sourceData, int rowIndex, Context context) {
+        Object mapObj = mapParser.parse(sourceData, rowIndex, context);
+        if (mapObj == null) {
+            return null;
+        }
+        if (mapObj instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) mapObj;
+            if (map.isEmpty()) {
+                return null;
+            }
+            return Arrays.toString(map.values().toArray());
+        }
+        return null;
+    }
+
+}
