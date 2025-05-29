ffac710ae7cc981d595c5215aaf55bbec4e06732
Marc Venturini <marc.venturini@gmail.com>
2025-05-29 10:28:32 +0800

    [INLONG-11871][SDK] Fix init crashes when no IP available (#11872)

--- inlong-sdk/dataproxy-sdk-twins/dataproxy-sdk-golang/util/id.go
+++ inlong-sdk/dataproxy-sdk-twins/dataproxy-sdk-golang/util/id.go
@@ -18,6 +18,7 @@ package util
 
 import (
 	"log"
+	"sync"
 
 	"github.com/bwmarrin/snowflake"
 	"github.com/google/uuid"
@@ -25,20 +26,22 @@ import (
 )
 
 var (
+	snowflakeOnce sync.Once
 	snowflakeNode *snowflake.Node
+	snowflakeErr  error
 )
 
-func init() {
+func newSnowFlakeNode() (*snowflake.Node, error) {
 	ip, err := GetOneIP()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
 	id := IPtoUInt(ip)
-	snowflakeNode, err = snowflake.NewNode(int64(id % 1024))
+	node, err := snowflake.NewNode(int64(id % 1024))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	return node, nil
 }
 
 // UInt64UUID generates an uint64 UUID
@@ -53,7 +56,23 @@ func UInt64UUID() (uint64, error) {
 	return cityhash.CityHash64WithSeeds(bytes, uint32(length), 13329145742295551469, 7926974186468552394), nil
 }
 
-// SnowFlakeID generates a snowflake ID
+// Deprecated: Use SafeSnowFlakeID instead.
+// SnowFlakeID generates a snowflake ID. If an error occurs, it logs it and exits.
 func SnowFlakeID() string {
-	return snowflakeNode.Generate().String()
+	id, err := SafeSnowFlakeID()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return id
+}
+
+// SafeSnowFlakeID generates a snowflake ID. If an error occurs it returns it.
+func SafeSnowFlakeID() (string, error) {
+	snowflakeOnce.Do(func() {
+		snowflakeNode, snowflakeErr = newSnowFlakeNode()
+	})
+	if snowflakeErr != nil {
+		return "", snowflakeErr
+	}
+	return snowflakeNode.Generate().String(), nil
 }
