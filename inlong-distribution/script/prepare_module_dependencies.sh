f272ccf1dd6c8dea197c3edc310d619a60690870
yfsn666 <61183968+yfsn666@users.noreply.github.com>
2025-05-30 16:50:24 +0800

    [INLONG-11875][Manager] Fix the startup script in standalone deployment mode does not copy module dependencies (#11876)
    Co-authored-by: jameswyli <jameswyli@tencent.com>

--- inlong-distribution/script/prepare_module_dependencies.sh
+++ inlong-distribution/script/prepare_module_dependencies.sh
@@ -20,12 +20,8 @@
 #
 cd "$(dirname "$0")"/../ || exit
 
-jar_file_num=`ls -l $1 |grep jar |wc -l`
-
-if [ $jar_file_num -eq 0 ]; then
-    if [ -e $1/dependencys.txt ]; then
-        for line in $(cat "$1/dependencys.txt"); do
-            cp "./lib/$line" $1/
-        done
-    fi
+if [ -e $1/dependencys.txt ]; then
+    for line in $(cat "$1/dependencys.txt"); do
+        cp -n "./lib/$line" $1/ 2>/dev/null
+    done
 fi
