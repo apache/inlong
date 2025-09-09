ea272663f4a1d539fd2595bc40c9e24a578e1c10
lsq <15297823178@163.com>
2025-09-09 12:50:42 +0800

    [INLONG-11923][Audit] Add the function of parsing Audit's basic indicator data from the database and reporting it to prometheus

--- inlong-audit/audit-tool/src/main/java/org/apache/inlong/audit/tool/util/CommonBeanUtils.java
+++ inlong-audit/audit-tool/src/main/java/org/apache/inlong/audit/tool/util/CommonBeanUtils.java
@@ -1,5 +1,3 @@
-package org.apache.inlong.audit.tool.util;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.inlong.audit.tool.util;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.inlong.audit.tool.util;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
