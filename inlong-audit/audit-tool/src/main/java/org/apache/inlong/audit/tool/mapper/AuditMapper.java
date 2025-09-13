71a9961752a15ffd33bc1a6b70cb8929786367dc
lsq <15297823178@163.com>
2025-09-13 23:48:14 +0800

    [Feature][Audit]Implement API for obtaining audit data in Audit Tool

---
+++ inlong-audit/audit-tool/src/main/java/org/apache/inlong/audit/tool/mapper/AuditMapper.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.audit.tool.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.inlong.audit.tool.entity.AuditMetric;
+
+import java.util.List;
+
+@Mapper
+public interface AuditMapper {
+
+    @Select("select inlong_group_id as inlongGroupId,inlong_stream_id as inlongStreamId, sum(count) count" +
+            " from audit_data where audit_id = #{audit_id}" +
+            " and log_ts between #{startLogTs} and #{endLogTs} group by inlong_group_id,inlong_stream_id")
+    List<AuditMetric> getAuditMetrics(@Param("startLogTs") String startLogTs, @Param("endLogTs") String endLogTs,
+                                      @Param("audit_id") String auditId);
+}
---
+++ inlong-audit/audit-tool/src/test/java/org/apache/inlong/tool/service/AuditMetricServiceTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.tool.service;
+
+import org.apache.inlong.audit.tool.config.AppConfig;
+import org.apache.inlong.audit.tool.entity.AuditMetric;
+import org.apache.inlong.audit.tool.service.AuditMetricService;
+import org.apache.inlong.audit.tool.util.AuditSQLUtil;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class AuditMetricServiceTest {
+
+    @Test
+    public void testGetDataProxyAuditMetrics() {
+        // Query service initialization
+        AppConfig appConfig = new AppConfig();
+        AuditSQLUtil.initialize(appConfig.getProperties());
+        AuditMetricService auditMetricService = new AuditMetricService();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String endLogTs = LocalDateTime.now().minusMinutes(1).format(formatter);
+        String startLogTs = LocalDateTime.now().minusMinutes(5).format(formatter);
+
+        // Search for relevant data
+        List<AuditMetric> dataproxyAuditMetrics =
+                auditMetricService.getStorageAuditMetrics("5", startLogTs, endLogTs);
+
+        for (AuditMetric auditMetric : dataproxyAuditMetrics) {
+            System.out.println(auditMetric.getInlongGroupId() + " " + auditMetric.getInlongStreamId() + " "
+                    + auditMetric.getCount());
+        }
+    }
+}
