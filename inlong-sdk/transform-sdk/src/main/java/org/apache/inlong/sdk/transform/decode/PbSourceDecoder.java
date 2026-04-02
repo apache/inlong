3d1121dfd06975b722d5cb6f77babfbec2088acf
ChunLiang Lu <946240095@qq.com>
2026-04-02 20:38:05 +0800

    [INLONG-12108][SDK] TransformSDK supports decoding and transformation of ProtoBuf description files with multiple Proto files and multi-level NestedType

--- inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/decode/PbSourceDecoder.java
+++ inlong-sdk/transform-sdk/src/main/java/org/apache/inlong/sdk/transform/decode/PbSourceDecoder.java
@@ -17,22 +17,26 @@
 
 package org.apache.inlong.sdk.transform.decode;
 
-import org.apache.inlong.sdk.transform.pojo.PbSourceInfo;
-import org.apache.inlong.sdk.transform.process.Context;
-
 import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
 import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
+
 import org.apache.commons.lang3.StringUtils;
+import org.apache.inlong.sdk.transform.pojo.PbSourceInfo;
+import org.apache.inlong.sdk.transform.process.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Base64;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -71,9 +75,11 @@ public class PbSourceDecoder extends SourceDecoder<String> {
             // parse description
             byte[] protoBytes = Base64.getDecoder().decode(protoDescription);
             DescriptorProtos.FileDescriptorSet descriptorSet = DescriptorProtos.FileDescriptorSet.parseFrom(protoBytes);
-            DescriptorProtos.FileDescriptorProto fileDesc = descriptorSet.getFile(0);
+            // format proto
+            DescriptorProtos.FileDescriptorProto fileDesc = formatFileDescription(descriptorSet, rootMessageType);
             Descriptors.FileDescriptor fileDescriptor = Descriptors.FileDescriptor.buildFrom(fileDesc,
                     new Descriptors.FileDescriptor[]{});
+            // find root
             this.rootDesc = fileDescriptor.findMessageTypeByName(rootMessageType);
             // child
             this.rowsNodePath = sourceInfo.getRowsNodePath();
@@ -155,5 +161,137 @@ public class PbSourceDecoder extends SourceDecoder<String> {
     public SourceData decode(String input, Context context) {
         byte[] srcBytes = Base64.getDecoder().decode(input);
         return decode(srcBytes, context);
+    }
+
+    private static DescriptorProtos.FileDescriptorProto formatFileDescription(
+            DescriptorProtos.FileDescriptorSet descriptorSet,
+            String rootMessageType) throws DescriptorValidationException, IOException {
+        // load
+        Map<String, DescriptorProtos.DescriptorProto> messageTypeMap = new ConcurrentHashMap<>();
+        Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap = new ConcurrentHashMap<>();
+        loadTypeFromFileSet(descriptorSet, messageTypeMap, enumTypeMap);
+        // find root type
+        String rootTypeName = findRootType(descriptorSet, rootMessageType);
+        // build root type
+        DescriptorProtos.FileDescriptorProto.Builder newFileBuilder = DescriptorProtos.FileDescriptorProto.newBuilder();
+        Set<String> addedTypeNames = new HashSet<>();
+        formatMessageTypeDescription(rootTypeName, newFileBuilder, messageTypeMap, enumTypeMap, addedTypeNames);
+        // DescriptorProtos.FileDescriptorSet newDescriptorSet = DescriptorProtos.FileDescriptorSet.newBuilder()
+        // .addFile(newFileBuilder).build();
+        // byte[] newProtoBytes = newDescriptorSet.toByteArray();
+        return newFileBuilder.build();
+    }
+
+    private static void formatMessageTypeDescription(String typeName,
+            DescriptorProtos.FileDescriptorProto.Builder newFileBuilder,
+            Map<String, DescriptorProtos.DescriptorProto> messageTypeMap,
+            Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap,
+            Set<String> addedTypeNames) {
+        DescriptorProtos.DescriptorProto typeDesc = messageTypeMap.get(typeName);
+        if (typeDesc == null) {
+            return;
+        }
+        DescriptorProtos.DescriptorProto.Builder typeBuilder = DescriptorProtos.DescriptorProto.newBuilder();
+        String newTypeName = typeName.substring(1).replace('.', '_');
+        typeBuilder.setName(newTypeName);
+        for (DescriptorProtos.FieldDescriptorProto fieldDesc : typeDesc.getFieldList()) {
+            DescriptorProtos.FieldDescriptorProto.Builder fieldBuilder = DescriptorProtos.FieldDescriptorProto
+                    .newBuilder().mergeFrom(fieldDesc);
+            if (fieldDesc.getType().equals(FieldDescriptorProto.Type.TYPE_MESSAGE)) {
+                String fieldTypeName = fieldDesc.getTypeName();
+                String newFieldTypeName = fieldTypeName.substring(1).replace('.', '_');
+                fieldBuilder.setTypeName(newFieldTypeName);
+                if (!addedTypeNames.contains(newFieldTypeName)) {
+                    addedTypeNames.add(newFieldTypeName);
+                    formatMessageTypeDescription(fieldTypeName, newFileBuilder, messageTypeMap, enumTypeMap,
+                            addedTypeNames);
+                }
+            } else if (fieldDesc.getType().equals(FieldDescriptorProto.Type.TYPE_ENUM)) {
+                String fieldTypeName = fieldDesc.getTypeName();
+                String newFieldTypeName = fieldTypeName.substring(1).replace('.', '_');
+                fieldBuilder.setTypeName(newFieldTypeName);
+                if (!addedTypeNames.contains(newFieldTypeName)) {
+                    addedTypeNames.add(newFieldTypeName);
+                    formatEnumTypeDescription(fieldTypeName, newFileBuilder, enumTypeMap, addedTypeNames);
+                }
+            }
+            typeBuilder.addField(fieldBuilder);
+        }
+        newFileBuilder.addMessageType(typeBuilder.build());
+    }
+
+    private static void formatEnumTypeDescription(String typeName,
+            DescriptorProtos.FileDescriptorProto.Builder newFileBuilder,
+            Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap,
+            Set<String> addedTypeNames) {
+        DescriptorProtos.EnumDescriptorProto typeDesc = enumTypeMap.get(typeName);
+        if (typeDesc == null) {
+            return;
+        }
+        DescriptorProtos.EnumDescriptorProto.Builder typeBuilder = DescriptorProtos.EnumDescriptorProto.newBuilder()
+                .mergeFrom(typeDesc);
+        String newTypeName = typeName.substring(1).replace('.', '_');
+        typeBuilder.setName(newTypeName);
+        newFileBuilder.addEnumType(typeBuilder.build());
+    }
+
+    private static String findRootType(DescriptorProtos.FileDescriptorSet descriptorSet,
+            String rootMessageType) {
+        for (DescriptorProtos.FileDescriptorProto fileDesc : descriptorSet.getFileList()) {
+            String packageName = fileDesc.getPackage();
+            String packagePrefix = "";
+            if (!StringUtils.isBlank(packageName)) {
+                packagePrefix = "." + packageName;
+            }
+            // message type
+            for (DescriptorProtos.DescriptorProto typeDesc : fileDesc.getMessageTypeList()) {
+                if (StringUtils.equals(typeDesc.getName(), rootMessageType)) {
+                    String fullTypeName = packagePrefix + "." + typeDesc.getName();
+                    return fullTypeName;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void loadTypeFromFileSet(DescriptorProtos.FileDescriptorSet descriptorSet,
+            Map<String, DescriptorProtos.DescriptorProto> messageTypeMap,
+            Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap) {
+        for (DescriptorProtos.FileDescriptorProto fileDesc : descriptorSet.getFileList()) {
+            loadTypeFromFile(fileDesc, messageTypeMap, enumTypeMap);
+        }
+    }
+
+    private static void loadTypeFromFile(DescriptorProtos.FileDescriptorProto fileDesc,
+            Map<String, DescriptorProtos.DescriptorProto> messageTypeMap,
+            Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap) {
+        String packageName = fileDesc.getPackage();
+        String packagePrefix = "";
+        if (!StringUtils.isBlank(packageName)) {
+            packagePrefix = "." + packageName;
+        }
+        for (DescriptorProtos.DescriptorProto typeDesc : fileDesc.getMessageTypeList()) {
+            loadTypeFromMessage(fileDesc, packagePrefix, typeDesc, messageTypeMap, enumTypeMap);
+        }
+        for (DescriptorProtos.EnumDescriptorProto typeDesc : fileDesc.getEnumTypeList()) {
+            String enumTypeName = packagePrefix + "." + typeDesc.getName();
+            enumTypeMap.putIfAbsent(enumTypeName, typeDesc);
+        }
+    }
+
+    private static void loadTypeFromMessage(DescriptorProtos.FileDescriptorProto fileDesc,
+            String parentTypeName,
+            DescriptorProtos.DescriptorProto typeDesc,
+            Map<String, DescriptorProtos.DescriptorProto> messageTypeMap,
+            Map<String, DescriptorProtos.EnumDescriptorProto> enumTypeMap) {
+        String fullTypeName = parentTypeName + "." + typeDesc.getName();
+        messageTypeMap.putIfAbsent(fullTypeName, typeDesc);
+        for (DescriptorProtos.DescriptorProto nestedTypeDesc : typeDesc.getNestedTypeList()) {
+            loadTypeFromMessage(fileDesc, fullTypeName, nestedTypeDesc, messageTypeMap, enumTypeMap);
+        }
+        for (DescriptorProtos.EnumDescriptorProto enumTypeDesc : typeDesc.getEnumTypeList()) {
+            String enumTypeName = fullTypeName + "." + enumTypeDesc.getName();
+            enumTypeMap.putIfAbsent(enumTypeName, enumTypeDesc);
+        }
     }
 }
